64f4860c4fae254f8713537e33553ed0b8e13d5f
Rick Bernotas <rbernota@yahoo-inc.com>
2012-10-09 17:31:57 -0500

    Modified hadooptest.cluster.VMViewer to support scoping stack variables at the breakpoint.  Shows variable name and type, child field name and type, and some child field values (like int, float, Boolean, etc.).

--- hadooptest/src/main/java/hadooptest/cluster/VMViewer.java
+++ hadooptest/src/main/java/hadooptest/cluster/VMViewer.java
@@ -35,6 +35,20 @@ import com.sun.jdi.request.ClassPrepareRequest;
 import com.sun.jdi.request.EventRequestManager;
 import com.sun.jdi.request.ModificationWatchpointRequest;
 
+import com.sun.jdi.Value;
+import com.sun.jdi.StringReference;
+import com.sun.jdi.ObjectReference;
+
+import com.sun.jdi.BooleanValue;
+import com.sun.jdi.ByteValue;
+import com.sun.jdi.CharValue;
+import com.sun.jdi.DoubleValue;
+import com.sun.jdi.FloatValue;
+import com.sun.jdi.IntegerValue;
+import com.sun.jdi.LongValue;
+import com.sun.jdi.ShortValue;
+import com.sun.jdi.VoidValue;
+
 public class VMViewer {
 
 	public static final String CLASS_NAME_WATCH = "org.apache.hadoop.mapreduce.Job";
@@ -146,6 +160,55 @@ public class VMViewer {
 					List<LocalVariable> visibleVars = stackFrame.visibleVariables();
 					for (LocalVariable visibleVar: visibleVars) {
 						System.out.println("VARIABLE NAME: " + visibleVar.name());
+						Value val = stackFrame.getValue(visibleVar);
+						String value = "";
+						if (val instanceof StringReference) {
+							value = ((StringReference)val).value();
+							System.out.println("VARIABLE VALUE: " + value);
+						}
+						else if (val instanceof ObjectReference) {
+							value = ((ObjectReference)val).toString();
+							System.out.println("OBJECT VALUE: " + value);
+							List<Field> childFields = ((ObjectReference)val).referenceType().allFields();
+							for (Field childField: childFields) {
+								System.out.println("CHILD FIELD NAME: " + childField.name() + " - " + childField.toString());
+								Value fieldValue = ((ObjectReference)val).getValue(childField);
+								//System.out.println("value = " + fieldValue.toString());
+								if (fieldValue instanceof BooleanValue) {
+									System.out.println("\tVALUE = " + ((BooleanValue)fieldValue).toString());
+								}
+								else if (fieldValue instanceof ByteValue) {
+									System.out.println("\tVALUE = " + ((ByteValue)fieldValue).toString());
+								}
+								else if (fieldValue instanceof CharValue) {
+									System.out.println("\tVALUE = " + ((CharValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof DoubleValue) {
+									System.out.println("\tVALUE = " + ((DoubleValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof FloatValue) {
+									System.out.println("\tVALUE = " + ((FloatValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof IntegerValue) {
+									System.out.println("\tVALUE = " + ((IntegerValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof LongValue) {
+									System.out.println("\tVALUE = " + ((LongValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof ShortValue) {
+									System.out.println("\tVALUE = " + ((ShortValue)fieldValue).toString());
+									
+								}
+								else if (fieldValue instanceof VoidValue) {
+									System.out.println("\tVALUE = " + ((VoidValue)fieldValue).toString());
+								}
+							}
+						}
 					}
 					
 					threadRef.resume();
