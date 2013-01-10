be9c70ebe10df015e4492cba499b732e167678d6
Rick Bernotas <rbernota@yahoo-inc.com>
2013-01-10 17:25:25 -0600

    Added hadooptest.cluster.fullydistributed.FullyDistributedCluster and updated hadooptest.config.testconfig.FullyDistributedConfiguration to begin support for distributed clusters.  Added hadooptest.regression.Version as an initial junit test to veryify operation of distributed clusters.

---
+++ hadooptest/src/main/java/hadooptest/cluster/fullydistributed/FullyDistributedCluster.java
@@ -0,0 +1,260 @@
+package hadooptest.cluster.fullydistributed;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import hadooptest.TestSession;
+import hadooptest.Util;
+import hadooptest.cluster.Cluster;
+import hadooptest.cluster.ClusterState;
+import hadooptest.config.testconfig.FullyDistributedConfiguration;
+
+public class FullyDistributedCluster implements Cluster {
+
+	// The base pseudodistributed configuration.
+	protected FullyDistributedConfiguration conf;
+	
+	// The state of the pseudodistributed cluster.
+	protected ClusterState cluster_state;
+
+	private static TestSession TSM;
+	
+	private String HADOOP_INSTALL;
+	private String CONFIG_BASE_DIR;
+	
+	/*
+	 * Class constructor.
+	 * 
+	 * Creates a brand new default PseudoDistributedConfiguration, and writes out the configuration to disk.
+	 */
+	public FullyDistributedCluster(TestSession testSession) throws IOException
+	{
+		TSM = testSession;
+		
+		this.conf = new FullyDistributedConfiguration(testSession);
+		
+		this.initTestSessionConf();
+		
+		this.conf.write();
+	}
+
+	/*
+	 * Class constructor.
+	 * 
+	 * Accepts a custom configuration, and assumed you will write it to disk.
+	 */
+	public FullyDistributedCluster(TestSession testSession, FullyDistributedConfiguration conf)
+	{
+		TSM = testSession;
+		this.conf = conf;
+		
+		this.initTestSessionConf();
+	}
+	
+	/*
+	 * Starts the pseudodistributed cluster instance by starting:
+	 *   - NameNode
+	 *   - SecondaryNameNode
+	 *   - DataNode
+	 *   - ResourceManager
+	 *   - JobHistoryServer
+	 *   
+	 * Also verifies that the daemons have started by using jps.
+	 * 
+	 * (non-Javadoc)
+	 * @see hadooptest.cluster.Cluster#start()
+	 */
+	public void start() throws IOException {
+
+		//String format_dfs = HADOOP_INSTALL + "/bin/hadoop --config " + CONFIG_BASE_DIR + " namenode -format";
+		String start_dfs = HADOOP_INSTALL + "/sbin/start-dfs.sh --config " + CONFIG_BASE_DIR;
+		String start_yarn = HADOOP_INSTALL + "/sbin/start-yarn.sh --config " + CONFIG_BASE_DIR;
+		String start_historyserver = HADOOP_INSTALL + "/sbin/mr-jobhistory-daemon.sh start historyserver --config " + CONFIG_BASE_DIR;
+		String start_datanode = HADOOP_INSTALL + "/sbin/hadoop-daemon.sh --config " + CONFIG_BASE_DIR + " start datanode";
+
+		//TSM.logger.info("FORMATTING DFS...");
+		//runProc(format_dfs);
+		
+		TSM.logger.info("STARTING DFS...");
+		runProc(start_dfs);
+		assertTrue("The NameNode was not started.", verifyJpsProcRunning("NameNode"));
+		assertTrue("The SecondaryNameNode was not started.", verifyJpsProcRunning("SecondaryNameNode"));
+
+		TSM.logger.info("STARTING DATANODE...");
+		runProc(start_datanode);
+		assertTrue("The DataNode was not started.", verifyJpsProcRunning("DataNode"));
+		
+		TSM.logger.info("STARTING YARN");
+		runProc(start_yarn);
+		assertTrue("The ResourceManager was not started.", verifyJpsProcRunning("ResourceManager"));
+
+		TSM.logger.info("STARTING JOB HISTORY SERVER...");
+		runProc(start_historyserver);
+		assertTrue("The JobHistoryServer was not started.", verifyJpsProcRunning("JobHistoryServer"));
+		
+		TSM.logger.info("Sleeping for 30s to wait for HDFS to get out of safe mode.");
+		Util.sleep(30);
+	}
+
+	/* 
+	 * Stops all daemons associated with the pseudodistributed cluster instance, and
+	 * verifies they have stopped with jps.
+	 * 
+	 * (non-Javadoc)
+	 * @see hadooptest.cluster.Cluster#stop()
+	 */
+	public void stop() throws IOException {
+		String stop_dfs = HADOOP_INSTALL + "/sbin/stop-dfs.sh";
+		String stop_yarn = HADOOP_INSTALL + "/sbin/stop-yarn.sh";
+		String stop_historyserver = HADOOP_INSTALL + "/sbin/mr-jobhistory-daemon.sh stop historyserver";
+		String stop_datanode = HADOOP_INSTALL + "/sbin/hadoop-daemon.sh stop datanode";
+
+		runProc(stop_dfs);
+		runProc(stop_yarn);
+		runProc(stop_historyserver);
+		runProc(stop_datanode);
+
+		// Wait for 10 seconds to ensure that the daemons have had time to stop.
+		Util.sleep(10);
+
+		assertFalse("The NameNode was not stopped.", verifyJpsProcRunning("NameNode"));
+		assertFalse("The SecondaryNameNode was not stopped.", verifyJpsProcRunning("SecondaryNameNode"));
+		assertFalse("The DataNode was not stopped.", verifyJpsProcRunning("DataNode"));
+		assertFalse("The ResourceManager was not stopped.", verifyJpsProcRunning("ResourceManager"));
+		assertFalse("The JobHistoryServer was not stopped.", verifyJpsProcRunning("JobHistoryServer"));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see hadooptest.cluster.Cluster#die()
+	 */
+	public void die() throws IOException {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see hadooptest.cluster.Cluster#reset()
+	 */
+	public void reset() {
+
+	}
+
+	/*
+	 * Set a custom configuration for the pseudodistributed cluster instance.
+	 * 
+	 * @param conf The custom PseudoDistributedConfiguration
+	 */
+	public void setConf(FullyDistributedConfiguration conf) {
+		this.conf = conf;
+	}
+
+	/*
+	 * Gets the configuration for this pseudodistributed cluster instance.
+	 * 
+	 * @return PseudoDistributedConfiguration the configuration for the cluster instance.
+	 */
+	public FullyDistributedConfiguration getConf() {
+		return this.conf;
+	}
+
+	/*
+	 * Returns the state of the pseudodistributed cluster instance.
+	 * 
+	 * @return ClusterState the state of the cluster.
+	 * 
+	 * (non-Javadoc)
+	 * @see hadooptest.cluster.Cluster#getState()
+	 */
+	public ClusterState getState() {
+		return this.cluster_state;
+	}
+
+	/*
+	 * Initialize the test session configuration properties necessary to use the 
+	 * pseudo distributed cluster instance.
+	 */
+	private void initTestSessionConf() {
+		HADOOP_INSTALL = TSM.conf.getProperty("HADOOP_INSTALL", "");
+		CONFIG_BASE_DIR = TSM.conf.getProperty("CONFIG_BASE_DIR", "");
+	}
+	
+	/*
+	 * Run a local system command.
+	 * 
+	 * @param command The system command to run.
+	 */
+	private static void runProc(String command) {
+		Process proc = null;
+
+		TSM.logger.debug(command);
+
+		try {
+			proc = Runtime.getRuntime().exec(command);
+			BufferedReader reader=new BufferedReader(new InputStreamReader(proc.getInputStream())); 
+			String line=reader.readLine(); 
+			while(line!=null) 
+			{ 
+				TSM.logger.debug(line); 				
+				line=reader.readLine();
+			} 
+		}
+		catch (Exception e) {
+			if (proc != null) {
+				proc.destroy();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Verifies, with jps, that a given process name is running.
+	 * 
+	 * @param process The String representing the name of the process to verify.
+	 */
+	private static boolean verifyJpsProcRunning(String process) {
+
+		Process jpsProc = null;
+
+		String jpsCmd = "jps";
+
+		TSM.logger.debug(jpsCmd);
+
+		String jpsPatternStr = "(.*)(" + process + ")(.*)";
+		Pattern jpsPattern = Pattern.compile(jpsPatternStr);
+
+		try {
+			jpsProc = Runtime.getRuntime().exec(jpsCmd);
+			BufferedReader reader=new BufferedReader(new InputStreamReader(jpsProc.getInputStream())); 
+			String line=reader.readLine(); 
+			while(line!=null) 
+			{  
+				TSM.logger.debug(line);
+
+				Matcher jpsMatcher = jpsPattern.matcher(line);
+
+				if (jpsMatcher.find()) {
+					TSM.logger.debug("FOUND PROCESS: " + process);
+					return true;
+				}
+
+				line=reader.readLine();
+			} 
+		}
+		catch (Exception e) {
+			if (jpsProc != null) {
+				jpsProc.destroy();
+			}
+			e.printStackTrace();
+		}
+
+		TSM.logger.debug("PROCESS IS NO LONGER RUNNING: " + process);
+		return false;
+	}
+}
--- hadooptest/src/main/java/hadooptest/config/testconfig/FullyDistributedConfiguration.java
+++ hadooptest/src/main/java/hadooptest/config/testconfig/FullyDistributedConfiguration.java
@@ -1,37 +1,145 @@
 /*
  * YAHOO!
- * 
- * A class that represents a Hadoop Configuration for a fully
- * distributed Hadoop cluster under test.
- * 
- * 2012.10.08 - Rick Bernotas - Initial version.
  */
 
 package hadooptest.config.testconfig;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import hadooptest.TestSession;
 import hadooptest.config.TestConfiguration;
 
+/*
+ * A class that represents a Hadoop Configuration for a distributed
+ * Hadoop cluster under test.
+ */
 public class FullyDistributedConfiguration extends TestConfiguration
 {
 
-   public FullyDistributedConfiguration()
-   {
-      super(new StandaloneConfiguration());
+	private String CONFIG_BASE_DIR;
+	
+	private static TestSession TSM;
 
-      initDefaults();
-   }
+	/*
+	 * Class constructor.
+	 * 
+	 * Calls the superclass constructor, and initializes the default
+	 * configuration parameters for a distributed cluster under test.  Hadoop
+	 * default configuration is not used.
+	 */
+	public FullyDistributedConfiguration(TestSession testSession) {
+		super(false);
 
-   public FullyDistributedConfiguration(boolean loadDefaults)
-   {
-      super(new StandaloneConfiguration()); 
+		TSM = testSession;
+		
+		CONFIG_BASE_DIR = TSM.conf.getProperty("CONFIG_BASE_DIR", "");
+		
+		this.initDefaults();
+	}
 
-      initDefaults();
-   }
+	/*
+	 * Class constructor.
+	 * 
+	 * Loads the Hadoop default configuration if true is passed as a parameter, before the 
+	 * distributed test cluster default configuration is initialized into the 
+	 * configuration.
+	 * 
+	 * @param loadDefaults whether or not to load the default configuration parameters
+	 * specified by the Hadoop installation, before loading the class configuration defaults.
+	 */
+	public FullyDistributedConfiguration(boolean loadDefaults) {
+		super(loadDefaults); 
 
-   /**
-    */
-   private void initDefaults()
-   {
-   }
+		this.initDefaults();
+	}
+
+	/*
+	 * Writes the distributed cluster configuration specified by the object out
+	 * to disk.
+	 */
+	public void write() throws IOException {
+		File outdir = new File(CONFIG_BASE_DIR);
+		outdir.mkdirs();
+		
+		File historytmp = new File(CONFIG_BASE_DIR + "jobhistory/tmp");
+		historytmp.mkdirs();
+		File historydone = new File(CONFIG_BASE_DIR + "jobhistory/done");
+		historydone.mkdirs();
+
+		File core_site = new File(CONFIG_BASE_DIR + "core-site.xml");
+		File hdfs_site = new File(CONFIG_BASE_DIR + "hdfs-site.xml");
+		File yarn_site = new File(CONFIG_BASE_DIR + "yarn-site.xml");
+		File mapred_site = new File(CONFIG_BASE_DIR + "mapred-site.xml");		
+
+		if (core_site.createNewFile()) {
+			FileOutputStream out = new FileOutputStream(core_site);
+			this.writeXml(out);
+		}
+		else {
+			TSM.logger.warn("Couldn't create the xml configuration output file.");
+		}
+
+		if (hdfs_site.createNewFile()) {
+			FileOutputStream out = new FileOutputStream(hdfs_site);
+			this.writeXml(out);
+		}
+		else {
+			TSM.logger.warn("Couldn't create the xml configuration output file.");
+		}
+
+		if (yarn_site.createNewFile()) {
+			FileOutputStream out = new FileOutputStream(yarn_site);
+			this.writeXml(out);
+		}
+		else {
+			TSM.logger.warn("Couldn't create the xml configuration output file.");
+		}
+
+		if (mapred_site.createNewFile()) {
+			FileOutputStream out = new FileOutputStream(mapred_site);
+			this.writeXml(out);
+		}
+		else {
+			TSM.logger.warn("Couldn't create the xml configuration output file.");
+		}
+
+		FileWriter slaves_file = new FileWriter(CONFIG_BASE_DIR + "slaves");
+		BufferedWriter slaves = new BufferedWriter(slaves_file);
+		slaves.write("localhost");
+		slaves.close();
+	}
+
+	/*
+	 * Removes the configuration files from disk, that were written to disk
+	 * by the .write() of the object.
+	 */
+	public void cleanup() {
+		File core_site = new File(CONFIG_BASE_DIR + "core-site.xml");
+		File hdfs_site = new File(CONFIG_BASE_DIR + "hdfs-site.xml");
+		File yarn_site = new File(CONFIG_BASE_DIR + "yarn-site.xml");
+		File mapred_site = new File(CONFIG_BASE_DIR + "mapred-site.xml");	
+		File slaves = new File(CONFIG_BASE_DIR + "slaves");	
+		File log4jProperties = new File(CONFIG_BASE_DIR + "log4j.properties");
+
+		core_site.delete();
+		hdfs_site.delete();
+		yarn_site.delete();
+		mapred_site.delete();
+		slaves.delete();
+		log4jProperties.delete();
+	}
+
+	/*
+	 * Initializes a set of default configuration properties that have been 
+	 * determined to be a reasonable set of defaults for running a distributed
+	 * cluster under test.
+	 */
+	private void initDefaults() {
+
+	}
 
 }
