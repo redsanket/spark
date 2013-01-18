cf41d9402b5618c6d640b215ca7ea4e4bb7aea14
philips <philips@yahoo-inc.com>
2013-01-18 17:27:06 -0600

    Initial Checkin.

---
+++ hadooptest/src/test/java/hadooptest/regression/yarn/SleepJob.java
@@ -0,0 +1,78 @@
+package hadooptest.regression.yarn;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import hadooptest.TestSession;
+import hadooptest.cluster.fullydistributed.FullyDistributedCluster;
+// import hadooptest.cluster.fullydistributed.SleepJob;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SleepJob {
+
+	private static TestSession testSession;
+	
+	private SleepJob sleepJob;
+	private static FullyDistributedCluster cluster;
+	
+	/******************* CLASS BEFORE/AFTER ***********************/
+	
+	/*
+	 * Configuration and cluster setup that should happen before running any of the tests in the class instance.
+	 */
+	@BeforeClass
+	public static void startCluster() throws FileNotFoundException, IOException{
+		testSession = new TestSession();
+		
+		cluster = new FullyDistributedCluster(testSession);
+		// cluster.start();
+	}
+	
+	/*
+	 * Cluster cleanup that should happen after running tests in the class instance.
+	 */
+	@AfterClass
+	public static void stopCluster() throws IOException {
+		// cluster.stop();
+		// cluster.getConf().cleanup();
+	}
+	
+	/******************* TEST BEFORE/AFTER ***********************/
+	
+	/*
+	 * Before each test.
+	 */
+	@Before
+	public void initTest() {
+	}
+	
+	/*
+	 * After each test, we must reset the state of the cluster to a known default state.
+	 */
+	@After
+	public void resetClusterState() {
+	}
+	
+	/******************* TESTS ***********************/	
+	
+	/*
+	 * A test for running a sleep job
+	 * 
+	 * Equivalent to JobSummaryInfo10 in the original shell script YARN regression suite.
+	 */
+	@Test
+	public void RunSleepRun() throws IOException, FileNotFoundException {
+		String version = cluster.runSleepJob();
+	}
+	
+	/******************* END TESTS ***********************/	
+}
