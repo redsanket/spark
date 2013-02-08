7471588e824438bd829f435a5c97dea9ea9b80c3
philips <philips@yahoo-inc.com>
2013-02-08 14:14:23 -0600

    Move Hadoop general properties configuration from FullyDistributedConfiguration to here.

--- hadooptest/src/main/java/hadooptest/config/TestConfiguration.java
+++ hadooptest/src/main/java/hadooptest/config/TestConfiguration.java
@@ -6,7 +6,13 @@ package hadooptest.config;
 
 import hadooptest.TestSession;
 
-import java.io.IOException;
+import java.io.File;
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.TimeZone;
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -29,6 +35,13 @@ public abstract class TestConfiguration extends Configuration {
 	public static final String NODEMANAGER = "nodemanager";
 	public static final String GATEWAY = "gateway";
 
+	// General Hadoop configuration properties such as cluster name, 
+	// directory paths, etc.	
+    protected Properties hadoopProps = new Properties();
+
+    // Track Hadoop override configuration directories
+    protected Properties hadoopConfDirPaths = new Properties();
+
 	/* 
 	 * Class Constructor.
 	 * 
@@ -37,7 +50,9 @@ public abstract class TestConfiguration extends Configuration {
 	 * configuration properties.
 	 */
 	public TestConfiguration() {   
-		super(false);
+		super(false);		
+		this.initDefaults();
+
 	}
 
 	/*
@@ -48,6 +63,8 @@ public abstract class TestConfiguration extends Configuration {
 	 */
 	public TestConfiguration(boolean loadDefaults) {
 		super(loadDefaults);
+		this.initDefaults();
+
 	}
 
 	/*
@@ -57,6 +74,7 @@ public abstract class TestConfiguration extends Configuration {
 	 */
 	public TestConfiguration(Configuration other) {
 		super(other);
+		this.initDefaults();
 	}
 
 	/*
@@ -69,6 +87,163 @@ public abstract class TestConfiguration extends Configuration {
 	 */
 	public abstract void write();
 	
-	public abstract String getHadoopProp(String key);
+    /*
+     * Returns the Hadoop general property value for a given property name.
+     * 
+     * @return String property value such as cluster name, directory paths, etc. 
+     */
+	public String getHadoopProp(String key) {
+		if (!hadoopProps.getProperty(key).equals(null)) {
+    		return this.hadoopProps.getProperty(key);
+    	}
+    	else {
+			TestSession.logger.error("Couldn't find value for key '" + key + "'.");
+			return "";
+    	}
+    }
+
+	/*
+     * Returns the Hadoop configuration directory paths.
+     * 
+     * @return Properties the Hadoop configuration directory paths by
+     * components.
+     */
+	public Properties getHadoopConfDirPaths() {
+    	return this.hadoopConfDirPaths;
+    }
+
+    /*
+     * Returns the Hadoop configuration directory path for the given component.
+     * 
+     * @param component The hadoop component such as gateway, namenode,
+     * resourcemaanger, etc.
+     * 
+     * @return String of the directory path name..
+     */
+	public String getHadoopConfDirPath(String component) {
+		return this.getHadoopConfDirPaths().getProperty(component);
+	}
 	
+
+    /*
+     * Set the Hadoop configuration directory path for the given component.
+     * 
+     * @param component The hadoop component such as gateway, namenode,
+     * resourcemaanger, etc.
+     * @param path String of the directory path name.
+     * 
+     */
+	public void setHadoopConfDirPath(String component, String path) {
+		this.getHadoopConfDirPaths().setProperty(component, path);
+	}
+
+	/*
+	 * Initializes a set of default configuration properties that have been 
+	 * determined to be a reasonable set of defaults for running a distributed
+	 * cluster under test.
+	 */
+	private void initDefaults() {
+		
+		/*
+		 * NOTES: some of these properties will need to be initialized
+		 * differently for pseudo distributed configuration.
+		 */
+		
+		// Specific to fully distributed cluster configuration.
+		hadoopProps.setProperty("CLUSTER_NAME", TestSession.conf.getProperty("CLUSTER_NAME", ""));
+		try {
+			hadoopProps.setProperty("GATEWAY", InetAddress.getLocalHost().getHostName());
+		}
+		catch (Exception e) {
+			TestSession.logger.warn("Hostname not found!!!");
+		}
+	
+		// String defaultTmpDir = "/grid/0/tmp";
+		String defaultTmpDir = "/homes/hadoopqa/tmp/hadooptest";
+		hadoopProps.setProperty("TMP_DIR", 
+				TestSession.conf.getProperty("TMP_DIR", defaultTmpDir));
+		DateFormat df = new SimpleDateFormat("yyyy-MMdd-hhmmss");  
+		df.setTimeZone(TimeZone.getTimeZone("CST"));  
+		String tmpDir = this.getHadoopProp("TMP_DIR") + "/hadooptest-" +	
+				df.format(new Date());
+		new File(tmpDir).mkdirs();
+		hadoopProps.setProperty("TMP_DIR", tmpDir);
+		
+		String HADOOP_ROOT="/home";  // /grid/0								
+		hadoopProps.setProperty("JAVA_HOME", HADOOP_ROOT+"/gs/java/jdk");
+		hadoopProps.setProperty("HADOOP_INSTALL", HADOOP_ROOT + "/gs/gridre/yroot." +
+				hadoopProps.getProperty("CLUSTER_NAME"));
+
+		/* 
+		 * Properties beyond this point should be common across pseudo and fully
+		 * distributed cluster configuration.
+		 */
+
+		// Configuration directory and files
+		hadoopProps.setProperty("HADOOP_COMMON_HOME", hadoopProps.getProperty("HADOOP_INSTALL") +
+				"/share/hadoop");
+		hadoopProps.setProperty("HADOOP_CONF_DIR", hadoopProps.getProperty("HADOOP_INSTALL") +
+				"/conf/hadoop");
+		hadoopProps.setProperty("HADOOP_DEFAULT_CONF_DIR",
+				hadoopProps.getProperty("HADOOP_CONF_DIR"));
+		this.setHadoopConfDirPath("gateway",
+				hadoopProps.getProperty("HADOOP_CONF_DIR"));
+		hadoopProps.setProperty("HADOOP_CONF_CORE",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_CORE);
+		hadoopProps.setProperty("HADOOP_CONF_HDFS",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_HDFS);
+		hadoopProps.setProperty("HADOOP_CONF_MAPRED",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_MAPRED);
+		hadoopProps.setProperty("HADOOP_CONF_YARN",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_YARN);
+		hadoopProps.setProperty("HADOOP_CONF_CAPACITY_SCHEDULER",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_CAPACITY_SCHEDULER);
+		hadoopProps.setProperty("HADOOP_CONF_FAIR_SCHEDULER",
+				hadoopProps.getProperty("HADOOP_CONF_DIR") + "/" + HADOOP_CONF_FAIR_SCHEDULER);
+
+		// Binaries
+		hadoopProps.setProperty("HADOOP_BIN_DIR", hadoopProps.getProperty("HADOOP_COMMON_HOME") + "/bin");
+		hadoopProps.setProperty("HADOOP_BIN", hadoopProps.getProperty("HADOOP_BIN_DIR") + "/hadoop");
+		hadoopProps.setProperty("HDFS_BIN", hadoopProps.getProperty("HADOOP_BIN_DIR") + "/hdfs");
+		hadoopProps.setProperty("MAPRED_BIN", hadoopProps.getProperty("HADOOP_BIN_DIR") + "/mapred");
+		hadoopProps.setProperty("YARN_BIN", getHadoopProp("HADOOP_BIN_DIR") + "/yarn");
+
+		// Version dependent environment variables
+		String HADOOP_VERSION = this.getVersion();
+
+		// String HADOOP_VERSION = "23.6";
+		hadoopProps.setProperty("HADOOP_VERSION", HADOOP_VERSION);
+		
+		// Jars
+		hadoopProps.setProperty("HADOOP_JAR_DIR", getHadoopProp("HADOOP_COMMON_HOME") +
+				"/share/hadoop");
+		hadoopProps.setProperty("HADOOP_SLEEP_JAR", getHadoopProp("HADOOP_JAR_DIR") + 
+				"/mapreduce/" + "hadoop-mapreduce-client-jobclient-" +
+				HADOOP_VERSION + "-tests.jar"); 
+		hadoopProps.setProperty("HADOOP_EXAMPLE_JAR", getHadoopProp("HADOOP_JAR_DIR") +
+				"/mapreduce/" + "hadoop-mapreduce-examples-" +
+				HADOOP_VERSION + ".jar"); 
+		hadoopProps.setProperty("HADOOP_MR_CLIENT_JAR", getHadoopProp("HADOOP_JAR_DIR") + 
+				"/mapreduce/" + "hadoop-mapreduce-client-jobclient-" +
+				HADOOP_VERSION + ".jar"); 
+		hadoopProps.setProperty("HADOOP_STREAMING_JAR", getHadoopProp("HADOOP_JAR_DIR") +
+				"/tools/lib/" + "hadoop-streaming-" + 
+				HADOOP_VERSION + ".jar");
+	}
+	
+	/*
+     * Returns the version of the fully distributed Hadoop cluster being used.
+     * 
+     * @return String the Hadoop version for the fully distributed cluster.
+     * 
+     * (non-Javadoc)
+     * @see hadooptest.cluster.Cluster#getVersion()
+     */
+    public String getVersion() {
+    	String[] cmd = { this.getHadoopProp("HADOOP_BIN"),
+    			"--config", this.getHadoopProp("HADOOP_CONF_DIR"), "version" };	
+    	String version = (TestSession.exec.runProcBuilder(cmd))[1].split("\n")[0];
+        return version.split(" ")[1];
+    }
+
 }
