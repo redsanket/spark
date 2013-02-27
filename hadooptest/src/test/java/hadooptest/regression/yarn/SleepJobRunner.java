c8fa19daa0aa77d7f05dc0ab0d339b39086e272d
philips <philips@yahoo-inc.com>
2013-02-27 17:47:03 -0600

    commented out cluster restart in Before and After Class.

--- hadooptest/src/test/java/hadooptest/regression/yarn/SleepJobRunner.java
+++ hadooptest/src/test/java/hadooptest/regression/yarn/SleepJobRunner.java
@@ -25,7 +25,7 @@ public class SleepJobRunner extends TestSession {
 	@BeforeClass
 	public static void startCluster() {
 		TestSession.start();
-		cluster.start();
+		// cluster.start();
 	}
 	
 	/*
@@ -33,8 +33,8 @@ public class SleepJobRunner extends TestSession {
 	 */
 	@AfterClass
 	public static void stopCluster() {
-		cluster.stop();
-		cluster.getConf().cleanup();
+		// cluster.stop();
+		// cluster.getConf().cleanup();
 	}
 	
 	/******************* TESTS ***********************/	
