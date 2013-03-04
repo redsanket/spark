1609a921d2bec65c020dd09b08554e5f4edea0bf
philips <philips@yahoo-inc.com>
2013-03-04 15:10:05 -0600

    Rename test.

---
+++ hadooptest/src/test/java/hadooptest/regression/TestClusterReset.java
@@ -0,0 +1,34 @@
+package hadooptest.regression;
+
+import static org.junit.Assert.assertTrue;
+import hadooptest.TestSession;
+import hadooptest.cluster.fullydistributed.FullyDistributedCluster;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TestClusterReset extends TestSession {
+
+	@BeforeClass
+	public static void startTestSession() throws IOException {
+		TestSession.start();
+	}
+	
+	@Test
+	public void restartCluster() {
+		FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
+		assertTrue("Cluster reset failed", cluster.reset());		
+		assertTrue("Cluster is not off of safemode after cluster reset", cluster.waitForSafemodeOff());
+		assertTrue("Cluster is not fully up after cluster reset", cluster.isClusterFullyUp());		
+	}	
+	
+}
