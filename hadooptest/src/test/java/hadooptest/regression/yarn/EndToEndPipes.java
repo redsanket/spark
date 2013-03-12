4599494b1baf0c2050278b87292e1e47168859a7
philips <philips@yahoo-inc.com>
2013-03-12 09:37:20 -0500

    [bug 6145689] - Port end-to-end pipes test suites to Java framework.

--- hadooptest/src/test/java/hadooptest/regression/yarn/EndToEndPipes.java
+++ hadooptest/src/test/java/hadooptest/regression/yarn/EndToEndPipes.java
@@ -1,23 +1,19 @@
 package hadooptest.regression.yarn;
 
-import static org.junit.Assert.assertTrue;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static org.junit.Assert.*;
 
 import hadooptest.TestSession;
-import hadooptest.Util;
-import hadooptest.cluster.fullydistributed.FullyDistributedCluster;
-import hadooptest.job.SleepJob;
 
-import org.apache.hadoop.fs.FileStatus;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.hadoop.security.SecurityUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.FsShell;
 import org.apache.hadoop.fs.Path;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,167 +23,137 @@ public class EndToEndPipes extends TestSession {
 	@BeforeClass
 	public static void startTestSession() {
 		TestSession.start();
-		// TestSession.cluster.start();
 	}
 
-	
-//	public void deleteHdfsDir() throws IOException {
+	public void fsls(String path) throws Exception {
+		fsls(path, null);
+	}
 
-		/*
+	public String getHdfsBaseUrl() throws Exception {
+		return "hdfs://" + TestSession.cluster.getNodes("namenode")[0];
+	}
+	
+	public void fsls(String path, String[] args) throws Exception {
+		TestSession.logger.debug("Show HDFS path: '" + path + "':");
+		FsShell fsShell = TestSession.cluster.getFsShell();
+		String URL = getHdfsBaseUrl() + path;
+ 
+		String[] cmd;
+		if (args == null) {
+			cmd = new String[] {"-ls", URL};
+		} else {
+			ArrayList list = new ArrayList();
+			list.add("-ls");
+			list.addAll(Arrays.asList(args));
+			list.add(URL);
+			cmd = (String[]) list.toArray(new String[0]);
+		}
+		TestSession.logger.info(TestSession.cluster.getConf().getHadoopProp("HDFS_BIN") +
+					" dfs " + StringUtils.join(cmd, " "));
+ 		fsShell.run(cmd);
+	}
+
+	public void showHdfsDir() throws Exception {
+		fsls("/user/" + System.getProperty("user.name") + "/pipes", new String[] {"-d"});
+		fsls("/user/" + System.getProperty("user.name") + "/pipes", new String[] {"-R"});
+	}	
+
+	public void setupHdfsDir() throws Exception {
 		FileSystem fs = TestSession.cluster.getFS();
-		FileUtil fileUtil = new FileUtil();
-		String[] files = fileUtil.list(new File("/"));
-		TestSession.logger.info("Hadoop FS = " + Arrays.toString(files));
-*/
-		
-		/*
-		fs.copyFromLocalFile(new Path("/home/user/directory/"), 
-		  new Path("/user/hadoop/dir"));
-		*/
-		
-		/*
-	    local dirName=$1
-	    local fs=$2
-	    local skipTrash=''
-
-	    #if $3 not empty and equal to 0 then skip trash
-	    if [ -n "$3" ] && [ "$3" -eq "0" ]; then
-	        skipTrash="-skipTrash"
-	    fi
-
-	    local cmd1
-	    local cmd2
-
-	    if [ -z "$fs" ]; then
-	        cmd1="$HADOOP_HDFS_CMD --config $HADOOP_CONF_DIR dfs -ls $dirName"
-	        cmd2="$HADOOP_HDFS_CMD --config $HADOOP_CONF_DIR dfs -rm -r $skipTrash $dirName"
-	    else
-	        cmd1="$HADOOP_HDFS_CMD --config $HADOOP_CONF_DIR dfs -fs $fs -ls $dirName"
-	        cmd2="$HADOOP_HDFS_CMD --config $HADOOP_CONF_DIR dfs -fs $fs -rm -r $skipTrash $dirName"
-	    fi
-	    echo $cmd1
-	    eval $cmd1
-	    if [ "$?" -eq 0 ]; then
-	        echo $cmd2
-	        eval $cmd2
-	    fi
-	    */
-	//}
+		FsShell fsShell = TestSession.cluster.getFsShell();		
+		String testDir = getHdfsBaseUrl() + "/user/" + System.getProperty("user.name") + "/pipes";
+		if (fs.exists(new Path(testDir))) {
+			TestSession.logger.info("Delete existing test directory: " + testDir);
+			fsShell.run(new String[] {"-rm", "-r", testDir});			
+		}
+		TestSession.logger.info("Create new test directory: " + testDir);
+		fsShell.run(new String[] {"-mkdir", "-p", testDir});
+	}
 	
+	
+	/*
+	 *  Check for the destination directory and create it if
+     * is not present because 'dfs put' used to do that 
+     */
+	private void putLocalToHdfs(String source, String target) throws Exception {
+	    TestSession.logger.debug("target=" + target);
+	    String targetDir = target.substring(0, target.lastIndexOf("/"));	    
+	    TestSession.logger.debug("target path=" + targetDir);
+
+	    FsShell fsShell = TestSession.cluster.getFsShell();
+		FileSystem fs = TestSession.cluster.getFS();
+
+		String URL = "hdfs://" + TestSession.cluster.getNodes("namenode")[0] + "/";
+		String homeDir = URL + "user/" + System.getProperty("user.name");
+		String testDir = homeDir + "/" + targetDir;
+		if (!fs.exists(new Path(testDir))) {
+			fsShell.run(new String[] {"-mkdir", "-p", testDir});
+		}
+		TestSession.logger.debug("dfs -put " + source + " " + target);
+		fsShell.run(new String[] {"-put", source, target});
+	}
+	
+	private String getResourceFullPath(String relativePath) throws Exception {
+        URL url = this.getClass().getClassLoader().getResource(relativePath);
+        String fullPath = url.getPath();
+        TestSession.logger.debug("Resource URL path=" + fullPath);
+        return fullPath;
+	}
 	
 	/*
 	 * A test for running a pipes wordcount job
 	 */
 	@Test
-	public void runPipesTest()  throws IOException {
+	public void runPipesTest() throws Exception {
+		TestSession.logger.info("Run EndToEnd Pipes Test");
 		
-		TestSession.logger.info("******************* Test FS");		
-		FileSystem fs = TestSession.cluster.getFS();		
+		TestSession.logger.trace(TestSession.cluster.getConf().toString("resources"));
+		TestSession.logger.trace(TestSession.cluster.getConf().toString("props"));
 
-		String src = "/homes/philips/foobar";
-		String dst = "/user/philips";
-		fs.copyFromLocalFile(new Path(src), new Path(dst));
-		assertTrue("Did not find root dir /", fs.exists(new Path("/")));
-		assertTrue("Did not find file!!!",  fs.exists(new Path(dst+"/foobar")));
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		// FileStatus[] status = fs.listStatus(new Path("hdfs://test.com:9000/user/test/in"));  // you need to pass in your hdfs path
-		FileStatus[] status = fs.listStatus(new Path("/"));  // you need to pass in your hdfs path
-        for (int i=0;i<status.length;i++){
-                BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(status[i].getPath())));
-                String line;
-                line=br.readLine();
-                while (line != null){
-                        System.out.println(line);
-                        line=br.readLine();
-                }
-        }
-		*/
-		
-		FileUtil fileUtil = new FileUtil();
-		String[] files = fileUtil.list(new File("/"));
-		TestSession.logger.info("Hadoop FS = " + Arrays.toString(files));
+		SecurityUtil.login(TestSession.cluster.getConf(),
+        		"keytab-hadoopqa", "user-hadoopqa");
 
-		
-		
-		
-		// FileSystem fs = cluster.getFS();
-		//fs.exists(new Path("/"));
-		
-		
-		// cluster.getFileSystem();
-	
-		/*
-		FileSystem fs = cluster.getFileSystem();
-		FileUtil fileUtil = new FileUtil();
-		String[] files = fileUtil.list(new File("/"));
-		TestSession.logger.info("Hadoop FS = " + Arrays.toString(files));
-*/
-		
-		/*		
-		  setTestCaseDesc "Pipes-$TESTCASE_ID - Run a pipes wordcount job"
-		deleteHdfsDir Pipes 2>&1
-		  createHdfsDir Pipes/pipes-${TESTCASE_ID} 2>&1
+		setupHdfsDir();
+		putLocalToHdfs(getResourceFullPath("resources/data/pipes/c++-examples/Linux-i386-32/bin/"), "pipes/");
+		putLocalToHdfs(getResourceFullPath("resources/data/pipes/input.txt"), "pipes/input.txt");
+		showHdfsDir();
 
-		#  echo "Copying HADOOP_PREFIX to $ARTIFACTS/hadoop"
-		#  cp -R $HADOOP_PREFIX/ $ARTIFACTS/hadoop  >> $ARTIFACTS_FILE 2>&1
+		String[] jobCmd = {
+				TestSession.cluster.getConf().getHadoopProp("MAPRED_BIN"),
+				"--config", TestSession.cluster.getConf().getHadoopProp("HADOOP_CONF_DIR"),
+				"pipes", 
+				"-conf", getResourceFullPath("resources/data/pipes/word.xml"),	
+				"-input", "pipes/input.txt",
+				"-output", "pipes/outputDir",	 
+				"-jobconf", "mapred.job.name=End2EndPipesTest",
+				"-jobconf", "mapreduce.job.acl-view-job=*"
+		 };
+		String[] jobOutput = TestSession.exec.runHadoopProcBuilder(jobCmd);
+		if (!jobOutput[0].equals("0")) {
+			TestSession.logger.info("Got unexpected non-zero exit code: " + jobOutput[0]);
+			TestSession.logger.info("stdout" + jobOutput[1]);
+			TestSession.logger.info("stderr" + jobOutput[2]);			
+		}
 
-		#  cd $ARTIFACTS/hadoop
-		#  export ANT_HOME=/homes/hadoopqa/tools/ant/latest/
-
-		#  echo "Changing permissions of $ARTIFACTS/hadoop/src/c++/pipes/"
-		#  chmod -R 755 $ARTIFACTS/hadoop/src/c++/pipes/
-		#  echo "Changing permissions of $ARTIFACTS/hadoop/src/c++/utils/"
-		#  chmod -R 755 $ARTIFACTS/hadoop/src/c++/utils/
-		#  echo "Changing permissions of $ARTIFACTS/hadoop/src/examples/pipes/"
-		#  chmod -R 755 $ARTIFACTS/hadoop/src/examples/pipes/
-
-		#  echo "Compiling the examples"
-		#  echo "$ANT_HOME/bin/ant -Dcompile.c++=yes examples"
-		#  $ANT_HOME/bin/ant -Dcompile.c++=yes examples 
-		#  if [[ $? -eq 0 ]] ; then
-		    putLocalToHdfs $JOB_SCRIPTS_DIR_NAME/data/pipes-$TESTCASE_ID/c++-examples/Linux-i386-32/bin/ Pipes/pipes-${TESTCASE_ID} 2>&1
-		    putLocalToHdfs $JOB_SCRIPTS_DIR_NAME/data/pipes-$TESTCASE_ID/input.txt Pipes/pipes-${TESTCASE_ID}/input.txt 2>&1
-
-		    cd - >> $ARTIFACTS_FILE 2>&1
-
-		    echo "PIPES COMMAND: $HADOOP_MAPRED_CMD --config $HADOOP_CONF_DIR  pipes -conf  $JOB_SCRIPTS_DIR_NAME/data/pipes-${TESTCASE_ID}/word.xml -input Pipes/pipes-${TESTCASE_ID}/input.txt -output Pipes/pipes-$TESTCASE_ID/outputDir -jobco
-		nf mapred.job.name="pipesTest-$TESTCASE_ID" -jobconf mapreduce.job.acl-view-job=*"
-		    echo "OUTPUT:"
-		    $HADOOP_MAPRED_CMD --config $HADOOP_CONF_DIR  pipes -conf  $JOB_SCRIPTS_DIR_NAME/data/pipes-${TESTCASE_ID}/word.xml -input Pipes/pipes-${TESTCASE_ID}/input.txt -output Pipes/pipes-$TESTCASE_ID/outputDir -jobconf mapred.job.name="p
-		ipesTest-$TESTCASE_ID" -jobconf mapreduce.job.acl-view-job=*
-		    validatePipesOutput
-		    COMMAND_EXIT_CODE=$?
-		#  else
-		#    REASONS="Compilation failed. Aborting the test. Bug 4213034"
-		#    echo $REASONS
-		#    COMMAND_EXIT
-		*/
+		String[] catCmd = {
+				TestSession.cluster.getConf().getHadoopProp("HDFS_BIN"),
+				"--config", TestSession.cluster.getConf().getHadoopProp("HADOOP_CONF_DIR"),
+				"dfs", "-cat", "pipes/outputDir/*"				
+		};
+		String[] catOutput = TestSession.exec.runHadoopProcBuilder(catCmd);
+		if (!catOutput[0].equals("0")) {
+			TestSession.logger.info("Got unexpected non-zero exit code: " + catOutput[0]);
+			TestSession.logger.info("stdout" + catOutput[1]);
+			TestSession.logger.info("stderr" + catOutput[2]);			
+		}
 		
-		
+		String expectedOutputStr = FileUtils.readFileToString(
+						new File(getResourceFullPath("" +
+								"resources/data/pipes/expectedOutput")));
+		String actualOutputStr = catOutput[1];
+		TestSession.logger.debug("expected output str = \n'" + expectedOutputStr + "'");
+		TestSession.logger.debug("actual output str = \n'" + actualOutputStr + "'");
+		assertEquals("Actual output is different than expected ouput.", expectedOutputStr, actualOutputStr);
 	}
-	
+
 }
