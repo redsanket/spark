ef167506da2b47d7ae42bc40bff5eee6a035beb5
Rick Bernotas <rbernota@yahoo-inc.com>
2013-03-13 16:41:16 -0500

    [bug 6149364] - Created a new hadooptest.cluster.DFS class to maintain common HDFS methods based on things Phil wrote for the end to end tests.  Added a basically empty class for the end to end streaming tests.

---
+++ hadooptest/src/test/java/hadooptest/regression/yarn/TestEndToEndStreaming.java
@@ -0,0 +1,34 @@
+package hadooptest.regression.yarn;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import hadooptest.TestSession;
+
+public class TestEndToEndStreaming extends TestSession {
+	
+	@BeforeClass
+	public static void startTestSession() {
+		TestSession.start();
+	}
+	
+	@Test
+	public void testCacheArchives() {
+		
+	}
+	
+	@Test
+	public void testArchives() {
+		
+	}
+	
+	@Test
+	public void testCacheFiles() {
+		
+	}
+	
+	@Test
+	public void testFiles() {
+		
+	}
+}
