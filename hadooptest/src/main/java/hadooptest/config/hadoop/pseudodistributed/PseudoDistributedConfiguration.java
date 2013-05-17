216d3975280e4108d4445fa63392c58bb6ebab9c
philips <philips@yahoo-inc.com>
2013-05-17 15:28:05 -0500

    [bug 6222232] - Add code coverage to the framework secondary and acceptance CI build.

--- hadooptest/src/main/java/hadooptest/config/hadoop/pseudodistributed/PseudoDistributedConfiguration.java
+++ hadooptest/src/main/java/hadooptest/config/hadoop/pseudodistributed/PseudoDistributedConfiguration.java
@@ -11,10 +11,6 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
 
 import hadooptest.TestSession;
 import hadooptest.config.hadoop.HadoopConfiguration;
@@ -28,72 +24,115 @@ public class PseudoDistributedConfiguration extends HadoopConfiguration
 
 	/**
 	 * Calls the superclass constructor, and initializes the default
-	 * configuration parameters for a pseudodistributed cluster under test.  Hadoop
-	 * default configuration is not used.
+	 * configuration parameters for a pseudodistributed cluster under test.
+	 * Hadoop default configuration is not used.
 	 * 
-	 * @throws UnknownHostException if there is a fatal error initializing the default configuration.
+	 * @throws UnknownHostException if there is a fatal error initializing the
+	 * default configuration.
 	 */
 	public PseudoDistributedConfiguration() throws Exception {
 		super(false);
 		this.initDefaults();
 	}
 
-	/**
+    /**
+     * Calls the superclass constructor, and initializes the default
+     * configuration parameters for a pseudodistributed cluster under test.
+     * Hadoop default configuration is not used.
+     * 
+     * @throws UnknownHostException if there is a fatal error initializing the
+     * default configuration.
+     */
+    public PseudoDistributedConfiguration(String confDir, String hostname,
+            String component) throws Exception {
+        super(false, confDir, hostname, component);
+        this.initDefaults();
+    }
+
+    /**
+     * Calls the superclass constructor, and initializes the default
+     * configuration parameters for a pseudodistributed cluster under test.
+     * Hadoop default configuration is not used.
+     * 
+     * @throws UnknownHostException if there is a fatal error initializing the
+     * default configuration.
+     */
+    public PseudoDistributedConfiguration(String hostname, String component)
+            throws Exception {
+        super(false, TestSession.conf.getProperty("HADOOP_INSTALL_CONF_DIR"),
+                hostname, component);
+        this.initDefaults();
+    }
+
+    /**
 	 * Class constructor.
 	 * 
-	 * Loads the Hadoop default configuration if true is passed as a parameter, before the 
-	 * pseudodistributed test cluster default configuration is initialized into the 
-	 * configuration.
+	 * Loads the Hadoop default configuration if true is passed as a parameter,
+	 * before the pseudodistributed test cluster default configuration is 
+	 * initialized into the configuration.
 	 * 
-	 * @param loadDefaults whether or not to load the default configuration parameters
-	 * specified by the Hadoop installation, before loading the class configuration defaults.
+	 * @param loadDefaults whether or not to load the default configuration 
+	 * parameters specified by the Hadoop installation, before loading the
+	 * class configuration defaults.
 	 * 
-	 * @throws UnknownHostException if there is a fatal error initializing the default configuration.
+	 * @throws UnknownHostException if there is a fatal error initializing the 
+	 * default configuration.
 	 */
-	public PseudoDistributedConfiguration(boolean loadDefaults) throws Exception {
+	public PseudoDistributedConfiguration(boolean loadDefaults) 
+	        throws Exception {
 		super(loadDefaults); 
 		this.initDefaults();
 	}
+
+	
+	/**
+     * Initializes a set of default configuration properties that have been 
+     * determined to be a reasonable set of defaults for running a 
+     * pseudodistributed cluster under test.
+     */
+    private void initDefaults() {
+        String confDir = this.hadoopConfDir;
+        set("fs.default.name", "hdfs://localhost/");
+        set("dfs.replication", "1");
+        set("mapreduce.framework.name", "yarn");
+        set("yarn.resourcemanager.address", "localhost:8032");
+        set("yarn.nodemanager.aux-services", "mapreduce.shuffle");
+        set("mapreduce.jobhistory.intermediate-done-dir",
+                confDir + "jobhistory/tmp");
+        set("mapreduce.jobhistory.done-dir",
+                confDir + "jobhistory/done");
+        set("fs.AbstractFileSystem.hdfs.impl", "org.apache.hadoop.fs.Hdfs");
+    }
 
 	/**
 	 * Initializes cluster-specific properties defaults.
 	 */
 	protected void initDefaultsClusterSpecific() {
-		String defaultTmpDir = "/Users/" + System.getProperty("user.name") + "/hadooptest/tmp";
-		hadoopProps.setProperty("TMP_DIR", 
-				TestSession.conf.getProperty("TMP_DIR", defaultTmpDir));
-		DateFormat df = new SimpleDateFormat("yyyy-MMdd-hhmmss");  
-		df.setTimeZone(TimeZone.getTimeZone("CST"));  
-		String tmpDir = this.getHadoopProp("TMP_DIR") + "/hadooptest-" +	
-				df.format(new Date());
-		new File(tmpDir).mkdirs();
-		hadoopProps.setProperty("TMP_DIR", tmpDir);
-		hadoopProps.setProperty("HADOOP_INSTALL", TestSession.conf.getProperty("HADOOP_INSTALL", ""));
-		
-		hadoopProps.setProperty("HADOOP_COMMON_HOME", hadoopProps.getProperty("HADOOP_INSTALL"));
+ 
 	}
 	
 	/**
-	 * Writes the pseudodistributed cluster configuration specified by the object out
-	 * to disk.
+	 * Writes the pseudodistributed cluster configuration specified by the 
+	 * object out to disk.
 	 * 
-	 * @throws IOException if there is a problem writing configuration files to disk.
+	 * @throws IOException if there is a problem writing configuration files
+	 * to disk.
 	 */
 	public void write() throws IOException {
-		String configurationDir = TestSession.conf.getProperty("HADOOP_INSTALL", "") + "/conf/hadoop/";
-		
-		File outdir = new File(configurationDir);
+	    String confDir = this.hadoopConfDir;
+	    
+		File outdir = new File(confDir);
 		outdir.mkdirs();
 		
-		File historytmp = new File(configurationDir + "jobhistory/tmp");
+		File historytmp = new File(confDir + "jobhistory/tmp");
 		historytmp.mkdirs();
-		File historydone = new File(configurationDir + "jobhistory/done");
+		File historydone = new File(confDir + "jobhistory/done");
 		historydone.mkdirs();
 
-		File core_site = new File(configurationDir + "core-site.xml");
-		File hdfs_site = new File(configurationDir + "hdfs-site.xml");
-		File yarn_site = new File(configurationDir + "yarn-site.xml");
-		File mapred_site = new File(configurationDir + "mapred-site.xml");		
+		File core_site = new File(confDir + "core-site.xml");
+		File hdfs_site = new File(confDir + "hdfs-site.xml");
+		File yarn_site = new File(confDir + "yarn-site.xml");
+		File mapred_site = new File(confDir + "mapred-site.xml");		
 
 		core_site.createNewFile();
 		hdfs_site.createNewFile();
@@ -112,7 +151,7 @@ public class PseudoDistributedConfiguration extends HadoopConfiguration
 		out = new FileOutputStream(mapred_site);
 		this.writeXml(out);
 
-		FileWriter slaves_file = new FileWriter(configurationDir + "slaves");
+		FileWriter slaves_file = new FileWriter(confDir + "slaves");
 		BufferedWriter slaves = new BufferedWriter(slaves_file);
 		slaves.write("localhost");
 		slaves.close();
@@ -123,14 +162,15 @@ public class PseudoDistributedConfiguration extends HadoopConfiguration
 	 * by the .write() of the object.
 	 */
 	public void cleanup() {
-		String configurationDir = TestSession.cluster.getConf().getHadoopConfDir();
-		
-		File core_site = new File(configurationDir + "/core-site.xml");
-		File hdfs_site = new File(configurationDir + "/hdfs-site.xml");
-		File yarn_site = new File(configurationDir + "/yarn-site.xml");
-		File mapred_site = new File(configurationDir + "/mapred-site.xml");	
-		File slaves = new File(configurationDir + "/slaves");	
-		File log4jProperties = new File(configurationDir + "/log4j.properties");
+	    String confDir = this.hadoopConfDir;
+		TestSession.logger.info("Clean up and delete the content of the " +
+		        "Hadoop configuration directory '" + confDir + "'.");
+		File core_site = new File(confDir + "/core-site.xml");
+		File hdfs_site = new File(confDir + "/hdfs-site.xml");
+		File yarn_site = new File(confDir + "/yarn-site.xml");
+		File mapred_site = new File(confDir + "/mapred-site.xml");	
+		File slaves = new File(confDir + "/slaves");	
+		File log4jProperties = new File(confDir + "/log4j.properties");
 
 		core_site.delete();
 		hdfs_site.delete();
@@ -138,24 +178,5 @@ public class PseudoDistributedConfiguration extends HadoopConfiguration
 		mapred_site.delete();
 		slaves.delete();
 		log4jProperties.delete();
-	}
-	
-	/**
-	 * Initializes a set of default configuration properties that have been 
-	 * determined to be a reasonable set of defaults for running a pseudodistributed
-	 * cluster under test.
-	 */
-	private void initDefaults() {
-		String configurationDir = TestSession.conf.getProperty("HADOOP_INSTALL", "") + "/conf/hadoop/";
-		
-		set("fs.default.name", "hdfs://localhost/");
-		set("dfs.replication", "1");
-		set("mapreduce.framework.name", "yarn");
-		set("yarn.resourcemanager.address", "localhost:8032");
-		set("yarn.nodemanager.aux-services", "mapreduce.shuffle");
-		set("mapreduce.jobhistory.intermediate-done-dir", configurationDir + "jobhistory/tmp");
-		set("mapreduce.jobhistory.done-dir", configurationDir + "jobhistory/done");
-		set("fs.AbstractFileSystem.hdfs.impl", "org.apache.hadoop.fs.Hdfs");
-	}
-
+	}	
 }
