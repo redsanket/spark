7b606f690c21def9298036470c0ba19dde3c0088
philips <philips@yahoo-inc.com>
2013-05-17 08:09:07 -0500

    [bug 6222232] - Add code coverage to the framework secondary and acceptance CI build.

--- hadooptest/conf/jacoco/jacoco-env.sh
+++ hadooptest/conf/jacoco/jacoco-env.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+hostname=`/bin/hostname`
+
 JACOCO_PATH="/homes/hadoopqa/jacoco"
 JACOCO_JAR="$JACOCO_PATH/jacocoagent.jar"
 JACOCO_DEST="/homes/hadoopqa/jacoco/results/latest"
 
-JACOCO_NN_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/namenode.jacoco.exec,includes=org.apache.hadoop*,append=true"
-JACOCO_DN_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/datanode.jacoco.exec,includes=org.apache.hadoop*,append=true"
-JACOCO_BAL_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/balancer.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_NN_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/namenode-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_DN_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/datanode-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_BAL_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/balancer-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
 
-JACOCO_RM_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/resourcemanager.jacoco.exec,includes=org.apache.hadoop*,append=true"
-JACOCO_NM_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/nodemanager.jacoco.exec,includes=org.apache.hadoop*,append=true"
-JACOCO_JHS_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/jhs.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_RM_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/resourcemanager-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_NM_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/nodemanager-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
+JACOCO_JHS_OPT="-javaagent:${JACOCO_JAR}=destfile=${JACOCO_DEST}/jhs-${hostname}.jacoco.exec,includes=org.apache.hadoop*,append=true"
 
