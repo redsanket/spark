266d6b9e8c7d0d37b7918d47bb1ab220cdd00bae
philips <philips@yahoo-inc.com>
2013-05-17 08:41:39 -0500

    [bug 6222232] - Add code coverage to the framework secondary and acceptance CI build.

--- hadooptest/src/test/java/hadooptest/hadoop/regression/TestConf.java
+++ hadooptest/src/test/java/hadooptest/hadoop/regression/TestConf.java
@@ -3,6 +3,7 @@ package hadooptest.hadoop.regression;
 import static org.junit.Assert.fail;
 import hadooptest.TestSession;
 import hadooptest.SerialTests;
+import hadooptest.cluster.hadoop.HadoopCluster;
 import hadooptest.cluster.hadoop.fullydistributed.FullyDistributedCluster;
 import hadooptest.config.hadoop.HadoopConfiguration;
 
@@ -18,6 +19,8 @@ import java.io.IOException;
 @Category(SerialTests.class)
 public class TestConf extends TestSession {
 
+    private String component = HadoopCluster.RESOURCE_MANAGER;
+    
 	@BeforeClass
 	public static void startTestSession() throws IOException {
 		TestSession.start();
@@ -32,91 +35,55 @@ public class TestConf extends TestSession {
     }
 
 	@Test
-	public void backupHadoopConf() {
-		try {
-			FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
-			String component = HadoopConfiguration.RESOURCE_MANAGER;
-
-			// Backup the default configuration directory on the Resource Manager
-			// component host.
-			cluster.getConf().backupConfDir(component);		
-
-		}
-		catch (Exception e) {
-			TestSession.logger.error("Exception failure.", e);
-			fail();
-		}
+	public void backupHadoopConf() throws Exception {
+	    /* Backup the default configuration directory on the
+	     * Resource Manager component host. 
+	     */
+	    FullyDistributedCluster cluster =
+	            (FullyDistributedCluster) TestSession.cluster;
+	    cluster.getConf(component).backupConfDir();
 	}
 
 	@Test
-	public void copyFilesToHadoopConf() {
-		try {
-			FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
-			String component = HadoopConfiguration.RESOURCE_MANAGER;
-
-			// Copy files to the custom configuration directory on the
-			// Resource Manager component host.
-			// String sourceDir = "/homes/philips/svn/HadoopQEAutomation/branch-23/tests/Regression/YARN/CapacitySchedulerLimits/config/baseline/";
-			String sourceDir = "./conf/TestConf/";
-			cluster.getConf().copyFilesToConfDir(sourceDir, component);
-		}
-		catch (Exception e) {
-			TestSession.logger.error("Exception failure.", e);
-			fail();
-		}
+	public void copyFilesToHadoopConf() throws Exception {
+	    /* Copy files to the custom configuration directory on the
+	     * Resource Manager component host.
+	     */
+        FullyDistributedCluster cluster =
+                (FullyDistributedCluster) TestSession.cluster;
+	    String sourceDir = "./conf/TestConf/";
+	    cluster.getConf(component).copyFilesToConfDir(sourceDir);
 	}
 
 	@Test
-	public void modifyHadoopConf() {
-		try {
-			FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
-			String component = HadoopConfiguration.RESOURCE_MANAGER;
-			String confFile = HadoopConfiguration.HADOOP_CONF_YARN;
-
-			// Insert a property to the yarn-site.xml configuration file on the
-			// Resource Manager component host.
-			cluster.getConf().setHadoopConfFileProp (
-					"yarn.admin.acl3",
-					"gridadmin,hadoop,hadoopqa,philips,foo",
-					component, confFile);
-		}
-		catch (Exception e) {
-			TestSession.logger.error("Exception failure.", e);
-			fail();
-		}
+	public void modifyHadoopConf() throws Exception {	    
+	    /* Insert a property to the yarn-site.xml configuration file on the
+	     * Resource Manager component host.
+	     */
+        FullyDistributedCluster cluster =
+                (FullyDistributedCluster) TestSession.cluster;
+        String confFile = HadoopConfiguration.HADOOP_CONF_YARN;
+	    cluster.getConf(component).setHadoopConfFileProp (
+	            "yarn.admin.acl3",
+	            "gridadmin,hadoop,hadoopqa,philips,foo", confFile);
 	}
 
 	@Test
-	public void resetHadoopConf() {
-		try {
-			// Restart the cluster
-			FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
-			TestSession.cluster.reset();
-			cluster.waitForSafemodeOff();
-			cluster.isFullyUp();
-		}
-		catch (Exception e) {
-			TestSession.logger.error("Exception failure.", e);
-			fail();
-		}
+	public void resetHadoopConf() throws Exception {
+	    // Restart the cluster
+        FullyDistributedCluster cluster =
+                (FullyDistributedCluster) TestSession.cluster;
+	    TestSession.cluster.reset();
+	    cluster.waitForSafemodeOff();
+	    cluster.isFullyUp();
 	}
 
 	@Test
-	public void getHadoopConfProperty() {
-		try {
-			FullyDistributedCluster cluster = (FullyDistributedCluster) TestSession.cluster;
-			String component = HadoopConfiguration.RESOURCE_MANAGER;
-			String propName = "fs.trash.interval";
-			String confFile = HadoopConfiguration.HADOOP_CONF_CORE;
-
-			String propValue = cluster.getConf().getResourceProp(
-					propName, confFile, component);
-			TestSession.logger.info("Prop name '" + propName + "' = " + propValue);
-		}
-		catch (Exception e) {
-			TestSession.logger.error("Exception failure.", e);
-			fail();
-		}
-	}
-	
+	public void getHadoopConfProperty() throws Exception {
+        FullyDistributedCluster cluster =
+                (FullyDistributedCluster) TestSession.cluster;
+	    String propName = "fs.trash.interval";
+	    String propValue = cluster.getConf(component).getResourceProp(propName);
+	    TestSession.logger.info("Prop name '" + propName + "' = " + propValue);
+	}	
 }
