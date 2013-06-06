854c0aa5ed292e630d97447596e457aff4967da6
philips <philips@yahoo-inc.com>
2013-06-06 16:28:00 -0500

    [bug 6336299] - Modify run_hadooptest_remote and run_hadooptest scripts to support testing coretest changes.

--- hadooptest/scripts/run_hadooptest_remote.pl
+++ hadooptest/scripts/run_hadooptest_remote.pl
@@ -23,8 +23,8 @@ The options
         [ -i|--install_only                ] : transport packages to remote host only
         [ -u|--user <remote user>          ] : remote user; e.g. hadoopqa
         [ -r|--remote_host <gateway>       ] : remote gateway host
-        [ -s|--source    <source root dir> ] : source root directory
-        [ -w|--workspace <workspace>       ] : remote workspace. Default remote workspace is 
+        [ -s|--local_ws <source root dir>  ] : local source root directory
+        [ -w|--remote_ws <workspace>       ] : remote workspace. Default remote workspace is 
                                                "/tmp/hadooptest-<REMOTE USER>-<CLUSTER>"
         [ -h|--help                        ] : help
 
@@ -35,6 +35,7 @@ Pass Through options
         [ -t|--test      <test suite(s)    ] : test suite name(s)
         [ -j|--java                        ] : run tests via java directly instead of via maven
         [ -m|--mvn                         ] : run tests via maven instead of via java directly
+        [ -build_coretest                  ] : build coretest
         [ -n|--nopasswd                    ] : no password prompt
 
 Example:
@@ -64,10 +65,10 @@ my $cluster;
 my $remote_host;
 my $install_only = 0;
 my $use_mvn = 1;
-my $local_workspace;
+my ($local_ws_ht, $local_ws_ct);
+my ($remote_ws, $remote_ws_ht, $remote_ws_ct);
 my $username = getpwuid($<);
 my $remote_username = $username;
-my $remote_workspace;
 
 #
 # Command line options processing
@@ -79,8 +80,8 @@ GetOptions(\%options,
     "install_only"         => \$install_only,
     "cluster|c=s"          => \$cluster,
     "remote_host|r=s"      => \$remote_host,
-    "local_workspace|s=s"  => \$local_workspace,
-    "remote_workspace|w=s" => \$remote_workspace,
+    "local_ws|s=s"         => \$local_ws_ht,
+    "remote_ws|w=s"        => \$remote_ws,
     "user|u=s"             => \$remote_username,
     "help|h|?"
     ) or usage(1);
@@ -99,58 +100,76 @@ if (!$remote_host) {
 }
 note("remote gateway host = '$remote_host'");
 
-$remote_workspace = "/tmp/hadooptest-$remote_username-$cluster"
-    unless ($remote_workspace);
+$remote_ws = "/tmp/hadooptest-$remote_username-$cluster"
+    unless ($remote_ws);
+$remote_ws_ht = "$remote_ws/hadooptest";
+$remote_ws_ct = "$remote_ws/coretest";
 
+# CLEAN UP EXISTING WORKSPACE DIRECTORY
 my $command;
 if (($remote_username eq "hadoopqa") && ($username ne "hadoopqa")) {
-    $command = "ssh -t $remote_host \"if [ -d $remote_workspace ]; then sudo /bin/rm -rf $remote_workspace; fi\"";
+    $command = "ssh -t $remote_host \"if [ -d $remote_ws ]; then sudo /bin/rm -rf $remote_ws; fi\"";
 } else {
-    $command = "ssh -t $remote_host \"if [ -d $remote_workspace ]; then /bin/rm -rf $remote_workspace; fi\"";
+    $command = "ssh -t $remote_host \"if [ -d $remote_ws ]; then /bin/rm -rf $remote_ws; fi\"";
 }
 execute($command);
 
-execute("ssh -t $remote_host \"/bin/mkdir -p $remote_workspace\"");
+# CREATE NEW WORKSPACE DIRECTORY
+execute("ssh -t $remote_host \"/bin/mkdir -p $remote_ws\"");
+execute("ssh -t $remote_host \"/bin/mkdir -p $remote_ws_ht\"");
+execute("ssh -t $remote_host \"/bin/mkdir -p $remote_ws_ct\"");
 
 $use_mvn = ( grep( /-j/, @ARGV ) ) ? 0 : 1;
-$local_workspace = "/Users/$username/git/hadooptest/hadooptest" unless ($local_workspace);
-my $local_workspace_target_dir="$local_workspace/target";
+$local_ws_ht = "/Users/$username/git/hadooptest/hadooptest" unless ($local_ws_ht);
+$local_ws_ct = "$local_ws_ht/../../coretest/coretest";
 my $tgz_dir="/tmp";
-my $tgz_file="hadooptest.tgz";
+my $tgz_file_ht="hadooptest.tgz";
+my $tgz_file_ct="coretest.tgz";
 
 note("cluster='$cluster'");
-note("local_workspace='$local_workspace'");
+note("local_ws_ht='$local_ws_ht'");
 note("remote_host='$remote_host'");
-note("remote_workspace='$remote_workspace'");
+note("remote_ws='$remote_ws'");
 note("use_mvn='$use_mvn'");
 note("install_only='$install_only'");
 
+# INSTALL HADOOPTEST FRAMEWORK 
 execute("mvn clean") if ($use_mvn);
-execute("tar -zcf $tgz_dir/$tgz_file --exclude='target' -C $local_workspace .");
-execute("scp $tgz_dir/$tgz_file $remote_host:$remote_workspace");
-execute("ssh -t $remote_host \"/bin/gtar fx $remote_workspace/$tgz_file -C $remote_workspace\"");
-execute("ssh -t $remote_host \"/bin/mkdir -p $remote_workspace/target\"");
-execute("scp $local_workspace_target_dir/*.jar $remote_host:$remote_workspace/target");
-execute("ssh -t $remote_host \"sudo chown -R hadoopqa $remote_workspace;\"") if ($remote_username eq "hadoopqa");
+execute("tar -zcf $tgz_dir/$tgz_file_ht --exclude='target' -C $local_ws_ht .");
+execute("scp $tgz_dir/$tgz_file_ht $remote_host:$remote_ws_ht");
+execute("ssh -t $remote_host \"/bin/gtar fx $remote_ws_ht/$tgz_file_ht -C $remote_ws_ht\"");
+execute("ssh -t $remote_host \"/bin/mkdir -p $remote_ws_ht/target\"");
+execute("scp $local_ws_ht/target/*.jar $remote_host:$remote_ws_ht/target");
+execute("ssh -t $remote_host \"sudo chown -R hadoopqa $remote_ws_ht;\"") if ($remote_username eq "hadoopqa");
 
-my $common_args = "--cluster $cluster --workspace $remote_workspace ".join(" ", @ARGV);
+# INSTALL CORETEST FRAMEWORK 
+execute("cd $local_ws_ct; mvn clean") if ($use_mvn);
+execute("tar -zcf $tgz_dir/$tgz_file_ct --exclude='target' -C $local_ws_ct .");
+execute("scp $tgz_dir/$tgz_file_ct $remote_host:$remote_ws_ct");
+execute("ssh -t $remote_host \"/bin/gtar fx $remote_ws_ct/$tgz_file_ct -C $remote_ws_ct\"");
+execute("ssh -t $remote_host \"/bin/mkdir -p $remote_ws_ct/target\"");
+execute("scp $local_ws_ct/target_dir/*.jar $remote_host:$remote_ws_ct/target");
+execute("ssh -t $remote_host \"sudo chown -R hadoopqa $remote_ws_ct;\"") if ($remote_username eq "hadoopqa");
+
+# EXECUTE TESTS
+my $common_args = "--cluster $cluster --workspace $remote_ws_ht ".join(" ", @ARGV);
 unless ($install_only) {
     if ($use_mvn) {
-        execute("rm -rf $local_workspace_target_dir/surefire-reports/");
-        execute("ssh -t $remote_host \"cd $remote_workspace; $remote_workspace/scripts/run_hadooptest $common_args\"");
+        execute("rm -rf $local_ws_ct/target/surefire-reports/");
+        execute("ssh -t $remote_host \"cd $remote_ws_ht; $remote_ws_ht/scripts/run_hadooptest $common_args\"");
 
         # WE NEED TO COPY THE TEST RESULTS FROM THE GATEWAY BACK TO THE BUILD HOST
-        execute("scp -r $remote_host:$remote_workspace/target/surefire-reports $local_workspace_target_dir/");
+        execute("scp -r $remote_host:$remote_ws_ht/target/surefire-reports $local_ws_ht/target/");
 
-        # execute("scp -r $remote_host:$remote_workspace/target/clover $local_workspace_target_dir/")
+        # execute("scp -r $remote_host:$remote_ws_ht/target/clover $local_ws_ht/target/")
         #   if (( "-p" ~~ @ARGV ) || ( "-profile" ~~ @ARGV ) || ( "--profile" ~~ @ARGV ));
-        execute("scp -r $remote_host:$remote_workspace/target/clover $local_workspace_target_dir/")
+        execute("scp -r $remote_host:$remote_ws_ht/target/clover $local_ws_ht/target/")
             if ( "clover" ~~ @ARGV );
 
-        execute("scp -r $remote_host:$remote_workspace/target/site $local_workspace_target_dir/")
+        execute("scp -r $remote_host:$remote_ws_ht/target/site $local_ws_ht/target/")
             if ( "jacoco" ~~ @ARGV );
     } else {
-        execute("ssh -t $remote_host \"$remote_workspace/scripts/run_hadooptest $common_args\"");
+        execute("ssh -t $remote_host \"$remote_ws_ht/scripts/run_hadooptest $common_args\"");
     }
 }
 
