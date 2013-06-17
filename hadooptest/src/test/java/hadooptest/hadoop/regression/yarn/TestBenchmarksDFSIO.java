bb028beb35625676f4888d084702e5e2b6bd0522
philips <philips@yahoo-inc.com>
2013-06-17 13:43:14 -0500

    [bug 6197018] - Port benchmark test suites to Java framework.

--- hadooptest/src/test/java/hadooptest/hadoop/regression/yarn/TestBenchmarksDFSIO.java
+++ hadooptest/src/test/java/hadooptest/hadoop/regression/yarn/TestBenchmarksDFSIO.java
@@ -96,7 +96,7 @@ public class TestBenchmarksDFSIO extends TestSession {
         FsShell fsShell = TestSession.cluster.getFsShell();
         DFS dfs = new DFS();
         String testDir = dfs.getBaseUrl() + "/user/" +
-            System.getProperty("user.name") + "/benchmarks";
+            System.getProperty("user.name") + "/benchmarks_dfsio";
         if (fs.exists(new Path(testDir))) {
             TestSession.logger.info("Delete existing test directory: " +
                 testDir);
@@ -135,7 +135,7 @@ public class TestBenchmarksDFSIO extends TestSession {
         DFS dfs = new DFS();
         String outputDir = 
                 dfs.getBaseUrl() + "/user/" + System.getProperty("user.name") + 
-                "/benchmarks/" + percentage;
+                "/benchmarks_dfsio/" + percentage;
         jobArgs.add("-D");
         jobArgs.add("test.build.data=" + outputDir);
         
