67e2ee316eb063ac8b8bd686a745d79bd5106899
Rick Bernotas <rbernota@yahoo-inc.com>
2013-07-08 14:09:18 -0500

    [bug 6393491] - Removed all classes and references to Standalone and Mini- clusters from the framework.  We haven't ever used these, they don't work, and have now started to cause problems with API references for testing against Hadoop 2.4.

--- hadooptest/src/main/java/hadooptest/cluster/hadoop/mini/mapreduce/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * YAHOO!
- */
-
-/**
- * Functionality specific to operating a Hadoop MapReduce minicluster.
- */
-
-package hadooptest.cluster.hadoop.mini.mapreduce;
--- hadooptest/src/main/java/hadooptest/config/hadoop/minicluster/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * YAHOO!
- */
-
-/**
- * Functionality specific to configuring a mini cluster Hadoop cluster.
- */
-
-package hadooptest.config.hadoop.minicluster;
