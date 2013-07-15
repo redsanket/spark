469c4382c14777d2e4ad44875eb34014254a1943
jingrui <jingrui@yahoo-inc.com>
2013-07-15 16:43:16 -0500

    Modify RandomWriterAPI, add TestDurability_TestRandomWriterAPI,TestDurability_TestFillinHDFS,TestDurability_TestHdfsUsage

--- hadooptest/src/test/java/hadooptest/hadoop/durability/TestDurability_TestFillingHdfs.java
+++ hadooptest/src/test/java/hadooptest/hadoop/durability/TestDurability_TestFillingHdfs.java
@@ -8,19 +8,22 @@ import hadooptest.cluster.hadoop.HadoopCluster;
 import hadooptest.cluster.hadoop.fullydistributed.FullyDistributedCluster;
 
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsStatus;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.yarn.api.records.QueueInfo;
 import org.apache.hadoop.yarn.client.YarnClientImpl;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestDurability_TestRandomWriterAPI extends TestSession {
+public class TestDurability_TestFillingHdfs extends TestSession {
 	
 	@BeforeClass
     public static void startTestSession() throws Exception{
@@ -68,33 +71,39 @@ public class TestDurability_TestRandomWriterAPI extends TestSession {
     }
     
     @Test 
-    public void TestRandomWriterAPI(){
-    	//<out-dir> <level> <jobNum><MAPS_PER_HOST_VAL><BYTES_PER_MAP_VAL>
-		String TargetLevel = System.getProperty("TargetLevel");
-		if(TargetLevel == null) TargetLevel = "";
-		
-		String JobNum = System.getProperty("JobNum");
-		if(JobNum == null) JobNum = "";
-		
-		String MAPS_PER_HOST = System.getProperty("MAPS_PER_HOST");
-		if(MAPS_PER_HOST == null) MAPS_PER_HOST = "";
-		
-		String BYTES_PER_MAP = System.getProperty("BYTES_PER_MAP");
-		if(BYTES_PER_MAP == null) BYTES_PER_MAP = "";
-
+    public void testFillDFS() throws Exception{
+    	double TargetLevel = Double.parseDouble(System.getProperty("TargetLevel"));
     	try {
+    		DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd___HH_mm_ss___");
 			Configuration conf = TestSession.cluster.getConf();
 			TestSession.cluster.setSecurityAPI("keytab-hadoopqa", "user-hadoopqa");
-
-			DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd___HH_mm_ss");
-			Date date = new Date();
-	    	String outdir = new DFS().getBaseUrl() + "/user/" + System.getProperty("user.name") + "/LargeFile/"+dateFormat.format(date).toString();
-	    	TestSession.logger.info("outdir = "+outdir);
-			String[] args = {outdir,TargetLevel,JobNum,MAPS_PER_HOST,BYTES_PER_MAP };
+			
+			// get status of hdfs
+			FileSystem fs = TestSession.cluster.getFS();
+	    	FsStatus status  =fs.getStatus();
+	    	long capacity = status.getCapacity();
+	    	long remain = status.getRemaining();
+	    	long used = status.getUsed();
+	    	double DFSUsedLevel = (double)used/(double)capacity;
+	    	logger.info("getCapacity ="+capacity);
+	    	logger.info("getRemaining ="+remain);
+	    	logger.info("getUsed ="+used);
+	    	logger.info("RemainingLevel = "+((double)remain/(double)capacity));
+	    	logger.info("DFSUsedLevel   = "+ DFSUsedLevel);
+	    	logger.info("TargetLevel = "+TargetLevel);
+	    	
+	    	DecimalFormat twoDForm = new DecimalFormat("###.###");
+			while(DFSUsedLevel < TargetLevel){
+	    		Date date = new Date();
+	    		String outdir = new DFS().getBaseUrl() + "/user/" + System.getProperty("user.name") + "/LargeFile/"+dateFormat.format(date).toString();
+	    		TestSession.logger.info("outdir = "+outdir);
+				String[] args = {outdir};
 				
-			int rc = ToolRunner.run(conf, new RandomWriterAPI(), args);
-			if (rc != 0) 
-				TestSession.logger.error("Job failed!!!");
+				logger.info("======= DFS used %"+ Double.valueOf(twoDForm.format(DFSUsedLevel*100))+", Filling it to %"+TargetLevel*100+" =======");
+				int rc = ToolRunner.run(conf, new RandomWriterAPI(), args);
+				if (rc != 0) 
+					TestSession.logger.error("Job failed!!!");
+			}
 		}
 		catch (Exception e) {
 			TestSession.logger.error("Exception failure.", e);
