b02a23e65ebd20e0aa8d55647384b7444b913fba
philips <philips@yahoo-inc.com>
2013-09-26 10:19:35 -0500

    [bug 6560022] Change HTF main branch to support Hadoop 2.x.

--- hadooptest/src/test/java/hadooptest/hadoop/stress/floodingHDFS/TestRandomWriterAPI.java
+++ hadooptest/src/test/java/hadooptest/hadoop/stress/floodingHDFS/TestRandomWriterAPI.java
@@ -17,7 +17,8 @@ import java.util.List;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.yarn.api.records.QueueInfo;
-import org.apache.hadoop.yarn.client.YarnClientImpl;
+//import org.apache.hadoop.yarn.client.YarnClientImpl; // H0.23
+import org.apache.hadoop.yarn.client.api.impl.YarnClientImpl; // H2.x
 import org.junit.BeforeClass;
 import org.junit.Test;
 
