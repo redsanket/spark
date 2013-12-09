568bd75afd86390ff54def0cad491e4d3c04671f
Amit Tiwari <tiwari@yahoo-inc.com>
2013-12-09 10:52:45 -0600

    Adding factory files, forYarnClientImpl

---
+++ hadooptest/src/test/java/hadooptest/dfs/regression/TestYarnClientImplFactory.java
@@ -0,0 +1,40 @@
+package hadooptest.dfs.regression;
+
+import java.lang.reflect.InvocationTargetException;
+
+import hadooptest.TestSession;
+import hadooptest.automation.factories.yarnClientImpl.IYarnClientFunctionality;
+import hadooptest.automation.factories.yarnClientImpl.YarnClientImplFactory;
+import hadooptest.monitoring.Monitorable;
+
+import org.apache.hadoop.yarn.api.records.QueueInfo;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestYarnClientImplFactory extends TestSession {
+	Logger logger = Logger.getLogger(TestYarnClientImplFactory.class);
+
+	@BeforeClass
+	public static void startTestSession() throws Exception {
+		TestSession.start();
+	}
+
+	@Test
+	@Monitorable
+	@Deprecated
+	public void testFactory() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException, SecurityException,
+			NoSuchMethodException, IllegalArgumentException,
+			InvocationTargetException {
+		IYarnClientFunctionality yarnClientImpl;
+		yarnClientImpl = YarnClientImplFactory.get();
+
+		yarnClientImpl.init(TestSession.getCluster().getConf());
+		yarnClientImpl.start();
+		for (QueueInfo aQueueInfo : yarnClientImpl.getAllQueues()) {
+			logger.info("Queue=" + aQueueInfo.getQueueName());
+		}
+
+	}
+}
