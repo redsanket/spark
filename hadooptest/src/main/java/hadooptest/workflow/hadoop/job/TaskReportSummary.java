66985cb793eebe3935c81c991894c3bd846d3364
philips <philips@yahoo-inc.com>
2013-12-17 15:15:58 -0600

    [bug 6705171] Enhance HTF to easily access job and task reports

--- hadooptest/src/main/java/hadooptest/workflow/hadoop/job/TaskReportSummary.java
+++ hadooptest/src/main/java/hadooptest/workflow/hadoop/job/TaskReportSummary.java
@@ -5,94 +5,118 @@
 package hadooptest.workflow.hadoop.job;
 
 import hadooptest.TestSession;
-import hadooptest.config.hadoop.HadoopConfiguration;
 
 import java.io.IOException;
 import java.util.HashMap;
 
-// import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobID;
-import org.apache.hadoop.mapred.JobStatus;
 import org.apache.hadoop.mapred.TIPStatus;
-import org.apache.hadoop.mapred.TaskReport;
 
 /**
- * A class which should represent the base capability of any job
- * submitted to a cluster.
+ * Container class for task report summary for a set of jobs.
  */
 public class TaskReportSummary {
     
     // Track map and reduce tasks total, and sub total for each task type
-    static int totalMapTasks = 0;
-    static int totalReduceTasks = 0;        
-    static HashMap<TIPStatus, Integer> mapStatusCounter =
+    private int totalMapTasks = 0;
+    private int totalReduceTasks = 0;        
+    private HashMap<TIPStatus, Integer> mapStatusCounter =
             new HashMap<TIPStatus, Integer>();
-    static HashMap<TIPStatus, Integer> reduceStatusCounter =
+    private HashMap<TIPStatus, Integer> reduceStatusCounter =
             new HashMap<TIPStatus, Integer>();
 
     public TaskReportSummary() {
-        this.totalMapTasks = 0;
-        this.totalReduceTasks = 0;            
     }
         
+    /**
+     * Get the total number of map tasks
+     */
     public int getMapTasks() {
         return totalMapTasks;
     }
     
+    /**
+     * Get the total number of reduce tasks
+     */
     public int getReduceTasks() {
         return totalReduceTasks;
     }
         
+    /**
+     * Get the total number of map tasks for a particular task status
+     */
     public int getMapTasks(TIPStatus status) {
         return mapStatusCounter.get(status);
     }
     
+    /**
+     * Get the total number of reduce tasks for a particular task status
+     */
     public int getReduceTasks(TIPStatus status) {
         return reduceStatusCounter.get(status);
     }
 
+    /**
+     * Get the total number of non-complete map tasks
+     */
     public int getNonCompleteMapTasks() {
         return (this.getMapTasks() - 
                 this.getMapTasks(TIPStatus.COMPLETE));
     }    
     
+    /**
+     * Get the total number of non-complete reduce tasks
+     */
     public int getNonCompleteReduceTasks() {
         return (this.getReduceTasks() - 
                 this.getReduceTasks(TIPStatus.COMPLETE));
     }    
     
+    /**
+     * Increment the number of map tasks
+     */
     public void incrementMapTasks(int numTasks) {
         totalMapTasks = totalMapTasks + numTasks;
     }
 
+    /**
+     * Increment the number of reduce tasks
+     */
     public void incrementReduceTasks(int numTasks) {
         totalReduceTasks = totalReduceTasks + numTasks;
     }
         
+    /**
+     * Set the map task status counter
+     */
     public HashMap<TIPStatus, Integer> getMapStatusCounter() {
         return mapStatusCounter;
     }
 
+    /**
+     * Get the reduce task status counter
+     */
     public HashMap<TIPStatus, Integer> getReduceStatusCounter() {
         return reduceStatusCounter;
     }
 
+    /**
+     * Set the map task status counter
+     */
     public void setMapStatusCounter(HashMap<TIPStatus, Integer> counter) {
         this.mapStatusCounter = counter;
     }
 
+    /**
+     * Set the reduce task status counter
+     */
     public void setReduceStatusCounter(HashMap<TIPStatus, Integer> counter) {
         this.reduceStatusCounter = counter;
     }
     
+    /**
+     * Print the task report summary.
+     */
     public void printSummary() {
-        // TestSession.logger.info("---> Tasks Summary: ");
-        // TestSession.logger.info("********************************************");
-        /*
-        int numJobs = jobsStatus.length;
-        TestSession.logger.info("Total Number of jobs = " + numJobs);
-        */
-        
         TestSession.logger.info("Total Number of map tasks = " + this.getMapTasks());
         TestSession.logger.info("Total Number of reduce tasks = " + this.getReduceTasks());
 
