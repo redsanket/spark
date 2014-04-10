c32fc8d89710a6f8e2bb1329274e7faa4bd55c37
Derek Dagit <derekd@yahoo-inc.com>
2014-04-10 16:35:25 +0000

    tests storm logviewer paging and download

--- hadooptest/src/test/java/hadooptest/storm/TestWordCountTopology.java
+++ hadooptest/src/test/java/hadooptest/storm/TestWordCountTopology.java
@@ -1,20 +1,44 @@
 package hadooptest.storm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import hadooptest.SerialTests;
 import hadooptest.TestSessionStorm;
 import hadooptest.Util;
+import hadooptest.cluster.storm.ModifiableStormCluster;
 import hadooptest.workflow.storm.topology.spout.FixedBatchSpout;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.simple.JSONValue;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.w3c.dom.Document;
+import org.w3c.tidy.Tidy;
 
 import storm.trident.TridentState;
 import storm.trident.TridentTopology;
@@ -28,10 +52,11 @@ import storm.trident.testing.MemoryMapState;
 import storm.trident.tuple.TridentTuple;
 import backtype.storm.Config;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.generated.TopologyInfo;
 import backtype.storm.generated.TopologySummary;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-
+import backtype.storm.utils.Utils;
 
 @Category(SerialTests.class)
 public class TestWordCountTopology extends TestSessionStorm {
@@ -55,8 +80,171 @@ public class TestWordCountTopology extends TestSessionStorm {
         return getTS(name).get_uptime_secs();
     }
 
+    private String downloadFile(URL url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
+        InputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = (InputStream) url.openConnection().getInputStream();
+            byte[] buf = new byte[1024];
+            int read = -1;
+            File tmpFile = File.createTempFile(this.getClass().getName(), null);
+            tmpFile.deleteOnExit();
+            out = new FileOutputStream(tmpFile);
+            try {
+                while ((read = in.read(buf)) != -1) {
+                    out.write(buf, 0, read);
+                }
+            } catch (IOException e) {
+                if (!"Premature EOF".equals(e.getMessage())) {
+                    throw e;
+                }
+                // Do nothing.
+                // We can remove this when the logviewer properly closes
+                // connections.
+            }
+            return tmpFile.getCanonicalPath();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
 
     @Test
+    public void LogviewerPagingTest() throws Exception {
+        assumeTrue(cluster instanceof ModifiableStormCluster);
+        StormTopology topology = buildTopology();
+
+        String topoName = "logviewer-paging-test";
+        String outputLoc = new File("/tmp", topoName).getCanonicalPath();
+
+        Config config = new Config();
+        config.put("test.output.location", outputLoc);
+        config.setDebug(true);
+        config.setNumWorkers(1);
+        config.put(Config.NIMBUS_TASK_TIMEOUT_SECS, 200);
+        // TODO turn this into a utility that has a conf setting
+        File jar = new File(
+                conf.getProperty("WORKSPACE")
+                        + "/target/hadooptest-ci-1.0-SNAPSHOT-test-jar-with-dependencies.jar");
+        cluster.submitTopology(jar, topoName, config, topology);
+        try {
+            int uptime = 20;
+            int cur_uptime = 0;
+            cur_uptime = getUptime(topoName);
+            if (cur_uptime < uptime) {
+                Util.sleep(uptime - cur_uptime);
+            }
+            cur_uptime = getUptime(topoName);
+            while (cur_uptime < uptime) {
+                Util.sleep(1);
+                cur_uptime = getUptime(topoName);
+            }
+
+            String topoId = getTS(topoName).get_id();
+
+            // Worker Host
+            TopologyInfo ti = cluster.getTopologyInfo(topoId);
+            String host = ti.get_executors().get(0).get_host();
+            int workerPort = ti.get_executors().get(0).get_port();
+
+            // Logviewer port on worker host
+            String jsonStormConf = cluster.getNimbusConf();
+            @SuppressWarnings("unchecked")
+            Map<String, Object> stormConf =
+                    (Map<String, Object>) JSONValue.parse(jsonStormConf);
+            Integer logviewerPort = Utils.getInt(stormConf
+                    .get(Config.LOGVIEWER_PORT));
+
+            String logPath = downloadFromLogviewer(topoId, host, logviewerPort,
+                    workerPort);
+            final String expectedRegex =
+                    ".*TRANSFERR?ING.*an apple a day keeps the doctor away.*";
+            assertTrue("Topology appears to be up-and-running.",
+                    fileContainsRegex(logPath, expectedRegex));
+
+            final int startByteNum = 42;
+            final int byteLength = 1947;
+            String logBytesString = readBytesFromFile(logPath, startByteNum,
+                    byteLength);
+            String expected = StringEscapeUtils.escapeXml(logBytesString);
+            String actual = getLogviewerPageContent(topoId, host,
+                    logviewerPort, workerPort, startByteNum, byteLength);
+            assertEquals("Log page returns correct bytes", expected, actual);
+        } finally {
+            cluster.killTopology(topoName);
+        }
+    }
+
+    private String getLogviewerPageContent(String topoId, String host,
+            int logviewerPort, int workerPort, final int startByteNum,
+            final int byteLength) throws MalformedURLException, IOException,
+            FileNotFoundException, XPathExpressionException {
+        URL logPageUrl = new URL("http", host, logviewerPort, "/log?file="
+                + topoId + "-worker-" + workerPort + ".log&" + "start="
+                + startByteNum + "&length=" + byteLength);
+        String logPagePath = downloadFile(logPageUrl);
+        Tidy tidy = new Tidy();
+        tidy.setQuiet(true);
+        Document doc = tidy.parseDOM(new FileInputStream(logPagePath),
+                new NullOutputStream());
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        String actual = (String) xpath.evaluate(
+                "//html/body/pre[contains(@id, 'logContent')]/text()", doc,
+                XPathConstants.STRING);
+        return actual;
+    }
+
+    private String downloadFromLogviewer(String topoId, String host,
+            int logviewerPort, int workerPort) throws MalformedURLException,
+            IOException {
+        URL logDownloadUrl = new URL("http", host, logviewerPort, "/download/"
+                + topoId + "-worker-" + workerPort + ".log");
+        String result = null;
+        result = this.downloadFile(logDownloadUrl);
+        return result;
+    }
+
+    private String readBytesFromFile(String filePath, final int startByteNum,
+            final int byteLength) throws FileNotFoundException, IOException {
+        File logFile = new File(filePath);
+        assertTrue("Log file is long enough for testing.",
+                logFile.length() >= byteLength);
+        byte[] buf = new byte[byteLength];
+        RandomAccessFile logPageFile = null;
+        try {
+            logPageFile = new RandomAccessFile(filePath, "r");
+            logPageFile.seek(startByteNum);
+            logPageFile.readFully(buf, 0, byteLength);
+        } finally {
+            logPageFile.close();
+        }
+        return new String(buf);
+    }
+
+    private boolean fileContainsRegex(String logPath, final String expectedRegex)
+            throws IOException {
+        String line = "";
+        LineIterator it = null;
+        try {
+            for (it = FileUtils.lineIterator(new File(logPath)); it.hasNext();
+                    line = it.next()) {
+                if (line.matches(expectedRegex)) {
+                    return true;
+                }
+            }
+            return false;
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
+    }
+
+    @Test(timeout=300000)
     public void WordCountTopologyTest() throws Exception{
         StormTopology topology = buildTopology();
 
@@ -104,9 +292,8 @@ public class TestWordCountTopology extends TestSessionStorm {
                 //todo.  Abstarcted drpc execute call.  Need to add cluster interface so that local mode and non-local mode are the same
                 String drpcResult = cluster.DRPCExecute( "words", key );
                 logger.info("The value for " + key + " is " + drpcResult );
-		System.out.println("The value for " + key + " is " + drpcResult);
                 Matcher matcher = pattern.matcher(drpcResult);
-		int thisCount = -1;
+                int thisCount = -1;
                 if (matcher.find()) {
                     logger.info("Matched " + matcher.group(0));
                     thisCount = Integer.parseInt(matcher.group(0));
@@ -120,6 +307,7 @@ public class TestWordCountTopology extends TestSessionStorm {
         }
     }    
 
+    @SuppressWarnings("serial")
     public static class Split extends BaseFunction {
         @Override
         public void execute(TridentTuple tuple, TridentCollector collector) {
@@ -132,6 +320,7 @@ public class TestWordCountTopology extends TestSessionStorm {
 
         
     public static StormTopology buildTopology() {
+        @SuppressWarnings("unchecked")
         FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3,
                 new Values ("the cow jumped over the moon"),
                 new Values ("an apple a day keeps the doctor away"),
