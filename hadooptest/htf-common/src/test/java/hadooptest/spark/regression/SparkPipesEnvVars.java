7bdf18b837e4d75d455a6b01ce790639f4a2eb6d
Thomas Graves <tgraves@apache.org>
2014-06-18 12:36:57 -0500

    update to use spark 1.0 interfaces

--- hadooptest/htf-common/src/test/java/hadooptest/spark/regression/SparkPipesEnvVars.java
+++ hadooptest/htf-common/src/test/java/hadooptest/spark/regression/SparkPipesEnvVars.java
@@ -1,19 +1,20 @@
 package hadooptest.spark.regression;
 
 import java.util.List;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkPipesEnvVars {
 
   public static void main(String[] args) throws Exception {
-    if (args.length < 2) {
-      System.err.println("Usage: SparkPipesEnvVars <master> <inputfile>");
+    if (args.length < 1) {
+      System.err.println("Usage: SparkPipesEnvVars inputfile>");
       System.exit(1);
     }
 
-    JavaSparkContext ctx = new JavaSparkContext(args[0], "SparkPipesEnvVars",
-        System.getenv("SPARK_HOME"), JavaSparkContext.jarOfClass(SparkPipesEnvVars.class));
+    SparkConf sparkConf = new SparkConf().setAppName("SparkPipesEnvVars");
+    JavaSparkContext ctx = new JavaSparkContext(sparkConf);
     JavaRDD<String> lines = ctx.textFile(args[1], 1);
 
     List<String> res1 = lines.pipe("printenv map_input_file").collect();
