f7921ca175fbce75cc6d4a0cdfb8b763b9646761
Raj <rchopde@yahoo-inc.com>
2014-07-29 15:34:12 -0500

    Remove PATH

--- deploySupport/258-installsteps-installTez.sh
+++ deploySupport/258-installsteps-installTez.sh
@@ -23,7 +23,6 @@ cmd="export TEZ_CONF_DIR=/home/gs/conf/tez/ ; \
      export HADOOP_HOME=$GSHOME/hadoop/current ; \
      export HADOOP_CLASSPATH="$TEZ_CONF_DIR:$TEZ_HOME/*:$TEZ_HOME/lib/*" ; \
      export JAVA_HOME=$GSHOME/java/jdk ; \
-     export PATH=$PATH:/home/gs/hadoop/current/bin ; \
      kinit -k -t /homes/hdfsqa/hdfsqa.dev.headless.keytab hdfsqa ; \
      echo installing Tez into hdfs ; \
      echo `pwd` ; \
