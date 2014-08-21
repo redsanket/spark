ef50d762e73efafcead7724638d6444a95495ddd
Amit Tiwari <tiwari@yahoo-inc.com>
2014-08-21 17:04:37 -0500

    this file got left off

---
+++ hadooptest/htf-common/src/test/java/hadooptest/tez/pig/localmode/TestPigTutorialScript2.java
@@ -0,0 +1,29 @@
+package hadooptest.tez.pig.localmode;
+
+
+import hadooptest.tez.HtfPigBaseClass;
+import hadooptest.tez.HtfTezUtils;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPigTutorialScript2 extends HtfPigBaseClass {
+	private static String SCRIPT_NAME = "script2-local.pig";
+	private static String OUTPUT_NAME = "/tmp/pigout/" + SCRIPT_NAME.replace(".pig", "");
+
+	@Test
+	public void testWithTez() throws Exception {
+		int returnCode = runPigScriptLocally( SCRIPT_NAME,
+				OUTPUT_NAME);
+		Assert.assertTrue(returnCode == 0);
+	}
+
+	@After
+	public void deleteOutputDirInHdfs() throws Exception {
+		HtfTezUtils.delete(new File(OUTPUT_NAME));
+		
+	}
+}
