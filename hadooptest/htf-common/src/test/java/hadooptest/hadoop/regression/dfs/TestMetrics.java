1a46b60b20b8857610a7fc1c2f20ce36e08d51d3
Rick Bernotas <rbernota@yahoo-inc.com>
2014-08-27 16:04:35 -0500

    Removed Parameterized test execution from DFS TestMetrics.

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestMetrics.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestMetrics.java
@@ -13,14 +13,10 @@ import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
 @Category(SerialTests.class)
 public class TestMetrics extends DfsTestsBaseClass {
 	String protocol;
@@ -53,8 +49,27 @@ public class TestMetrics extends DfsTestsBaseClass {
 				{ HadooptestConstants.Schema.HDFS }, });
 	}
 
-	@Before
-	public void beforeEachTest() throws Exception {
+	@Test public void test_FilesInGetListingOps1_webhdfs() throws Exception { test_FilesInGetListingOps1(HadooptestConstants.Schema.WEBHDFS); }
+    @Test public void test_FilesInGetListingOps1_none() throws Exception { test_FilesInGetListingOps1(""); }
+    @Test public void test_FilesInGetListingOps1_hdfs() throws Exception { test_FilesInGetListingOps1(HadooptestConstants.Schema.HDFS); }
+    
+	@Test public void test_FilesInGetListingOps2_webhdfs() throws Exception { test_FilesInGetListingOps2(HadooptestConstants.Schema.WEBHDFS); }
+    @Test public void test_FilesInGetListingOps2_none() throws Exception { test_FilesInGetListingOps2(""); }
+    @Test public void test_FilesInGetListingOps2_hdfs() throws Exception { test_FilesInGetListingOps2(HadooptestConstants.Schema.HDFS); }
+    
+	@Test public void test_FilesInGetListingOps4_webhdfs() throws Exception { test_FilesInGetListingOps4(HadooptestConstants.Schema.WEBHDFS); }
+    @Test public void test_FilesInGetListingOps4_none() throws Exception { test_FilesInGetListingOps4(""); }
+    @Test public void test_FilesInGetListingOps4_hdfs() throws Exception { test_FilesInGetListingOps4(HadooptestConstants.Schema.HDFS); }
+    
+    @Test public void test_FilesInGetListingOps5_webhdfs() throws Exception { test_FilesInGetListingOps5(HadooptestConstants.Schema.WEBHDFS); }
+    @Test public void test_FilesInGetListingOps5_none() throws Exception { test_FilesInGetListingOps5(""); }
+    @Test public void test_FilesInGetListingOps5_hdfs() throws Exception { test_FilesInGetListingOps5(HadooptestConstants.Schema.HDFS); }
+    
+    @Test public void test_FilesInGetListingOps6_webhdfs() throws Exception { test_FilesInGetListingOps6(HadooptestConstants.Schema.WEBHDFS); }
+    @Test public void test_FilesInGetListingOps6_none() throws Exception { test_FilesInGetListingOps6(""); }
+    @Test public void test_FilesInGetListingOps6_hdfs() throws Exception { test_FilesInGetListingOps6(HadooptestConstants.Schema.HDFS); }
+    
+	private void setupTest(String protocol) throws Exception {
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -323,8 +338,9 @@ public class TestMetrics extends DfsTestsBaseClass {
 
 	}
 
-	@Test
-	public void test_FilesInGetListingOps1() throws Exception {
+	private void test_FilesInGetListingOps1(String protocol) throws Exception {
+	    setupTest(protocol);
+	    
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -347,8 +363,9 @@ public class TestMetrics extends DfsTestsBaseClass {
 
 	}
 
-	@Test
-	public void test_FilesInGetListingOps2() throws Exception {
+	private void test_FilesInGetListingOps2(String protocol) throws Exception {
+        setupTest(protocol);
+        
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -372,8 +389,9 @@ public class TestMetrics extends DfsTestsBaseClass {
 
 	}
 
-	@Test
-	public void test_FilesInGetListingOps4() throws Exception {
+	private void test_FilesInGetListingOps4(String protocol) throws Exception {
+        setupTest(protocol);
+        
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -416,8 +434,9 @@ public class TestMetrics extends DfsTestsBaseClass {
 
 	}
 
-	@Test
-	public void test_FilesInGetListingOps5() throws Exception {
+	private void test_FilesInGetListingOps5(String protocol) throws Exception {
+        setupTest(protocol);
+        
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -473,8 +492,9 @@ public class TestMetrics extends DfsTestsBaseClass {
 
 	}
 
-	@Test
-	public void test_FilesInGetListingOps6() throws Exception {
+	private void test_FilesInGetListingOps6(String protocol) throws Exception {
+        setupTest(protocol);
+        
 		DfsCliCommands dfsCliCommands = new DfsCliCommands();
 		GenericCliResponseBO genericCliResponse;
 
@@ -523,12 +543,6 @@ public class TestMetrics extends DfsTestsBaseClass {
 
         Util.sleep(10);
 		
-	}
-
-	@Override
-	@After
-	public void logTaskReportSummary() {
-		// Override to hide the Test Session logs
 	}
 
 }
