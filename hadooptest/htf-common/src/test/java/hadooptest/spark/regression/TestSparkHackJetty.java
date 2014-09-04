1f35b32ab53e306b775078cb274a95a8872d2354
Thomas Graves <tgraves@modesroads.corp.ne1.yahoo.com>
2014-09-04 21:10:07 +0000

    Add tests for security testing Jetty

---
+++ hadooptest/htf-common/src/test/java/hadooptest/spark/regression/TestSparkHackJetty.java
@@ -0,0 +1,143 @@
+package hadooptest.spark.regression;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import hadooptest.TestSession;
+import hadooptest.workflow.spark.app.AppMaster;
+import hadooptest.workflow.spark.app.SparkRunClass;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.fs.Path;
+
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import hadooptest.Util;
+
+public class TestSparkHackJetty extends TestSession {
+
+        private static String localJar = null;
+
+        @BeforeClass
+        public static void startTestSession() throws Exception {
+                TestSession.start();
+                setupTestDir();
+        }
+
+        public static void setupTestDir() throws Exception {
+
+                TestSession.cluster.getFS();
+                localJar = Util.getResourceFullPath("../htf-common/target/htf-common-1.0-SNAPSHOT-tests.jar");
+        }
+
+        // requires that authentication is on
+	@Test
+	public void runSparkHackJettyBroadcast() throws Exception {
+		SparkRunClass appUserDefault = new SparkRunClass();
+
+		appUserDefault.setWorkerMemory("2g");
+		appUserDefault.setNumWorkers(3);
+		appUserDefault.setWorkerCores(1);
+		appUserDefault.setClassName("hadooptest.spark.regression.SparkHackJetty");
+		appUserDefault.setJarName(localJar);
+		appUserDefault.setAppName("runSparkHackJettyBroadcast");
+		appUserDefault.setShouldPassName(true);
+                String[] argsArray = {"true", "false"};
+                appUserDefault.setArgs(argsArray);
+
+		appUserDefault.start();
+
+		assertTrue("App (default user) was not assigned an ID within 30 seconds.", 
+			appUserDefault.waitForID(30));
+		assertTrue("App ID for sleep app (default user) is invalid.", 
+			appUserDefault.verifyID());
+
+		int waitTime = 30;
+		assertTrue("Job (default user) did not succeed.",
+			appUserDefault.waitForSuccess(waitTime));
+	}
+	@Test
+	public void runSparkHackJettyBroadcastAuthOff() throws Exception {
+		SparkRunClass appUserDefault = new SparkRunClass();
+
+		appUserDefault.setWorkerMemory("2g");
+		appUserDefault.setNumWorkers(3);
+		appUserDefault.setWorkerCores(1);
+		appUserDefault.setClassName("hadooptest.spark.regression.SparkHackJetty");
+		appUserDefault.setJarName(localJar);
+		appUserDefault.setAppName("runSparkHackJettyBroadcastAuthOff");
+		appUserDefault.setShouldPassName(true);
+                String[] argsArray = {"true", "true"};
+                appUserDefault.setArgs(argsArray);
+
+		appUserDefault.start();
+
+		assertTrue("App (default user) was not assigned an ID within 30 seconds.", 
+			appUserDefault.waitForID(30));
+		assertTrue("App ID for sleep app (default user) is invalid.", 
+			appUserDefault.verifyID());
+
+		int waitTime = 30;
+		assertTrue("Job (default user) did not succeed.",
+			appUserDefault.waitForSuccess(waitTime));
+	}
+	
+	@Test
+	public void runSparkHackJettyFileServer() throws Exception {
+		SparkRunClass appUserDefault = new SparkRunClass();
+
+		appUserDefault.setWorkerMemory("2g");
+		appUserDefault.setNumWorkers(3);
+		appUserDefault.setWorkerCores(1);
+		appUserDefault.setClassName("hadooptest.spark.regression.SparkHackJetty");
+		appUserDefault.setJarName(localJar);
+		appUserDefault.setAppName("runSparkHackJettyFileServer");
+		appUserDefault.setShouldPassName(true);
+                String[] argsArray = {"false", "false"};
+                appUserDefault.setArgs(argsArray);
+
+		appUserDefault.start();
+
+		assertTrue("App (default user) was not assigned an ID within 30 seconds.", 
+			appUserDefault.waitForID(30));
+		assertTrue("App ID for sleep app (default user) is invalid.", 
+			appUserDefault.verifyID());
+
+		int waitTime = 30;
+		assertTrue("Job (default user) did not succeed.",
+			appUserDefault.waitForSuccess(waitTime));
+	}
+
+	@Test
+	public void runSparkHackJettyFileServerAuthOff() throws Exception {
+		SparkRunClass appUserDefault = new SparkRunClass();
+
+		appUserDefault.setWorkerMemory("2g");
+		appUserDefault.setNumWorkers(3);
+		appUserDefault.setWorkerCores(1);
+		appUserDefault.setClassName("hadooptest.spark.regression.SparkHackJetty");
+		appUserDefault.setAppName("runSparkHackJettyFileServerAuthOff");
+		appUserDefault.setShouldPassName(true);
+		appUserDefault.setJarName(localJar);
+                String[] argsArray = {"false", "true"};
+                appUserDefault.setArgs(argsArray);
+
+		appUserDefault.start();
+
+		assertTrue("App (default user) was not assigned an ID within 30 seconds.", 
+			appUserDefault.waitForID(30));
+		assertTrue("App ID for sleep app (default user) is invalid.", 
+			appUserDefault.verifyID());
+
+		int waitTime = 30;
+		assertTrue("Job (default user) did not succeed.",
+			appUserDefault.waitForSuccess(waitTime));
+	}
+	
+}
