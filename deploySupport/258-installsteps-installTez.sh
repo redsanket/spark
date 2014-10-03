b00287ea07e390abca80bca80b8bc771fd1a5092
Amit Tiwari <tiwari@yahoo-inc.com>
2014-10-02 21:13:20 -0500

    Add /* at the end of path

--- deploySupport/258-installsteps-installTez.sh
+++ deploySupport/258-installsteps-installTez.sh
@@ -22,7 +22,7 @@ st=$?
 cmd="export TEZ_CONF_DIR=/home/gs/conf/tez/ ; \
      export TEZ_HOME=/home/gs/tez/ ; \
 
-     export HADOOP_CLASSPATH="$TEZ_CONF_DIR:$TEZ_HOME/*:$TEZ_HOME/lib/*:$yrootHadoopCurrent" ; \
+     export HADOOP_CLASSPATH="$TEZ_CONF_DIR:$TEZ_HOME/*:$TEZ_HOME/lib/*:$yrootHadoopCurrent/*" ; \
      export JAVA_HOME=$GSHOME/java/jdk ; \
      kinit -k -t /homes/hdfsqa/hdfsqa.dev.headless.keytab hdfsqa ; \
      echo installing Tez into hdfs ; \
