bdec5eae741ecbc22ad1ee59d7ed28d847335aa0
Amit Tiwari <tiwari@yahoo-inc.com>
2015-01-06 17:04:12 -0600

    Conform to the port change from 8188 to 4080 for ATS timeline server

--- hadooptest/htf-common/src/main/java/hadooptest/automation/constants/HadooptestConstants.java
+++ hadooptest/htf-common/src/main/java/hadooptest/automation/constants/HadooptestConstants.java
@@ -167,7 +167,7 @@ public class HadooptestConstants {
 	// Ports
 	public static class Ports {
 		public static String HDFS = "8020";
-		public static int HTTP_ATS_PORT = 8188;
+		public static int HTTP_ATS_PORT = 4080;
 
 	}
 		// Execution
