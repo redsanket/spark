29a774ab89739a5b3828b3dd03bbfe23820a658b
lawkp <lawkp@yahoo-inc.com>
2015-02-20 14:11:54 -0600

    Fixed date issues in TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate testcase

--- hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate.java
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate.java
@@ -17,8 +17,11 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -53,11 +56,12 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 	private static final int SUCCESS = 200;
 	final static Charset ENCODING = StandardCharsets.UTF_8;
 	private static final String HCAT_TYPE = "DataOnly";
-	private String START_DATE; 
-	private String INSTANCE_DATE_1 = "50";
-	private String INSTANCE_DATE_2 = "40";
-	private String INSTANCE_DATE_3  = "20";
+	private String START_DATE;
+	private String INSTANCE_DATE_1;
+	private String INSTANCE_DATE_2;
+	private String INSTANCE_DATE_3;
 	private List<String> instanceList;
+	private Map<String, String> instanceDateMap = new HashMap<String,String>();
 	
 	@BeforeClass
 	public static void startTestSession() throws Exception {
@@ -89,25 +93,37 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 
-		// instance 1 date
-		cal.add(Calendar.DAY_OF_MONTH, -Integer.parseInt(this.INSTANCE_DATE_1));
+		// instance 1 date , 50 days instance are generated
+		cal.add(Calendar.DAY_OF_MONTH, -50);	// 50 days
 		this.INSTANCE_DATE_1 = sdf.format(cal.getTime());
 		instanceList.add(this.INSTANCE_DATE_1);
+		instanceDateMap.put("50-Days", this.INSTANCE_DATE_1);
+		
+		// reset the value to current day
+		cal.add(Calendar.DAY_OF_MONTH, 50);
 
-		// instance 2 date
-		cal.add(Calendar.DAY_OF_MONTH, -Integer.parseInt(this.INSTANCE_DATE_2));
+		// instance 2 date , 40 days instance are generated
+		cal.add(Calendar.DAY_OF_MONTH, -40); // 40 days
 		this.INSTANCE_DATE_2 = sdf.format(cal.getTime());
 		instanceList.add(this.INSTANCE_DATE_2);
+		instanceDateMap.put("40-Days", this.INSTANCE_DATE_2);
+		
+		// reset the value to current day
+		cal.add(Calendar.DAY_OF_MONTH, 40);
 
-		// start date ( instance will not be generated for start date.
-		cal.add(Calendar.DAY_OF_MONTH, -20);
+		// 30 days, but instance are not generated , but where as this date is considered as start date
+		cal.add(Calendar.DAY_OF_MONTH, -30);  
 		this.START_DATE = sdf.format(cal.getTime());
+		instanceDateMap.put("30-Days - startDate", this.START_DATE );
+		
+		// reset the value to current day
+		cal.add(Calendar.DAY_OF_MONTH, 30);
 
-		// instance 3 date
-		cal.add(Calendar.DAY_OF_MONTH, -Integer.parseInt(this.INSTANCE_DATE_3));
+		// instance 3 date, 20 days instance are generated
+		cal.add(Calendar.DAY_OF_MONTH, -20);
 		this.INSTANCE_DATE_3 = sdf.format(cal.getTime());
 		instanceList.add(this.INSTANCE_DATE_3);
-
+		instanceDateMap.put("20-Days", this.INSTANCE_DATE_3);
 	}
 
 	@Test
@@ -117,23 +133,63 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 		for ( String instance : this.instanceList) {
 			generatePerformanceFeeds.generateFeed(instance);
 		}
-
+		
+		TestSession.logger.info("************************************************************************");
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		TestSession.logger.info("Start Date - " + this.START_DATE);
+		TestSession.logger.info("Current date - " + sdf.format(cal.getTime()));
+		 cal.add(Calendar.DAY_OF_MONTH, -1);
+		 String endDateStr = sdf.format(cal.getTime());
+		TestSession.logger.info("End date ( offset value is 1) " + endDateStr);
+		TestSession.logger.info("Following are the dates that instances are generated");
+		List<String> logList = new ArrayList<String> ();
+		for (String key : instanceDateMap.keySet()) {
+			if (!key.contains("startDate")) {
+				logList.add( instanceDateMap.get(key));
+				TestSession.logger.info( key + " - "  + instanceDateMap.get(key));
+			}
+		}
+		
+		// collect all the instance that are going to get discovered.
+		List<String> discoveredInstanceList = new ArrayList<String>();
+		
+		TestSession.logger.info("Discovert should happen between " + this.START_DATE  + "  and " + endDateStr );
+		Date startDate = sdf.parse(this.START_DATE);
+		Date endDate = sdf.parse(endDateStr);
+		for (String instance : logList) {
+			Date dt = sdf.parse(instance);
+			if ( dt.compareTo(startDate) > 0 && dt.compareTo(endDate) < 0) {
+				discoveredInstanceList.add(instance);
+				TestSession.logger.info(instance);
+			} else if ( dt.compareTo(startDate) == 0) {
+				discoveredInstanceList.add(instance);
+				TestSession.logger.info(instance);
+			} else if ( dt.compareTo(endDate ) == 0) {
+				discoveredInstanceList.add(instance);
+				TestSession.logger.info(instance);
+			}
+		}
+		TestSession.logger.info("************************************************************************");
+		
 		// create a dataset
 		createDataSetForAcqRep( );
 
 		// check for acquisition workflow
-		this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "acquisition", this.datasetActivationTime , this.INSTANCE_DATE_1);
-		this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "acquisition", this.datasetActivationTime , this.INSTANCE_DATE_2);
-
-		// check whether instance files are available on target1
-		this.checkInstanceFiles(this.targetGrid1 , "acquisition" , this.dataSetName);
-
-		//check for replication workflow
-		this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "replication", this.datasetActivationTime, this.INSTANCE_DATE_1);
-		this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "replication", this.datasetActivationTime, this.INSTANCE_DATE_2);
+		for ( String instance : discoveredInstanceList ) {
+			this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "acquisition", this.datasetActivationTime , instance);
+			
+			// check whether instance files are available on target1
+			this.checkInstanceFiles(this.targetGrid1 , "acquisition" , this.dataSetName , instance);
+		}
 		
-		// check whether instance files are available on target2
-		this.checkInstanceFiles(this.targetGrid2 , "replication" , this.dataSetName);
+		//check for replication workflow
+		for ( String instance : discoveredInstanceList ) {
+			this.workFlowHelperObj.checkWorkFlow(this.dataSetName, "replication", this.datasetActivationTime, instance);
+			
+			// check whether instance files are available on target2
+			this.checkInstanceFiles(this.targetGrid2 , "replication" , this.dataSetName , instance);
+		}
 	}
 
 	/**
@@ -142,12 +198,12 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 	 * @param facetName
 	 * @param dataSetName
 	 */
-	private void checkInstanceFiles(String targetName , String facetName , String dataSetName) {
+	private void checkInstanceFiles(String targetName , String facetName , String dataSetName , String instance) {
 		String hadoopLSURL =  this.url + "/console/api/admin/hadoopls?dataSource=" + targetName + "&dataSet=" + dataSetName + "&format=json";
 		TestSession.logger.info("testURL = " + hadoopLSURL);
 		com.jayway.restassured.response.Response response = given().cookie( this.cookie).get(hadoopLSURL);
 		assertTrue("Failed to get the respons = " + hadoopLSURL , (response != null) );
-		boolean instance_1_Found = false,instance_2_Found = false , instance_3_Found = false;
+		boolean instanceFound = false;
 		JSONArray hadoopLSJsonArray = this.consoleHandle.convertResponseToJSONArray(response, "Files");
 		if (hadoopLSJsonArray.size() > 0) {
 			Iterator iterator = hadoopLSJsonArray.iterator();
@@ -156,18 +212,12 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 				String isDirectory = hadoopLSJsonObject.getString("Directory");
 				if( isDirectory.equals("yes")) {
 					String path = hadoopLSJsonObject.getString("Path").trim();
-					if (path.contains(this.INSTANCE_DATE_1) == true) {
-						instance_1_Found = true;
-					} else if (path.contains(this.INSTANCE_DATE_2) == true ) {
-						instance_2_Found = true;
-					} else if (path.contains(this.INSTANCE_DATE_3) == true) {
-						instance_3_Found = true;
-					}
+					if (path.contains(instance) == true) {
+						instanceFound = true;
+					} 
 				}
 			}
-			assertTrue( this.INSTANCE_DATE_1 + " instance failed to copy to " + facetName , instance_1_Found == true );
-			assertTrue( this.INSTANCE_DATE_2 + " instance failed to copy to " + facetName , instance_2_Found == true );
-			assertTrue( this.INSTANCE_DATE_3 + " instance should have not copied to " + facetName , instance_3_Found == false );
+			assertTrue( instance + " instance failed to copy to " + facetName , instanceFound == true );
 		}
 	}
 
@@ -185,18 +235,17 @@ public class TestDyamicStartTypeOffSetAndEndTypeFixedWithInstaceAfterStartDate e
 		dataSetXml = dataSetXml.replaceAll("SOURCE_NAME", sourceName );
 		dataSetXml = dataSetXml.replaceAll("TARGET1", this.targetGrid1 );
 		dataSetXml = dataSetXml.replaceAll("TARGET2", this.targetGrid2 );
-		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_START_DATE_TYPE", "fixed" );
+		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_TYPE_START", "Fixed" );
 		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_START_DATE", this.START_DATE );
-		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_END_DATE_TYPE", "offset" );
+		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_TYPE_END", "Offset" );
 		dataSetXml = dataSetXml.replaceAll("TARGET_ONE_END_DATE", "1" );
-		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_START_DATE_TYPE", "fixed" );
+		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_TYPE_START", "Fixed" );
 		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_START_DATE", this.START_DATE );
-		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_END_DATE_TYPE", "offset" );
+		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_TYPE_END", "Offset" );
 		dataSetXml = dataSetXml.replaceAll("TARGET_TWO_END_DATE", "1");
 		dataSetXml = dataSetXml.replaceAll("TABLE_NAME", this.dataSetName);
 		dataSetXml = dataSetXml.replaceAll("GROUP_NAME", "users");
 		dataSetXml = dataSetXml.replaceAll("DATA_OWNER", "");
-
 
 		TestSession.logger.info("**** DataSet Name = " + dataSetXml   + " ********** ");
 		Response response = this.consoleHandle.createDataSet(this.dataSetName, dataSetXml);
