8cadc25f0fa6048623a8e2a4f0e210ff40b723c8
Amit Tiwari <tiwari@yahoo-inc.com>
2015-03-04 17:02:07 -0600

    Uncomment the fetching of cookies from namenode. (2) Removed the randomization of dag names

--- hadooptest/htf-common/src/test/java/hadooptest/tez/mapreduce/examples/extensions/FilterLinesByWordExtendedForTezHTF.java
+++ hadooptest/htf-common/src/test/java/hadooptest/tez/mapreduce/examples/extensions/FilterLinesByWordExtendedForTezHTF.java
@@ -6,7 +6,6 @@ import hadooptest.tez.utils.HtfTezUtils.Session;
 import hadooptest.tez.utils.HtfTezUtils.TimelineServer;
 
 import java.util.Map;
-import java.util.Random;
 import java.util.TreeMap;
 import java.util.UUID;
 
@@ -158,7 +157,7 @@ public class FilterLinesByWordExtendedForTezHTF extends FilterLinesByWord {
 	    fs.copyFromLocalFile(new Path(jarPath), remoteJarPath);
 	    TestSession.logger.info("Amit: remoteJarPath:" + remoteJarPath.toString());
 	    FileStatus remoteJarStatus = fs.getFileStatus(remoteJarPath);
-//	    TokenCache.obtainTokensForNamenodes(credentials, new Path[]{remoteJarPath}, conf);
+	    TokenCache.obtainTokensForNamenodes(credentials, new Path[]{remoteJarPath}, conf);
 
 	    Map<String, LocalResource> commonLocalResources = new TreeMap<String, LocalResource>();
 	    LocalResource dagJarLocalRsrc = LocalResource.newInstance(
@@ -175,11 +174,7 @@ public class FilterLinesByWordExtendedForTezHTF extends FilterLinesByWord {
 	    TestSession.logger.info("Amit: dagJarLocalRsrc.getPattern():" + dagJarLocalRsrc.getPattern());
 	    
 	    
-
-
-	    Random randomGenerator = new Random();
-	    int randomInt = randomGenerator.nextInt(100000);
-	    String tezClientName= "FilterLinesByWordSession" + randomInt;
+	    String tezClientName= "FilterLinesByWordSession";
 	    TezClient tezSession = TezClient.create(tezClientName, tezConf,
 	        commonLocalResources, null);
 	    tezSession.start(); // Why do I need to start the TezSession.
@@ -226,7 +221,7 @@ public class FilterLinesByWordExtendedForTezHTF extends FilterLinesByWord {
 	        .newBuilder(Text.class.getName(), TextLongPair.class.getName())
 	        .setFromConfiguration(tezConf).build();
 
-	    DAG dag = DAG.create("FilterLinesByWord"+randomInt);
+	    DAG dag = DAG.create("FilterLinesByWord");
 	    Edge edge =
 	        Edge.create(stage1Vertex, stage2Vertex, edgeConf.createDefaultBroadcastEdgeProperty());
 	    dag.addVertex(stage1Vertex).addVertex(stage2Vertex).addEdge(edge);
