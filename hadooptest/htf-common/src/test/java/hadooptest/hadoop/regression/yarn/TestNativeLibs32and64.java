35eef022a196cbccc54d2d08cd73ba8411e046e8
Patrick White <patwhite@yahoo-inc.com>
2015-03-24 11:00:26 -0500

    ignore java8 test if no java8 support

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestNativeLibs32and64.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestNativeLibs32and64.java
@@ -35,7 +35,7 @@ import java.io.File;
 import org.apache.hadoop.yarn.client.api.impl.YarnClientImpl; // H2.x, this test is 2.x only
 import org.apache.hadoop.mapred.TaskReport;
 import org.junit.Assert;
-import org.junit.Assume.assumeTrue; 
+import org.junit.Assume.*; 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
