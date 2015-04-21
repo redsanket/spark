355451e19ffee398de018174c4850c074803f7e0
Robert (Bobby) Evans <evans@yahoo-inc.com>
2015-04-21 19:27:26 +0000

    Addressed rework

--- hadooptest/htf-common/src/test/java/hadooptest/storm/TestDHSpoutTopology.java
+++ hadooptest/htf-common/src/test/java/hadooptest/storm/TestDHSpoutTopology.java
@@ -38,7 +38,8 @@ public class TestDHSpoutTopology extends TestSessionStorm {
     //static String configURI="http://0.0.0.0:8090/registry/v1/";
     //static String serverURI=configURI;
     static String serverURI=null;
-    static String vhURI="http://myvh-stormtest.corp.yahoo.com/";
+    static String vhURI="http://myvh-stormtest.corp.yahoo.com:9153/";
+    static String vhEphemeralURI="http://myvh-stormtest.corp.yahoo.com/";
     private backtype.storm.Config _conf;
     private String _ycaV1Role;
     private TestEventCountBolt theBolt = null;
@@ -106,7 +107,6 @@ public class TestDHSpoutTopology extends TestSessionStorm {
         _conf.setNumWorkers(3);
         _conf.put(backtype.storm.Config.NIMBUS_TASK_TIMEOUT_SECS, 200);
         _conf.put(Config.REGISTRY_URI, serverURI);
-        _conf.put("topology.http_spout.port.range","10000-10100");
 
         _conf.registerSerialization(AvroEventRecord.class, KryoEventRecord.class);
 
@@ -133,6 +133,106 @@ public class TestDHSpoutTopology extends TestSessionStorm {
 
         logger.info("Launching DHSimulator:");
         DHSimulator dh = new DHSimulator(_conf, new URI(vhURI), null, null, 1, false, null, dhCount);
+
+        try {
+            logger.info("Starting DHSimulator:");
+            dh.start();
+            logger.info("Joining DHSimulator:");
+            dh.join();
+            logger.info("Leaving join of DHSimulator, sleeping for a little bit to make sure all batches processed:");
+            Util.sleep(10);
+        } finally {
+            logger.info("Halting DHSimulator:");
+            dh.halt();
+        }
+
+        boolean passed = true;
+        // Need to fix this, but we are going to sleep for a while so I can check state of cluster to see if it really did anything.
+        try {
+
+            int uptime = 10;
+            int cur_uptime = 0;
+
+            cur_uptime = getUptime(topoName);
+
+            if (cur_uptime < uptime){
+                Util.sleep(uptime - cur_uptime);
+            }
+
+            cur_uptime = getUptime(topoName);
+
+            while (cur_uptime < uptime){
+                Util.sleep(1);
+                cur_uptime = getUptime(topoName);
+            }
+
+            //get results
+
+            //get expected results
+            String file = conf.getProperty("WORKSPACE") + "/htf-common/resources/storm/testinputoutput/TestDHSpoutTopology/expected_results";
+            logger.info("Read epected results from: "+ file);
+            HashMap<String, Integer> expectedWordCount = Util.readMapFromFile(file);
+
+            // Now check to see if the bolt counted the right number of packets
+            for (String key: expectedWordCount.keySet()){
+                logger.info("Checking to see if the value for " + key + "is " + expectedWordCount.get(key));
+                String drpcResult = cluster.DRPCExecute( function, key );
+                passed &= Integer.toString(expectedWordCount.get(key)).equals(drpcResult);
+                if (!passed) {
+                    logger.error("Expected (" + expectedWordCount.get(key) + ") and result (" + drpcResult +") do not match for " + key );
+                }
+            }
+
+        } finally {
+            cluster.killTopology(topoName);
+        }
+
+        if (!passed) {
+            logger.error("A test case failed.  Throwing error");
+            throw new Exception();
+        }
+    }
+
+    @Test(timeout=600000)
+    public void TestDHSpoutTopologyHTTPEphemeral() throws Exception{
+        logger.info("Starting TestDHSpoutTopology");
+        StormTopology topology = buildTopology(vhEphemeralURI);
+
+        String topoName = "dhspout-topology-test";
+
+        _conf = new backtype.storm.Config();
+        _conf.putAll(backtype.storm.utils.Utils.readStormConfig());
+        _conf.setDebug(true);
+        _conf.setNumWorkers(3);
+        _conf.put(backtype.storm.Config.NIMBUS_TASK_TIMEOUT_SECS, 200);
+        _conf.put(Config.REGISTRY_URI, serverURI);
+        _conf.put("topology.http_spout.port.range","10000-10100");
+
+        _conf.registerSerialization(AvroEventRecord.class, KryoEventRecord.class);
+
+        String outputLoc = "/tmp/dhcount"; //TODO change this to use a shared directory or soemthing, so we can get to it simply
+        _conf.put("test.output.location",outputLoc);
+
+        addVirtualHost(new URI(vhEphemeralURI));
+        //TODO turn this into a utility that has a conf setting
+        File jar = new File(conf.getProperty("WORKSPACE") + "/topologies/target/topologies-1.0-SNAPSHOT-jar-with-dependencies.jar");
+        try {
+            cluster.submitTopology(jar, topoName, _conf, topology);
+        } catch (Exception e) {
+            logger.error("Couldn't launch topology: " + e );
+            throw new Exception();
+        }
+
+        // Now we need to wait until the spout shows up in the registry server's YFOR config.
+        //
+        // Actually, it __should__ wait for it to come up, but let's sleep a little.
+
+        _conf.put(Config.REGISTRY_URI, serverURI);
+        Integer dhCount = new Integer(20);
+
+
+        logger.info("Launching DHSimulator:");
+        DHSimulator dh = new DHSimulator(_conf, new URI(vhEphemeralURI), null, null, 1, false, null, dhCount);
 
         try {
             logger.info("Starting DHSimulator:");
