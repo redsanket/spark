767164cb304bb625a2399b160a4cf7453ca312eb
sanket <schintap@yahoo-inc.com>
2015-04-27 16:43:49 -0500

    Changed the config data

--- hadooptest/htf-common/src/test/java/hadooptest/storm/TestStormKafkaTopology.java
+++ hadooptest/htf-common/src/test/java/hadooptest/storm/TestStormKafkaTopology.java
@@ -25,6 +25,10 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
+//import kafka.javaapi.producer.Producer;
+//import kafka.producer.KeyedMessage;
+//import kafka.producer.ProducerConfig;
+
 @Category(SerialTests.class)
 public class TestStormKafkaTopology extends TestSessionStorm {
 
@@ -47,23 +51,34 @@ public class TestStormKafkaTopology extends TestSessionStorm {
     {
         String pathToFile = conf.getProperty("WORKSPACE") + "/htf-common/resources/storm/testinputoutput/TestStormKafkaTopology/producer";
         String pathToScripts = conf.getProperty("KAFKA_HOME");
-        String hostAddress = "gsbl90782.blue.ygrid.yahoo.com";
+        String brokerHost = "gsbl90786.blue.ygrid.yahoo.com";
+        String zookeeperHost = "gsbl90782.blue.ygrid.yahoo.com";
         String brokerPort = "9092";
         String[] returnTopicValue = exec.runProcBuilder(new String[]{ pathToScripts + "kafka-topics.sh", "--create", "--zookeeper",
-                hostAddress+":2181", "--replication-factor", "1", "--partitions", "1" ,"--topic", "test"}, true);
+                zookeeperHost+":2181", "--replication-factor", "1", "--partitions", "1" ,"--topic", "test"}, true);
         assertTrue( "Could not create topic for consuming", returnTopicValue[0].equals("0") );
 
 //        String[] returnProducerValue = exec.runProcBuilder(new String[]{ pathToScripts + "kafka-console-producer.sh", "--broker-list",
 //                 hostAddress + ":" + brokerPort,  "--topic", "test", "<", pathToFile }, true);
 //        assertTrue("Could not write to the producer", returnProducerValue[0].equals("0"));
 
-        
-        Map<String, Object> config = new HashMap<String, Object>();
-        config.put("metadata.broker.list", hostAddress + ":9092");
-        config.put("serializer.class", "kafka.serializer.StringEncoder");
-        config.put("request.required.acks", "1");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", (new ArrayList()).add(brokerHost+":9092"));
+        //props.put("serializer.class", "kafka.serializer.StringEncoder");
+        //props.put("request.required.acks", "1");
 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(config);
+//        ProducerConfig config = new ProducerConfig(props);
+//        Producer<String, String> producer = new Producer<String, String>(config);
+//        KeyedMessage<String, String> line1 = new KeyedMessage<String, String>("test", "1", "hello Yahoo");
+//        KeyedMessage<String, String> line2 = new KeyedMessage<String, String>("test", "2", "hello Champaign");
+//
+//        producer.send(line1);
+//        producer.send(line2);
+//
+//        producer.close();
+
+// For New JAVA KAFKA CLIENT API
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
         ProducerRecord<String, String> line1 = new ProducerRecord<String, String>("test", "1","hello Yahoo");
         ProducerRecord<String, String> line2 = new ProducerRecord<String, String>("test", "2","hello Champaign");
