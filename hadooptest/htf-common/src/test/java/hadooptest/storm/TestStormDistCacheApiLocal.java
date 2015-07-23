6d3e421935a44214c32380b3f9d6b834f1575557
Sanket <schintap@untilservice-lm>
2015-07-23 11:39:17 -0500

    removed unnecessary commits

--- hadooptest/htf-common/src/test/java/hadooptest/storm/TestStormDistCacheApiLocal.java
+++ hadooptest/htf-common/src/test/java/hadooptest/storm/TestStormDistCacheApiLocal.java
@@ -5,28 +5,17 @@ import hadooptest.Util;
 import hadooptest.SerialTests;
 import hadooptest.TestSessionStorm;
 
-import hadooptest.automation.utils.http.HTTPHandle;
-import hadooptest.automation.utils.http.Response;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import org.apache.commons.httpclient.HttpMethod;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-
-import java.lang.reflect.Array;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static org.junit.Assert.*;
 import hadooptest.cluster.storm.ModifiableStormCluster;
 import static org.junit.Assume.assumeTrue;
 import hadooptest.cluster.storm.StormDaemon;
-import backtype.storm.blobstore.ClientBlobStore;
-import backtype.storm.generated.SettableBlobMeta;
 
 @Category(SerialTests.class)
 public class TestStormDistCacheApiLocal extends TestStormDistCacheApi {
