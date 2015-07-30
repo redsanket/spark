e424346c9316b6797fa17801f8c07c874784127a
lawkp <lawkp@yahoo-inc.com>
2015-07-30 10:16:02 -0500

    Fixed custom paths

--- hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/TestDiscoveryPathSpecifiedAfterDate.java
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/TestDiscoveryPathSpecifiedAfterDate.java
@@ -139,11 +139,11 @@ public class TestDiscoveryPathSpecifiedAfterDate extends TestSession {
 		dataSetXml = dataSetXml.replace("HCAT_TYPE", this.HCAT_TYPE);
 		dataSetXml = dataSetXml.replaceAll("HCAT_TABLE_NAME", this.dataSetName);
 		dataSetXml = dataSetXml.replaceAll("REPLICATION_SOURCE_DATA_PATH", this.getReplicationPath());
-		dataSetXml = dataSetXml.replaceAll("<Path location=\"REPLICATION_SOURCE_SCHEMA_PATH\" type=\"schema\"/>", "");
-		dataSetXml = dataSetXml.replaceAll("<Path location=\"REPLICATION_SOURCE_COUNT_PATH\" type=\"count\"/>", "");
+		dataSetXml = dataSetXml.replaceAll("REPLICATION_SOURCE_SCHEMA_PATH", this.getReplicationPath());
+		dataSetXml = dataSetXml.replaceAll("REPLICATION_SOURCE_COUNT_PATH", this.getReplicationPath());
 		dataSetXml = dataSetXml.replaceAll("CUSTOM_DATA_PATH", this.getCustomPath());
-		dataSetXml = dataSetXml.replaceAll("<Path location=\"CUSTOM_COUNT_PATH\" type=\"count\"/>", "");
-		dataSetXml = dataSetXml.replaceAll("<Path location=\"CUSTOM_SCHEMA_PATH\" type=\"schema\"/>", "");
+		dataSetXml = dataSetXml.replaceAll("CUSTOM_COUNT_PATH", this.getCustomPath());
+		dataSetXml = dataSetXml.replaceAll("CUSTOM_SCHEMA_PATH", this.getCustomPath());
 		dataSetXml = dataSetXml.replaceAll("20130725" , instanceDates.get(0));
 
 		Response response = this.consoleHandle.createDataSet(this.dataSetName, dataSetXml);
