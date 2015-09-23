5b2955debf2f6adbf3833e53824bf21a2597f327
philips <philips@yahoo-inc.com>
2015-09-23 10:12:25 -0500

    export HOMEDIR before invoking test remotely

--- deploySupport/500-installsteps-runsimpletest.sh
+++ deploySupport/500-installsteps-runsimpletest.sh
@@ -13,10 +13,6 @@ else
 	testname=${RUNSIMPLETEST}
    fi
 
-
-   echo === running a simple test on  gateway=$gateway
-   scp "$testfile"  ${gateway}:/tmp/${cluster}-${testname}
-   
    export yarnProcessor=yarn
 #  [ -n  "$PREFERREDJOBPROCESSOR" ] && export yarnProcessor=$PREFERREDJOBPROCESSOR
    if [ "$REMOVEEXISTINGDATA" = true ]
@@ -27,10 +23,23 @@ else
       sleep 180
    fi
 
-   echo  GSHOME=$GSHOME yroothome=$yroothome sh /tmp/${cluster}-${testname} -c $cluster -N "'$namenode'" ${writeenabledflag} -P "${yarnProcessor}" -u "${MAPREDUSER}" | ssh $gateway su - hadoopqa
+   # Run $cluster-shorttest-rw.sh from the gateway, passing the primary namenode value.
+   echo === running a simple test on  gateway=$gateway
+   set +x
+   scp "$testfile"  ${gateway}:/tmp/${cluster}-${testname}
+   set -x
+
+   # echo  GSHOME=$GSHOME yroothome=$yroothome sh /tmp/${cluster}-${testname} -c $cluster -N "'$namenode'" ${writeenabledflag} -P "${yarnProcessor}" -u "${MAPREDUSER}" | ssh $gateway su - hadoopqa
+   ssh $gateway "su - hadoopqa -c '\
+export GSHOME=$GSHOME && \
+export yroothome=$yroothome && \
+export HOMEDIR=$HOMEDIR && \
+sh /tmp/${cluster}-${testname} \
+-c $cluster -N $namenode ${writeenabledflag} -P ${yarnProcessor} -u ${MAPREDUSER} \
+'"
 
    if [ "$?" -ne 0 ]; then
-       echo "deployment veirification test failed! exiting...."
+       echo "500-installsteps-runsimpletest.sh: deployment verification test failed! exiting...."
        exit 1
    fi
 fi
