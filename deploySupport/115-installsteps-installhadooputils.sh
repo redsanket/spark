6646564079b033f01866652f8269f22421f1f37a
Randy Clayton <randyc@yahoo-inc.com>
2015-12-10 13:34:21 -0600

    not on test branch, remove

--- deploySupport/115-installsteps-installhadooputils.sh
+++ deploySupport/115-installsteps-installhadooputils.sh
@@ -5,6 +5,6 @@ LOCALCONFDIR=/usr/local/conf/bouncer
 cmd="[[ -d ${LOCALCONFDIR} ]] || mkdir -p ${LOCALCONFDIR}"
 fanout $cmd
 
-cmd="/usr/local/bin/yinst install -root ${LOCALCONFDIR} -yes -branch test ygrid_hadoop_utils"
+cmd="/usr/local/bin/yinst install -root ${LOCALCONFDIR} -yes ygrid_hadoop_utils"
 fanout $cmd
 
