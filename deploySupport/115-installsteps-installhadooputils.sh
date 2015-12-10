32a8bbf8eabb9124b1ea309813a9ecbe7f7ebab7
Randy Clayton <randyc@yahoo-inc.com>
2015-12-10 15:55:23 -0600

    fix which ygrid hadoop utils we are using

--- deploySupport/115-installsteps-installhadooputils.sh
+++ deploySupport/115-installsteps-installhadooputils.sh
@@ -1,10 +1,5 @@
 echo "Installing ygrid_hadoop_utils ..."
 
-LOCALCONFDIR=/usr/local/conf/bouncer
-
-cmd="[[ -d ${LOCALCONFDIR} ]] || mkdir -p ${LOCALCONFDIR}"
-fanout $cmd
-
-cmd="/usr/local/bin/yinst install -root ${LOCALCONFDIR} -yes ygrid_hadoop_utils"
+cmd="/usr/bin/yum -y install ygrid-hadoop-utils"
 fanout $cmd
 
