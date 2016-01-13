b17c29b55dea62ad638118a77441541ee1980028
Randy Clayton <randyc@yahoo-inc.com>
2016-01-13 15:19:09 -0600

    raise timeout to allow kerberos ticket to expire

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestDelegationTokens.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestDelegationTokens.java
@@ -136,7 +136,7 @@ public class TestDelegationTokens extends DfsTestsBaseClass {
 				fileStoringDelegationTokenReceivedViaKinit.getPath(), "60s");
 
 		// Wait for longer than 60s, let the ticket expire
-		Thread.sleep(90000);
+		Thread.sleep(180000);
 		HashMap<String, String> testSpecificEnvVars = new HashMap<String, String>();
 		testSpecificEnvVars.put(KRB5CCNAME,
 				fileStoringDelegationTokenReceivedViaKinit.getPath());
