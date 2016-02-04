55bd751205dee744795acc678126ed6cbda881ed
Patrick White <patwhite@yahoo-inc.com>
2016-02-04 11:05:00 -0600

    enable oozie submitter

--- hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/integration/DataAvailabilityPoller.java
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/integration/DataAvailabilityPoller.java
@@ -501,7 +501,7 @@ public class DataAvailabilityPoller {
 
 					// set state to START_OOZIE_JOB
 					this.searchDataAvailablity.setState("START_OOZIE_JOB");
-					/*				if ( this.searchDataAvailablity.getState().equals("START_OOZIE_JOB")) {
+					if ( this.searchDataAvailablity.getState().equals("START_OOZIE_JOB")) {
 
 						String oozieCommand = "ssh " + this.oozieHostName + "   \" " + this.kINIT_COMMAND + ";"  +   OOZIE_COMMAND + " job -run -config " +  "/tmp/" + this.currentFrequencyHourlyTimeStamp + "-job.properties" + " -oozie " + "http://" + this.oozieHostName + ":4080/oozie -auth kerberos"   + " \"";
 						TestSession.logger.info("oozieCommand  = " + oozieCommand);
@@ -516,7 +516,7 @@ public class DataAvailabilityPoller {
 						String oozieWorkFlowName = "stackint_oozie_RawInput_" + this.getCurrentFrequencyValue();
 						TestSession.logger.info("*************************************************************************************************************************************************");
 						this.oozieJobResult = this.pollOozieJob(this.oozieJobID , oozieWorkFlowName);
-					}*/
+					}
 				}
 			}
 
