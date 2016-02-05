b3db5ddee1f9fe79af3ea6f169c1274ef80e6b0e
Aaron Gresch <agresch@yahoo-inc.com>
2016-02-05 09:43:06 -0600

    GDM-996 add dataset JSON retention test

--- hadooptest/htf-common/src/main/java/hadooptest/cluster/gdm/SourcePath.java
+++ hadooptest/htf-common/src/main/java/hadooptest/cluster/gdm/SourcePath.java
@@ -1,47 +1,38 @@
+// Copyright 2016, Yahoo Inc.
 package hadooptest.cluster.gdm;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class SourcePath {
-	private JSONArray sourcePaths;
-	private int sourcePathsCount ;
-	
-	public SourcePath() {
-		this.sourcePaths = new JSONArray();
-		this.sourcePathsCount = 0;
-	}
-	
-/*	public SourcePath(SourcePath sourceObject) {
-		sourcePaths = sourceObject.sourcePaths;
-	}*/
-	
-	public SourcePath addSourcePath(String pathValue) {
-		JSONObject pathJSONObject = new JSONObject();
-		if (this.sourcePaths == null) {
-			this.sourcePaths = new JSONArray();
-			this.sourcePathsCount = 0;
-		}
-		pathJSONObject.put("Path", pathValue);
-		this.sourcePaths.add(this.sourcePathsCount, pathJSONObject);
-		this.sourcePathsCount++;
-		return this;
-	}
+    private JSONArray sourcePaths;
+    
+    public SourcePath() {
+        this.sourcePaths = new JSONArray();
+    }
+    
+    public SourcePath addSourcePath(String pathValue) {
+        if (this.sourcePaths == null) {
+            this.sourcePaths = new JSONArray();
+        }
+        this.sourcePaths.add(pathValue);
+        return this;
+    }
 
-	public JSONArray getSourcePath() throws Exception {
-		if (this.sourcePaths == null) {
-			throw new Exception("There is no any path specified.");
-		}
-		return sourcePaths;
-	}
-	
-	public String toString() {
-		String returnValue = null;
-		try {
-			returnValue = getSourcePath().toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return returnValue;
-	}
+    public JSONArray getSourcePath() throws Exception {
+        if (this.sourcePaths == null) {
+            throw new Exception("There is no any path specified.");
+        }
+        return sourcePaths;
+    }
+    
+    public String toString() {
+        String returnValue = null;
+        try {
+            returnValue = getSourcePath().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
 }
