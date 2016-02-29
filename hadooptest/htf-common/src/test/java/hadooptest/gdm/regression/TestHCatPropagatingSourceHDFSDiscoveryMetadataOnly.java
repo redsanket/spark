bf49bff9726bd87b6bf7c500437e6764ce99298e
mmukhi <mmukhi@yahoo-inc.com>
2016-02-29 23:01:58 +0000

    [GDM-1030] added HTF test case for hcat testing for propagating source, HDFS discovery and metadata only

---
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/TestHCatPropagatingSourceHDFSDiscoveryMetadataOnly.java
@@ -0,0 +1,109 @@
+package gdm.regression;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import hadooptest.TestSession;
+import hadooptest.cluster.gdm.ConsoleHandle;
+import hadooptest.cluster.gdm.GdmUtils;
+import hadooptest.cluster.gdm.Response;
+import hadooptest.cluster.gdm.WorkFlowHelper;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.assertTrue;
+
+public class TestHCatPropagatingSourceHDFSDiscoveryMetadataOnly extends TestSession {
+    
+    private static String baseDataSetName = "HCat_Test_Template";
+    private static String sourceCluster = "qe6blue";
+    private static String targetCluster = "qe9blue";
+    private static final int SUCCESS = 200;
+    private String dsActivationTime; 
+    private String dataSetName; 
+    private ConsoleHandle consoleHandle;
+    private WorkFlowHelper workFlowHelperObj = null;
+    private String tableName;
+    private String partition;
+    
+    @BeforeClass
+    public static void startTestSession() {
+        TestSession.start();
+    }
+    
+    @Before
+    public void setup() throws Exception{
+        String suffix = String.valueOf(System.currentTimeMillis());
+        dataSetName = "TestHCatPropSrcHDFSDiscMeta_" + suffix;
+        consoleHandle = new ConsoleHandle();
+        workFlowHelperObj = new WorkFlowHelper();
+        tableName = "HTF_Test_" + suffix;
+        //create table
+        HCatDataHandle.createTable(sourceCluster, tableName);
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = new Date();
+        partition = dateFormat.format(date);
+    }
+    
+    @Test 
+    public void testHcatPropSrcHDFSDiscMeta() throws Exception{
+        //create dataset
+        createDataSet();
+        
+        //activate dataset
+        consoleHandle.checkAndActivateDataSet(this.dataSetName);
+        dsActivationTime = GdmUtils.getCalendarAsString();
+        
+        //check for workflow
+        workFlowHelperObj.checkWorkFlow(this.dataSetName , "replication", this.dsActivationTime);
+        
+        //check if data got replicated
+        boolean status = HCatDataHandle.doesPartitionExist(targetCluster, tableName, partition);
+        assertTrue("The "+ tableName +" didn't get replicated from " + sourceCluster +
+                " to " + targetCluster + ".",status);
+    }
+    
+    public void createDataSet(){
+        StringBuilder dataSetBuilder = new StringBuilder(this.consoleHandle.getDataSetXml(this.baseDataSetName));
+        
+        //set replication to be metadata only
+        String pattern = "<HCatTargetType>Mixed</HCatTargetType>";
+        String replaceWith = "<HCatTargetType>HCatOnly</HCatTargetType>";
+        int indexOf = dataSetBuilder.indexOf(pattern);
+        dataSetBuilder.replace(indexOf, indexOf + pattern.length(), replaceWith);
+        
+        //set discovery interface to be HDFS
+        pattern = "<DiscoveryInterface>HCAT</DiscoveryInterface>";
+        replaceWith = "<DiscoveryInterface>HDFS</DiscoveryInterface>";
+        indexOf = dataSetBuilder.indexOf(pattern);
+        dataSetBuilder.replace(indexOf , indexOf + pattern.length(), replaceWith);
+        
+        //replace dummy table name with correct table name
+        pattern = "<HCatTableName>dummy_tablename</HCatTableName>";
+        replaceWith = "<HCatTableName>"+ tableName +"</HCatTableName>";
+        indexOf = dataSetBuilder.indexOf(pattern);
+        dataSetBuilder.replace(indexOf, indexOf + pattern.length(), replaceWith);
+        
+        //replace dummy path with correct path
+        pattern = "location=\"/data/daqdev/data/dummy_path/instancedate=%{date}\" type=\"data\"/>";
+        replaceWith = "location=\"/data/daqdev/data/"+ tableName +"/instancedate=%{date}\" type=\"data\"/>";
+        indexOf = dataSetBuilder.indexOf(pattern);
+        dataSetBuilder.replace(indexOf, indexOf + pattern.length(), replaceWith);
+        
+        String dataSetXml = dataSetBuilder.toString();
+        // replace basedatasetName with the new datasetname
+        dataSetXml = dataSetXml.replaceAll(this.baseDataSetName, this.dataSetName);
+        TestSession.logger.info("dataSetXml  = " + dataSetXml);
+        Response response = this.consoleHandle.createDataSet(this.dataSetName, dataSetXml);
+        assertTrue("Failed to create the dataset " + this.dataSetName ,  response.getStatusCode() == SUCCESS);
+        this.consoleHandle.sleep(5000);
+        
+    }
+    
+
+}
