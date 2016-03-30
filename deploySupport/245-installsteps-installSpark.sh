dc17eda4ef17766f59fac2cb54b44518be33f72d
Raj Chopde <rchopde@yahoo-inc.com>
2016-03-30 10:11:49 -0500

    Move spark loggin to conditional block

--- deploySupport/245-installsteps-installSpark.sh
+++ deploySupport/245-installsteps-installSpark.sh
@@ -3,18 +3,18 @@
 # inputs: cluster being installed
 # outputs: 0 on success
 
-if [ $# -ne 1 ]; then
-  echo "ERROR: need the cluster name"
-  exit 1
-fi
-
-CLUSTER=$1
-echo "INFO: Cluster being installed: $CLUSTER"
-echo "INFO: Spark node being installed: $gateway"
-echo "INFO: Spark version being installed: $SPARKVERSION"
-
-# Install Spark
+# Install Spark only when values are non default
 if [ $SPARKVERSION != none ] && [ $STACK_COMP_INSTALL_SPARK != false ]; then
+   if [ $# -ne 1 ]; then
+     echo "ERROR: need the cluster name"
+     exit 1
+   fi
+
+   CLUSTER=$1
+   echo "INFO: Cluster being installed: $CLUSTER"
+   echo "INFO: Spark node being installed: $gateway"
+   echo "INFO: Spark version being installed: $SPARKVERSION"
+
    cmd="yinst i yspark_yarn_install -br current \
    -set yspark_yarn_install.DOT_SIX=yspark_yarn-$SPARKVERSION \
    -set yspark_yarn_install.LATEST=yspark_yarn-$SPARKVERSION \
