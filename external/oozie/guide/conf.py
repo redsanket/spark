e8a2343dd9eb476f315de37a646519ef4acf0239
Joseph Catera <zhouyaoji@yahoo.com>
2016-04-13 18:45:44 -0700

    Removed extension that required rst2pdf.

--- external/oozie/guide/conf.py
+++ external/oozie/guide/conf.py
@@ -37,7 +37,6 @@ extensions = ['sphinx.ext.extlinks']
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc','rst2pdf.pdfbuilder']
 # Add any paths that contain templates here, relative to this directory.
 
 # Templates path for docs on YDN.
