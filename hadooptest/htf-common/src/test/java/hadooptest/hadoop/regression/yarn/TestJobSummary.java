8be0ceb225a33dfc3632737ff683797af09c3983
Patrick White <patwhite@yahoo-inc.com>
2016-05-12 11:18:32 -0500

    do not fail if jhs transfer takes too long

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestJobSummary.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestJobSummary.java
@@ -91,10 +91,13 @@ public class TestJobSummary extends YarnTestsBaseClass {
 			TestSession.logger.info("JobID " + jobId + " not found in historyserver done paths after 60 seconds!!");
                 }
 			
-		Assert.assertTrue(
-				"Not able to lookup:(" + jobId + ") "
-						+ responsesInDoneAndDoneIntermediateFolder.toString(),
-				responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
+		// gridci-1051, don't fail now, seeing intermittent cases where it takes longer than 60secs for the
+		// job's done files to transfer, Core Dev has said this is not a specific concern so we don't fail
+		// but will still log the warning
+		//Assert.assertTrue(
+				//"Not able to lookup:(" + jobId + ") "
+						//+ responsesInDoneAndDoneIntermediateFolder.toString(),
+				//responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
 	}
 
 	@Test
@@ -141,10 +144,14 @@ public class TestJobSummary extends YarnTestsBaseClass {
                         TestSession.logger.info("JobID " + jobId + " not found in historyserver done paths after 60 seconds!!");
                 }
 
-		Assert.assertTrue(
-				"Not able to lookup:(" + jobId + ") "
-						+ responsesInDoneAndDoneIntermediateFolder.toString(),
-				responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
+
+                // gridci-1051, don't fail now, seeing intermittent cases where it takes longer than 60secs for the
+                // job's done files to transfer, Core Dev has said this is not a specific concern so we don't fail
+                // but will still log the warning
+		//Assert.assertTrue(
+				//"Not able to lookup:(" + jobId + ") "
+						//+ responsesInDoneAndDoneIntermediateFolder.toString(),
+				//responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
 	}
 
 	@Test
@@ -266,6 +273,7 @@ public class TestJobSummary extends YarnTestsBaseClass {
 		TestSession.logger.info(responsesInDoneAndDoneIntermediateFolder
 				.toString());
 
+
                 // race condition when checking JHS, since we don't have a definite job "done"
                 // state to use, check JHS for job and break to the Assert if it's there, if not
                 // then we throw the Assert
@@ -273,10 +281,13 @@ public class TestJobSummary extends YarnTestsBaseClass {
                         TestSession.logger.info("JobID " + jobId + " not found in historyserver done paths after 60 seconds!!");
                 }
 
-		Assert.assertTrue(
-				"Not able to lookup:(" + jobId + ") "
-						+ responsesInDoneAndDoneIntermediateFolder.toString(),
-				responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
+                // gridci-1051, don't fail now, seeing intermittent cases where it takes longer than 60secs for the
+                // job's done files to transfer, Core Dev has said this is not a specific concern so we don't fail
+                // but will still log the warning
+		//Assert.assertTrue(
+				//"Not able to lookup:(" + jobId + ") "
+						//+ responsesInDoneAndDoneIntermediateFolder.toString(),
+				//responsesInDoneAndDoneIntermediateFolder.toString().contains(jobId));
 	}
 
 
