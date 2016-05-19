ab730022617395ebed5b98f8feb4293328b90710
philips <philips@yahoo-inc.com>
2016-05-19 11:11:45 -0500

    added required export of HADOOP_PREFIX and HADOOP_CONF_DIR

--- deploySupport/255-installsteps-setstickybit.sh
+++ deploySupport/255-installsteps-setstickybit.sh
@@ -8,6 +8,8 @@ fi
 
 cmd="export HADOOP_HOME=$GSHOME/hadoop/current ; \
      export JAVA_HOME=$JAVA_HOME ; \
+     export HADOOP_PREFIX=${yroothome}/share/hadoop ; \
+     export HADOOP_CONF_DIR=${yroothome}/conf/hadoop ; \
      kinit -k -t $HOMEDIR/hdfsqa/hdfsqa.dev.headless.keytab hdfsqa ; \
      $GSHOME/hadoop/current/bin/hadoop fs -ls / ; \
      $GSHOME/hadoop/current/bin/hadoop fs -chmod +t /tmp ; \
