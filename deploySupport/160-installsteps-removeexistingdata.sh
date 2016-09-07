d01975a156d59190872b28b284a75730f03ba36e
philips <philips@yahoo-inc.com>
2016-09-07 15:55:05 -0500

    add option to remove only yarn data

--- deploySupport/160-installsteps-removeexistingdata.sh
+++ deploySupport/160-installsteps-removeexistingdata.sh
@@ -1,4 +1,4 @@
-if [ "$REMOVEEXISTINGDATA" = true ]
+if [ "$REMOVEEXISTINGDATA" == "true" ]
 then
     echo == removing existing data on grid.
     for nnode in $namenode
@@ -11,6 +11,12 @@ then
     cp ${YINST_ROOT}/conf/hadoop/hadoopAutomation/cleangrid.sh  $scripttmp/cleangrid.sh
     fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
     fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER &&  sh /tmp/cleangrid.sh && rm /tmp/cleangrid.sh "
+
+elif [ "$REMOVE_YARN_DATA" == "true" ]
+    echo == removing existing yarn data on grid.
+    cp ${YINST_ROOT}/conf/hadoop/hadoopAutomation/cleangrid.sh  $scripttmp/cleangrid.sh
+    fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
+    fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER && sh /tmp/cleangrid.sh yarn && rm /tmp/cleangrid.sh "
 
 else
     echo == not removing existing data on grid.
