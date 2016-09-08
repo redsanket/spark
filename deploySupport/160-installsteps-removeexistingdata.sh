78c8182891040534593541def6e4a78a9eca2d63
Prakyath Jaladhar <prakyath@yahoo-inc.com>
2016-09-08 09:37:28 -0500

    [GRIDCI-1502] Hadoop 2.7 2.8 Deploy fix.

--- deploySupport/160-installsteps-removeexistingdata.sh
+++ deploySupport/160-installsteps-removeexistingdata.sh
@@ -13,6 +13,7 @@ then
     fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER &&  sh /tmp/cleangrid.sh && rm /tmp/cleangrid.sh "
 
 elif [ "$REMOVE_YARN_DATA" == "true" ]
+then
     echo == removing existing yarn data on grid.
     cp ${YINST_ROOT}/conf/hadoop/hadoopAutomation/cleangrid.sh  $scripttmp/cleangrid.sh
     fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
@@ -20,6 +21,5 @@ elif [ "$REMOVE_YARN_DATA" == "true" ]
 
 else
     echo == not removing existing data on grid.
-    
 
 fi
