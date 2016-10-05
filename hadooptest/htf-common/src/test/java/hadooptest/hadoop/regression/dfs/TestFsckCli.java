28b95a60ff263027becfa012b2e8fa6489f97a6d
Patrick White <patwhite@yahoo-inc.com>
2016-10-05 13:54:53 -0500

    debugging

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestFsckCli.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestFsckCli.java
@@ -327,8 +327,7 @@ public class TestFsckCli extends DfsTestsBaseClass {
                 dfsCommonCli.dfsadmin(EMPTY_ENV_HASH_MAP, Report.NO, "leave",
                                 ClearQuota.NO, SetQuota.NO, 0, ClearSpaceQuota.NO,
                                 SetSpaceQuota.NO, 0, PrintTopology.NO, EMPTY_FS_ENTITY);
-		GenericCliResponseBO responseBO = new dfsCommonCli.GenericCliResponseBO(); 
-                responseBO = dfsCommonCli.dfsadmin(EMPTY_ENV_HASH_MAP, Report.NO, "get",
+		GenericCliResponseBO responseBO = dfsCommonCli.dfsadmin(EMPTY_ENV_HASH_MAP, Report.NO, "get",
                                 ClearQuota.NO, SetQuota.NO, 0, ClearSpaceQuota.NO,
                                 SetSpaceQuota.NO, 0, PrintTopology.NO, EMPTY_FS_ENTITY);
 		if ( responseBO.response.contains("Safemode is OFF") )
