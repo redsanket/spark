5cac1bb33d7ef577785f4a60ac9ee8cb49276f74
Patrick White <patwhite@yahoo-inc.com>
2016-12-08 12:30:45 -0600

    add test branch to yooize install

--- deploySupport/oozie-install.sh
+++ deploySupport/oozie-install.sh
@@ -143,8 +143,9 @@ else
   exit 1
 fi
 
-yinst i -same -live -downgrade  $PACKAGE_VERSION_OOZIE
-yinst i -same -live -downgrade  $PACKAGE_VERSION_OOZIE_CLIENT
+# gridci-1708, add '-br test' to allow pulling dependencies that are on 'test'
+yinst i -same -live -downgrade -br test   $PACKAGE_VERSION_OOZIE
+yinst i -same -live -downgrade -br test   $PACKAGE_VERSION_OOZIE_CLIENT
 
 
 #
