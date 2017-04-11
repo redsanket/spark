a214f1f4d4a868e67356cad5067221e443b5c196
pat white <mcg30005@yahoo.com>
2017-04-11 11:29:50 -0500

    add core ssl certs

--- deploySupport/111-installsteps-check_ssl_certs.sh
+++ deploySupport/111-installsteps-check_ssl_certs.sh
@@ -5,6 +5,7 @@ CERT_HOME="/etc/ssl/certs/prod/_open_ygrid_yahoo_com"
 echo == verify Core SSL certs are in place
  
 fanout "if [ ! -d ${CERT_HOME} ] ; then 
+           echo "Going to create ${CERT_HOME}";
            mkdir -p ${CERT_HOME};
            chmod 755 ${CERT_HOME};
         fi"
