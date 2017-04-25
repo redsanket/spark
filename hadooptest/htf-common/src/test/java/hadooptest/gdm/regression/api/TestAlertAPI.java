95aa932ad1a827126894f7822d5ee56d481acf03
lawkp <lawkp@yahoo-inc.com>
2017-04-25 10:17:53 -0500

    modified sla to sla-miss in TestAlertAPI.java  [ GDM-1537  ]

--- hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/api/TestAlertAPI.java
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/api/TestAlertAPI.java
@@ -95,72 +95,81 @@ public class TestAlertAPI extends TestSession {
 	 * 
 	 */
 	public void executeAlertQuery(String... args) {
-		String facetType = args[0];
-		String alertType = args[1];
-		String severityType = args[2];
+	    String facetType = args[0];
+	    String alertType = args[1];
+	    String severityType = args[2];
 
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		String endDate = sdf.format(cal.getTime());
-		cal.add(Calendar.DAY_OF_MONTH, -5);
-		String startDate = sdf.format(cal.getTime());
-		TestSession.logger.info("start date = " + startDate  + "   end date = " + endDate);
+	    Calendar cal = Calendar.getInstance();
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+	    String endDate = sdf.format(cal.getTime());
+	    cal.add(Calendar.DAY_OF_MONTH, -5);
+	    String startDate = sdf.format(cal.getTime());
+	    TestSession.logger.info("start date = " + startDate  + "   end date = " + endDate);
 
-		String testURL = null;
-		if ( (args.length - 1 ) > 2) {
-			String dataSetName = args[3];
-			testURL = this.consoleHandle.getConsoleURL() + this.ALERT_API +  "severity=" + severityType + "&facet=" + facetType + "&starttime=" + startDate + "&endtime=" + endDate + "&type=" + alertType + "&datasetName=" + dataSetName;
-			TestSession.logger.info("testURL  = " + testURL);	
-		} else {
-			testURL = this.consoleHandle.getConsoleURL() + this.ALERT_API +  "severity=" + severityType + "&facet=" + facetType + "&starttime=" + startDate + "&endtime=" + endDate + "&type=" + alertType ;
-			TestSession.logger.info("testURL  = " + testURL);
-		}
-		Response response = given().cookie(this.cookie).get(testURL);
-                String responseString;
-		if ( response != null && !((responseString=response.getBody().asString()).equals("null")) ) {
-			
-			TestSession.logger.info("Response = " + responseString );
-			// convert responseString to json
-			JSONObject obj =  (JSONObject) JSONSerializer.toJSON(responseString.toString());
-			JSONArray jsonArray = null;
-			jsonArray = obj.getJSONArray("AlertEvents");
+	    String testURL = null;
+	    if ( (args.length - 1 ) > 2) {
+		String dataSetName = args[3];
+		testURL = this.consoleHandle.getConsoleURL() + this.ALERT_API +  "severity=" + severityType + "&facet=" + facetType + "&starttime=" + startDate + "&endtime=" + endDate + "&type=" + alertType + "&datasetName=" + dataSetName;
+		TestSession.logger.info("testURL  = " + testURL);
+	    } else {
+		testURL = this.consoleHandle.getConsoleURL() + this.ALERT_API +  "severity=" + severityType + "&facet=" + facetType + "&starttime=" + startDate + "&endtime=" + endDate + "&type=" + alertType ;
+		TestSession.logger.info("testURL  = " + testURL);
+	    }
+	    Response response = given().cookie(this.cookie).get(testURL);
+	    String responseString;
+	    if ( response != null && !((responseString=response.getBody().asString()).equals("null")) ) {
 
-			if (jsonArray.size() > 0 && jsonArray != null) {
-				Iterator iterator = jsonArray.iterator();
-				while (iterator.hasNext()) {
-					JSONObject jsonObject = (JSONObject) iterator.next();
-					TestSession.logger.info("AlertEvents  = " + jsonObject.toString());
+		TestSession.logger.info("Response = " + responseString );
 
-					// check for facet
-					String facetName = jsonObject.getString("facetName");
-					assertTrue("Expected " + facetType + " but got " + facetName , facetType.equals(facetName) );
+		// convert responseString to json
+		JSONObject obj =  (JSONObject) JSONSerializer.toJSON(responseString.toString());
+		Object tObj = obj.get("AlertEvents");
+		if ( tObj instanceof JSONArray) {
+		    JSONArray jsonArray = null;
+		    jsonArray = obj.getJSONArray("AlertEvents");
+		    if (jsonArray.size() > 0 && jsonArray != null) {
 
-					// check serverity type
-					String serverity = jsonObject.getString("severity");
-					assertTrue("Expected " + severityType + " but got " + serverity , severityType.equals(serverity));
+			Iterator iterator = jsonArray.iterator();
+			while (iterator.hasNext()) {
+			    JSONObject jsonObject = (JSONObject) iterator.next();
+			    TestSession.logger.info("AlertEvents  = " + jsonObject.toString());
 
-					if(alertType.equals("sla")) {
-						String name = jsonObject.getString("name");
-						assertTrue("Expected availability, but got " + name  , name.equals("sla"));
-					} else if (alertType.equals("system-warning")) {
-						String name = jsonObject.getString("name");
-						assertTrue("Expected availability, but got " + name  , name.equals("system-warning"));
+			    // check for facet
+			    String facetName = jsonObject.getString("facetName");
+			    assertTrue("Expected " + facetType + " but got " + facetName , facetType.equals(facetName) );
 
-						String context = jsonObject.getString("context");
-						assertTrue("Expected Memory Threshold Exceeded , but got " + context , context.equals("Memory Threshold Exceeded"));
+			    // check serverity type
+			    String serverity = jsonObject.getString("severity");
+			    assertTrue("Expected " + severityType + " but got " + serverity , severityType.equals(serverity));
 
-					} else if (alertType.equals("availability")) {
-						String name = jsonObject.getString("name");
-						assertTrue("Expected availability, but got " + name  , name.equals("availability"));
-					}
-				}
-			} else {
-				TestSession.logger.info("Empty AlertEvents array, no alert were found for " + testURL  + "  query.");
+			    if(alertType.equals("sla-miss")) {
+				String name = jsonObject.getString("name");
+				assertTrue("Expected availability, but got " + name  , name.equals("sla-miss"));
+			    } else if (alertType.equals("system-warning")) {
+				String name = jsonObject.getString("name");
+				assertTrue("Expected availability, but got " + name  , name.equals("system-warning"));
+
+				String context = jsonObject.getString("context");
+				assertTrue("Expected Memory Threshold Exceeded , but got " + context , context.equals("Memory Threshold Exceeded"));
+
+			    } else if (alertType.equals("availability")) {
+				String name = jsonObject.getString("name");
+				assertTrue("Expected availability, but got " + name  , name.equals("availability"));
+			    }
 			}
-		} else {
-			// TODO : this else block will be removed once the response returning null is fixed.
-			TestSession.logger.info("Didn't get any response for " + testURL  + "  query.");
+		    }else {
+			TestSession.logger.info("Empty AlertEvents array, no alert were found for " + testURL  + "  query.");
+		    }
+		} else if ( tObj instanceof JSONObject) {
+		    TestSession.logger.info("***************** not a json array, its  a simple json Object*************");
+		    JSONObject jObj = obj.getJSONObject("AlertEvents");
+		    String facetName = jObj.getString("facetName");
+		    assertTrue("Expected " + facetType + " but got " + facetName , facetType.equals(facetName) );
 		}
+	    } else {
+		// TODO : this else block will be removed once the response returning null is fixed.
+		TestSession.logger.info("Didn't get any response for " + testURL  + "  query.");
+	    }
 	}
 }
