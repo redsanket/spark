baad211accbeb1d1a51cb316757524f8eae4e236
pat white <patwhite@oath.com>
2017-08-18 17:24:08 -0500

    fix

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestBackwardCompatibility.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/yarn/TestBackwardCompatibility.java
@@ -412,9 +412,9 @@ public class TestBackwardCompatibility extends YarnTestsBaseClass {
 		// gridci-2383, property changed in 2.8.1 to include 'final' so need to leave
 		// 2.7 and 2.8.0 as-is but check for 'final' block in 2.8.1 and forward
 		String clusterVersion = TestSession.cluster.getVersion();
-		TestSession.logger.debug("\nHadoop cluster version is: " + clusterVersion());
+		TestSession.logger.debug("\nHadoop cluster version is: " + clusterVersion);
 		String stringToLookFor;
-		if (clusterVersion().contains("2.8.0") || clusterVersion().contains("2.7."))
+		if (clusterVersion.contains("2.8.0") || clusterVersion.contains("2.7."))
 		{
 			stringToLookFor = "<property><name>mapreduce.job.working.dir</name><value>/tmp</value><source>because mapred.working.dir is deprecated</source><source>job.xml</source></property>";
 		}
