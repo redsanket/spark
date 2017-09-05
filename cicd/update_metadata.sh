85f6217ab3e357bbee24cf206f96fe478f0e5517
Raj Chopde <rchopde@yahoo-inc.com>
2017-09-05 17:15:54 -0500

    [GRIDCI-2338] - Add trigger to hadoop artifactory job

--- cicd/update_metadata.sh
+++ cicd/update_metadata.sh
@@ -7,21 +7,20 @@ if [ $# -eq 0 ]
     echo "No branch supplied"
     exit -1
 fi
-
+git checkout $1
 echo "Creating build info..."
-echo "Screwdriver: ${SCREWDRIVER}" > ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "Build Description: `cat ${SOURCE_DIR}/RELEASE`" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "BUILD_URL=${BUILD_URL}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "REPO=${GIT_URL}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "COMMIT=${GIT_COMMIT}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "BRANCH=${GIT_BRANCH}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "JOB_NAME=${JOB_NAME}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
-echo "BUILD_NUMBER=${BUILD_NUMBER}" >> ${SOURCE_DIR}/cicd/gridci-metadata.txt
+echo "Screwdriver: ${SCREWDRIVER}" > ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "BUILD_URL=${BUILD_URL}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "REPO=${GIT_URL}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "COMMIT=${GIT_COMMIT}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "BRANCH=${GIT_BRANCH}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "JOB_NAME=${JOB_NAME}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
+echo "BUILD_NUMBER=${BUILD_NUMBER}" >> ${WORKSPACE}/cicd/gridci-metadata.txt
 
 echo "Updating cicd/gridci-metadata.txt file..."
 git branch
 git status
 git add . && git commit -m "Updating cicd metadata" cicd/gridci-metadata.txt
-git push origin $1
+git push $2 $1
 rc=$?; if [[ $rc != 0 ]]; then exit -1; fi
 echo "Metadata update completed!"
