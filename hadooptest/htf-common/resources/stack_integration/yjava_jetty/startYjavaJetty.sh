8f91d6d181fec77ad9571eccfc7136579a82cb21
Prakyath Jaladhar <prakyath@yahoo-inc.com>
2017-10-03 14:00:16 -0500

    Revert "GRIDCI-2582 : yjava_jetty is not starting on integration clusters"

--- hadooptest/htf-common/resources/stack_integration/yjava_jetty/startYjavaJetty.sh
+++ hadooptest/htf-common/resources/stack_integration/yjava_jetty/startYjavaJetty.sh
@@ -2,10 +2,6 @@
 
 set +x
 
-yinst i -downgrade -live  yjava_jdk-1.8.0_60.51
-yinst i -downgrade  yhdrs-1.24.3 yhdrs_data-2.1.4 yjava_yhdrs-0.32.22
-yinst i -downgrade -live yjava_jetty-9.2.15.v20160210_274
-
 SERVICE="yjava_jetty";
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
