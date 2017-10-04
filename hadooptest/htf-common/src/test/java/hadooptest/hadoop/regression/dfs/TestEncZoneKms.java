5cfd6f6f361fe62dc75e3f7e169a97f78cf16c92
pat white <patwhite@oath.com>
2017-10-04 13:35:05 -0500

    fix ez nums

--- hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestEncZoneKms.java
+++ hadooptest/htf-common/src/test/java/hadooptest/hadoop/regression/dfs/TestEncZoneKms.java
@@ -43,7 +43,6 @@ public class TestEncZoneKms extends DfsTestsBaseClass {
         private static String TEST_FOLDER_ON_HDFS_REFERRED_TO_AS_BASE_DIR3 = "/tmp/hadoop3/BaseDirInEz3/";
         private static String TEST_FOLDER_ON_HDFS_REFERRED_TO_AS_BASE_DIR4 = "/tmp/hadoop3/BaseDirInEz4/";
         private static String TEST_FOLDER_ON_HDFS_REFERRED_TO_AS_BASE_DIR5 = "/tmp/hadoop3/BaseDirInEz5/";
-        private static String TEST_FOLDER_ON_HDFS_REFERRED_TO_AS_BASE_DIR5 = "/tmp/hadoop3/BaseDirInEz5/";
         private static String TEST_FOLDER_ON_HDFS_REFERRED_TO_AS_BASE_DIR6 = "/tmp/hadoop3/BaseDirInEz6/";
         private static String localCluster = System.getProperty("CLUSTER_NAME");
         private static String SSH_OPTS_1 = "-o StrictHostKeyChecking=no";
