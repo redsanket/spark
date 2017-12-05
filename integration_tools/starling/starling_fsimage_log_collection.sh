44c03e5f36af811cec37c9472ad1b9a879028ee3
Jihui Yang <jihuiy@yahoo-inc.com>
2017-12-05 14:46:08 -0600

    starling integration log collection scripts

--- integration_tools/starling/starling_fsimage_log_collection.sh
+++ integration_tools/starling/starling_fsimage_log_collection.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 set -x
+source ./library/utils.sh
 
-GRIDNAME=`hostname`
-DEST_HDFS="hdfs://${GRIDNAME}:8020"
-HADOOP_HOME=/home/gs/hadoop/current
-JAVA_HOME=/home/gs/java/jdk64/current/
-HADOOP_CONF_DIR=/home/gs/conf/current/
-HDFS="$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR"
+DEST_GRID="openqe95blue"
+DEST_HDFS="hdfs://`yinst range -ir "(@grid_re.clusters.${DEST_GRID}.namenode)"`"
 LOGTYPE="fsimage"
-GRID=`echo $DEST_HDFS | cut -d . -f 1 | cut -d '/' -f 3`
 LOGPATH="/grid/0/hadoop/var/hdfs/name/current/"
+GRIDNAME=`hostname | cut -d . -f 1 | cut -d '/' -f 3`
 
 DAYS=0
 echo "`date +%FT%T` info: Collecting fsimage logs for today and ${DAYS} days prior to today"
-
-/usr/kerberos/bin/kinit -k -t /homes/hadoopqa/hadoopqa.dev.headless.keytab hadoopqa@YGRID.YAHOO.COM
 
 fsimage_ctime=`stat -c %Y ${LOGPATH}VERSION`
 if [ ! -z "$fsimage_ctime" ] ; then
@@ -23,12 +18,7 @@ if [ ! -z "$fsimage_ctime" ] ; then
     if [ $((now-fsimage_ctime)) -gt 3600 ] ; then
         echo "`date +%FT%T` info: +++$+++"
         while [ ${DAYS} -ge 0 ]; do
-            if [ ${DAYS} -eq 0 ]; then
-                ENDDATE=`date +%F -d "+$((${DAYS} + 1)) days"`
-            else
-                ENDDATE=`date +%F -d "-$((${DAYS} - 1)) days"`
-            fi
-            STARTDATE=`date +%F -d "-${DAYS} days"`
+            read STARTDATE ENDDATE < <(get_dates ${DAYS})
             fsimage_files=`find ${LOGPATH} -type f -newermt ${STARTDATE} ! -newermt ${ENDDATE} | grep -E "fsimage_[0-9]*" | grep -v "md5" | rev | cut -d / -f 1 | rev`
             if [[ -z $fsimage_files ]]; then
                 echo "`date +%FT%T` info: skipping.. no fsimage files present on ${STARTDATE}"
@@ -36,26 +26,10 @@ if [ ! -z "$fsimage_ctime" ] ; then
                 continue
             fi
             dest_pfx=${DEST_HDFS}/projects/starling/hadoopqa/logs/fsimage/${STARTDATE}
-            $HDFS dfs -test -d ${dest_pfx} || $HDFS dfs -Dfs.permissions.umask-mode=027 -mkdir -p ${dest_pfx}
-            if [ ! $? -eq 0 ]; then
-                echo "`date +%FT%T` error: failed to create folder ${dest_pfx} on HDFS"
-                exit 1;
-            fi
+            create_folder ${dest_pfx}
             for fsimage_file in ${fsimage_files[@]}; do
                 HOUR=`date -r ${LOGPATH}${fsimage_file} +%T | cut -d : -f 1`
-                cp ${LOGPATH}${fsimage_file} /grid/0/tmp/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}"
-                gzip -fv /grid/0/tmp/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}"
-                if [ ! $? -eq 0 ]; then
-                    echo "`date +%FT%T` error: failed to generate gzip file from /grid/0/tmp/'${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}'"
-                    exit 1;
-                fi
-                $HDFS dfs -test -f ${dest_pfx}/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}".gz || $HDFS dfs -copyFromLocal /grid/0/tmp/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}".gz $dest_pfx/.
-                if [ ! $? -eq 0 ]; then
-                    echo "`date +%FT%T` error: failed to upload gzip file /grid/0/tmp/'${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}' to HDFS"
-                    exit 1;
-                fi
-                $HDFS dfs -chmod -R 750 ${dest_pfx}/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}".gz
-                rm /grid/0/tmp/"${GRID}-${LOGTYPE}-${STARTDATE}-${HOUR}".gz
+                copy_to_hdfs ${LOGPATH}${fsimage_file} ${dest_pfx} ${GRIDNAME}-${LOGTYPE}-${STARTDATE}-${HOUR} ${LOGTYPE}
             done
             DAYS=$[${DAYS}-1]
         done
