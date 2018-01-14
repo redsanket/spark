b0b8d9979532398c4579f020cfdacb0e89cef2ae
Raj Chopde <rchopde@yahoo-inc.com>
2018-01-14 15:37:16 -0600

    provide HADOOP_HOME to shell commands

--- deploySupport/101-installsteps-killprocess.sh
+++ deploySupport/101-installsteps-killprocess.sh
@@ -8,11 +8,11 @@ then
 	# Step 1a: kill job tracker script.
 	echo "Step 1a: kill job tracker script, if running."
 	# echo ${yrootHadoopMapred}/sbin/yarn-daemon.sh --config $yroothome/conf/hadoop  stop resourcemanager | $SSH $jobtrackernode su - $MAPREDUSER
-        echo ${HADOOP_YARN_HOME}/bin/yarn --daemon stop resourcemanager | $SSH $jobtrackernode su - $MAPREDUSER
+        echo ${HADOOP_HOME}/bin/yarn --daemon stop resourcemanager | $SSH $jobtrackernode su - $MAPREDUSER
         # echo ${yrootHadoopMapred}/sbin/mr-jobhistory-daemon.sh --config $yroothome/conf/hadoop  stop historyserver | $SSH $jobtrackernode su - $MAPREDUSER
-	echo ${HADOOP_YARN_HOME}/bin/mapred --daemon stop historyserver | $SSH $jobtrackernode su - $MAPREDUSER
+	echo ${HADOOP_HOME}/bin/mapred --daemon stop historyserver | $SSH $jobtrackernode su - $MAPREDUSER
         # echo ${yrootHadoopMapred}/sbin/yarn-daemon.sh --config $yroothome/conf/hadoop  stop timelineserver | $SSH $jobtrackernode su - $MAPREDUSER
-	echo ${HADOOP_YARN_HOME}/bin/yarn --daemon stop timelineserver | $SSH $jobtrackernode su - $MAPREDUSER
+	echo ${HADOOP_HOME}/bin/yarn --daemon stop timelineserver | $SSH $jobtrackernode su - $MAPREDUSER
 
         # removing su - $HDFSUSER as we might have started the jt as mapred/mapredqa users
         JAVA_HOME="$GSHOME/java8/jdk64/current"
@@ -21,7 +21,7 @@ then
 export JAVA_HOME=$JAVA_HOME && \
 export HADOOP_PREFIX=${yroothome}/share/hadoop && \
 export HADOOP_CONF_DIR=${yroothome}/conf/hadoop && \
-${yrootHadoopCurrent}/sbin/hadoop-daemon.sh stop namenode '"
+${HADOOP_HOME}/bin/hdfs --daemon stop namenode '"
 
 	echo "Step 1c: stop data nodes, if running."
 	fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/datanode-script.sh __HOSTNAME__:/tmp/datanode-script.sh" "$SLAVELIST"
