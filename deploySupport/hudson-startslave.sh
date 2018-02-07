4e0e77bea7c9a6d3b333d02767c1a1a780899225
Raj Chopde <rchopde@yahoo-inc.com>
2018-02-07 09:37:35 -0600

    Revert "use full coretree version in the pkg vars"
    This reverts commit 6809cd3fca104a980736bfaef9261452b4fa57b6.

--- deploySupport/hudson-startslave.sh
+++ deploySupport/hudson-startslave.sh
@@ -177,8 +177,8 @@ if [ -n "$HADOOP_RELEASE_TAG" ]; then
   
     # explicitly set coretree and hadoopCommonsDaemon for rhel7
     # must do this to get the rhel7 compat version on test branch
-    # Again, rhel6 installs should be good with this
-    HADOOP_INSTALL_STRING+=" hadoopcoretree-$FULLHADOOPVERSION hadoopCommonsDaemon "
+    #phw HADOOP_INSTALL_STRING+=" hadoopcoretree hadoopCommonsDaemon-1.0.4.1707172207 "
+    HADOOP_INSTALL_STRING+=" hadoopCommonsDaemon-1.0.4.1707172207 "
 
     if [ -n "$SPARK_SHUFFLE_VERSION" ]; then
         HADOOP_INSTALL_STRING+=" yspark_yarn_shuffle-$SPARK_SHUFFLE_VERSION"
