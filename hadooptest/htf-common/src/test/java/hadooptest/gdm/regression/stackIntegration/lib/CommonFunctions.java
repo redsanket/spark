11825b9bb1f7e2142522df83860eb26faee3d356
mayankjaglan <mayankjaglan@oath.com>
2018-11-08 15:08:04 -0600

    remove debug print lines

--- hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/stackIntegration/lib/CommonFunctions.java
+++ hadooptest/htf-common/src/test/java/hadooptest/gdm/regression/stackIntegration/lib/CommonFunctions.java
@@ -437,50 +437,32 @@ public class CommonFunctions {
 				testList.add(testIntHive);
 			}
 		}
-		TestSession.logger.info("mjaglan hbase - 0");
 		if (currentStackTestComponent.contains("hbase")) {
-			TestSession.logger.info("mjaglan hbase - 1");
 			StackComponent hbaseStackComponent = stackComponentMap.get("hbase");
-			TestSession.logger.info("mjaglan hbase - 2");
 			Callable<String> testHbaseComponent = null;
-			TestSession.logger.info("mjaglan hbase - 3");
 			if (hbaseStackComponent != null) {
-				TestSession.logger.info("mjaglan hbase - 4");
 				TestSession.logger.info("hbase hostname  = " + hbaseStackComponent.getHostName()
 						+ "  hbaseStackComponent = " + hbaseStackComponent.toString() + "  script location = "
 						+ hbaseStackComponent.getScriptLocation());
-				TestSession.logger.info("mjaglan hbase - 5");
 				testHbaseComponent = new TestIntHBase(hbaseStackComponent, nNodeName, this.getClusterName());
-				TestSession.logger.info("mjaglan hbase - 6");
 				testList.add(testHbaseComponent);
-				TestSession.logger.info("mjaglan hbase - 7");
 			}
 		}
-		TestSession.logger.info("mjaglan oozie - 0");
 		if (currentStackTestComponent.contains("oozie")) {
-			TestSession.logger.info("mjaglan oozie - 1");
 			StackComponent oozieStackComponent = stackComponentMap.get("oozie");
-			TestSession.logger.info("mjaglan oozie - 2");
 			Callable<String> testIntOozie = null;
 			if (oozieStackComponent != null) {
-				TestSession.logger.info("mjaglan oozie - 3");
 				try {
 					org.apache.hadoop.conf.Configuration configuration = this.getNameConfForRemoteFS(nNodeName);
-					TestSession.logger.info("mjaglan oozie - 4");
 					testIntOozie = new TestIntOozie(oozieStackComponent, oozieStackComponent.getHostName(),
 							configuration);
-					TestSession.logger.info("mjaglan oozie - 5");
 					testList.add(testIntOozie);
-					TestSession.logger.info("mjaglan oozie - 6");
 				} catch (IOException e) {
 					TestSession.logger.error("Failed to create configuration " + e);
 					e.printStackTrace();
 				}
-				TestSession.logger.info("mjaglan oozie - 7");
 			}
 		}
-		TestSession.logger.info("mjaglan starling - 0");
-		
 		if (currentStackTestComponent.contains("starling")) {
 			StackComponent starlingStackComponent = stackComponentMap.get("starling");
 			Callable<String> testStarlingComponent = null;
