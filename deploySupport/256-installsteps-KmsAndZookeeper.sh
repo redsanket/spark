aa877af2e53b46b9a5813d268238d0ab3e847762
pat white <patwhite@oath.com>
2019-01-09 15:18:09 -0600

    need to check for success case still

--- deploySupport/256-installsteps-KmsAndZookeeper.sh
+++ deploySupport/256-installsteps-KmsAndZookeeper.sh
@@ -337,7 +337,7 @@ sleep 30
 # do not exhibit the 'incomplete groups' problem
 #
 CURL_KEY=`$SSH $kmsnode  "kinit -kt /etc/grid-keytabs/$kmsnodeshort.dev.service.keytab hdfs/$kmsnode; curl --tlsv1.2 --negotiate -u: -k https://$kmsnode:4443/kms/v1/key/hitusr_4/_metadata"`
-if [[ ! "$CURL_KEY" =~ "hitusr_4 in ykeykey store" ]] && [[ ! "$CURL_KEY" =~ "hitusr_4 in ykeykey store" ]]; then
+if [[ ! "$CURL_KEY" =~ "AES/CTR/NoPadding" ]] && [[ ! "$CURL_KEY" =~ "hitusr_4 in ykeykey store" ]]; then
   echo "Failed to get key info, KMS or ZK service may not be running!"
   exit 1
 fi
