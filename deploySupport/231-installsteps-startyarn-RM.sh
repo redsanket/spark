53469672e6ca2d43fb5981e609e3ebb332f00b63
Shawna Martell <shawnam@verizonmedia.com>
2019-02-21 12:20:53 -0600

    See if this will install without any additional repos

--- deploySupport/231-installsteps-startyarn-RM.sh
+++ deploySupport/231-installsteps-startyarn-RM.sh
@@ -28,7 +28,7 @@ ssh  $jobtrackernode su - $MAPREDUSER
     slavefanout "sh -x /tmp/setup_nm_dockerd_check_script.sh" "$SLAVELIST"
 
     # Install runc on all the nodemanagers that are not RHEL6
-    slavefanout '[[ $(cut -d" " -f7 < /etc/redhat-release) =~ ^6. ]] || yum -y --enablerepo=latest-rhel-7-server-extras-rpms install runc'
+    slavefanout '[[ $(cut -d" " -f7 < /etc/redhat-release) =~ ^6. ]] || yum -y install runc'
 
     # Setup cgroups on the worker nodes
     tmpsetupfile=/tmp/setup_nm_cgroups.sh.$$
