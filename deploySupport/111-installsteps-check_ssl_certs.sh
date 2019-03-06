f2d3bfa719192e8aec6f2c4cb04c62ab5fa112b8
philips <philips@oath.com>
2019-03-06 15:39:04 -0600

    update

--- deploySupport/111-installsteps-check_ssl_certs.sh
+++ deploySupport/111-installsteps-check_ssl_certs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set +x
+
 # gridci-2138/gridci-2292, copy jks files from devadm102:/grid/3/dev/ygrid_certs_flubber/*.jks
 # to each  cluster node, needed to support webui https/ssl connections on any server run on
 # said node, includes certs for Core and HBase
@@ -19,22 +21,23 @@ CERT_REFERENCE_PATH="/grid/3/dev/ygrid_certs_flubber/*"
 CERT_HOME="/etc/ssl/certs/prod/_open_ygrid_yahoo_com"
 
 echo == verify Core SSL certs are in place
- 
+
+set -x
 fanout "if [ ! -d ${CERT_HOME} ] ; then 
            echo "Going to create ${CERT_HOME}";
            mkdir -p ${CERT_HOME};
            chmod 755 ${CERT_HOME};
         fi"
+set +x
 
 NODES=`yinst range -ir @grid_re.clusters.$CLUSTER,@grid_re.clusters.$CLUSTER.gateway`
 for NODE in $NODES; do
-
-  $SSH $ADM_HOST "sudo $SCP $CERT_REFERENCE_PATH $NODE:$CERT_HOME"
-  if [ $? -ne 0 ]; then
-    echo "Error: node $NODE failed to scp JKS files!"
-    exit 1
-  fi
-
+    echo "$SSH $ADM_HOST \"sudo $SCP $CERT_REFERENCE_PATH $NODE:$CERT_HOME\""
+    $SSH $ADM_HOST "sudo $SCP $CERT_REFERENCE_PATH $NODE:$CERT_HOME"
+    if [ $? -ne 0 ]; then
+        echo "Error: node $NODE failed to scp JKS files!"
+        exit 1
+    fi
 done
 
 ###### TODO - pdcp would be more efficient because it will parallel scp instead
