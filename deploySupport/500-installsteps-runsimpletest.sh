b5cf3fbcbdac52a000c0a5797d04ce8e4becb7b5
philips <philips@oath.com>
2019-03-07 09:28:21 -0600

    update

--- deploySupport/500-installsteps-runsimpletest.sh
+++ deploySupport/500-installsteps-runsimpletest.sh
@@ -1,37 +1,38 @@
-if [ "$RUNSIMPLETEST" = false -o -z "$RUNSIMPLETEST" ]
-then
-	echo Running no tests for ${cluster}.
+set +x
+if [ "$RUNSIMPLETEST" = false -o -z "$RUNSIMPLETEST" ]; then
+	echo "Running no tests for ${cluster}."
 else
-   if [ "$RUNSIMPLETEST" = true ] ; then
+    echo "Running tests for ${cluster}."
+    if [ "$RUNSIMPLETEST" = true ] ; then
 	testfile="$base/shorttest-rw.sh"
 	testname=shorttest-rw.sh
-   elif [ -f "$RUNSIMPLETEST" ] ; then
+    elif [ -f "$RUNSIMPLETEST" ] ; then
 	testfile=${RUNSIMPLETEST}
 	testname=${RUNSIMPLETEST}
-   elif [ -f "$base/$RUNSIMPLETEST" ] ; then
+    elif [ -f "$base/$RUNSIMPLETEST" ] ; then
 	testfile="$base/$RUNSIMPLETEST"
 	testname=${RUNSIMPLETEST}
-   fi
+    fi
 
-   export yarnProcessor=yarn
-#  [ -n  "$PREFERREDJOBPROCESSOR" ] && export yarnProcessor=$PREFERREDJOBPROCESSOR
-   if [ "$REMOVEEXISTINGDATA" = true ]
-   then
-      writeenabledflag=-w
-   else
-      writeenabledflag=
-      sleep 180
-   fi
+    export yarnProcessor=yarn
+    # [ -n  "$PREFERREDJOBPROCESSOR" ] && export yarnProcessor=$PREFERREDJOBPROCESSOR
+    if [ "$REMOVEEXISTINGDATA" = true ]; then
+        writeenabledflag=-w
+    else
+        writeenabledflag=
+        sleep 180
+    fi
 
-   # Run $cluster-shorttest-rw.sh from the gateway, passing the primary namenode value.
-   echo === running a simple test on  gateway=$gateway
-   set +x
-   scp "$testfile"  ${gateway}:/tmp/${cluster}-${testname}
-   set -x
+    # Run $cluster-shorttest-rw.sh from the gateway, passing the primary namenode value.
+    echo "=== running a simple test on  gateway=$gateway"
+    set -x
+    scp "$testfile"  ${gateway}:/tmp/${cluster}-${testname}
+    set +x
 
-   logfile="deploy_${cluster}_test.log"
-   # echo  GSHOME=$GSHOME yroothome=$yroothome sh /tmp/${cluster}-${testname} -c $cluster -N "'$namenode'" ${writeenabledflag} -P "${yarnProcessor}" -u "${MAPREDUSER}" | ssh $gateway su - hadoopqa
-   ssh $gateway "su - hadoopqa -c '\
+    logfile="deploy_${cluster}_test.log"
+    set -x
+    # echo  GSHOME=$GSHOME yroothome=$yroothome sh /tmp/${cluster}-${testname} -c $cluster -N "'$namenode'" ${writeenabledflag} -P "${yarnProcessor}" -u "${MAPREDUSER}" | ssh $gateway su - hadoopqa
+    ssh $gateway "su - hadoopqa -c '\
 export GSHOME=$GSHOME && \
 export yroothome=$yroothome && \
 export HADOOP_27=$HADOOP_27 && \
@@ -40,15 +41,15 @@ sh /tmp/${cluster}-${testname} \
 -c $cluster -N $namenode ${writeenabledflag} -P ${yarnProcessor} -u ${MAPREDUSER} \
 > /tmp/$logfile 2>&1 \
 '"
-   RC=$?
-   echo "RC='$RC'"
+    RC=$?
+    set +x
 
-   set +x
-   scp ${gateway}:/tmp/$logfile /grid/0/tmp/scripts.deploy.$cluster/$logfile
-   set -x
+    set -x
+    scp ${gateway}:/tmp/$logfile /grid/0/tmp/scripts.deploy.$cluster/$logfile
+    set +x
 
-   if [ "$RC" -ne 0 ]; then
-       echo "500-installsteps-runsimpletest.sh: deployment verification test failed! exiting...."
-       exit 1
-   fi
+    if [ "$RC" -ne 0 ]; then
+        echo "500-installsteps-runsimpletest.sh: deployment verification test failed! exiting...."
+        exit 1
+    fi
 fi
