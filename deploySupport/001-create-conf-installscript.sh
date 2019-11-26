312efb4b8326b34efc069799ea0d3b51d27d521b
Eric Badger <ebadger@verizonmedia.com>
2019-11-26 17:56:15 +0000

    remove unnecessary if statement

--- deploySupport/001-create-conf-installscript.sh
+++ deploySupport/001-create-conf-installscript.sh
@@ -176,17 +176,10 @@ filename="/grid/0/tmp/deploy.$cluster.confoptions.sh"
         echo "$yinst set -root ${yroothome} \\"
         echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_ALLOWED_RUNTIMES=default"
     else
-        if [[ ${DOCKER_IMAGE_TAG_TO_USE} == 'rhel7' || ${DOCKER_IMAGE_TAG_TO_USE} == 'rhel6' ]]; then
-            echo "echo get latest ${DOCKER_IMAGE_TAG_TO_USE}:current tag image"
-            echo "$yinst set -root ${yroothome} \\"
-            echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_IMAGE_NAME=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE} \\"
-            echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_ALLOWED_IMAGES=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE}"
-        else
-            echo "echo get specific ${DOCKER_IMAGE_TAG_TO_USE} tag image"
-            echo "$yinst set -root ${yroothome} \\"
-            echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_IMAGE_NAME=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE} \\"
-            echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_ALLOWED_IMAGES=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE}"
-        fi
+        echo "echo get latest ${DOCKER_IMAGE_TAG_TO_USE} image"
+        echo "$yinst set -root ${yroothome} \\"
+        echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_IMAGE_NAME=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE} \\"
+        echo "    $confpkg.TODO_YARN_NODEMANAGER_RUNTIME_LINUX_DOCKER_ALLOWED_IMAGES=docker-registry.ops.yahoo.com:4443/hadoop-user-images/hadoop/${DOCKER_IMAGE_TAG_TO_USE}"
     fi
 
     # Hack to deal with split client/server configs. Currently QE does not
