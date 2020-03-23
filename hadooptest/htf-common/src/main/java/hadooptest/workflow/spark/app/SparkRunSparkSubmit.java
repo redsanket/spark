8725eb3cf9b7cd51d9f42852c4d0b9cb459b0f34
schintap <schintap@verizonmedia.com>
2020-03-23 10:27:44 -0400

    set delegation token kafka creds to false

--- hadooptest/htf-common/src/main/java/hadooptest/workflow/spark/app/SparkRunSparkSubmit.java
+++ hadooptest/htf-common/src/main/java/hadooptest/workflow/spark/app/SparkRunSparkSubmit.java
@@ -512,6 +512,8 @@ public class SparkRunSparkSubmit extends App {
         }
 
         cmd.add("--conf");
+        cmd.add("spark.security.credentials.kafka.enabled=false");
+        cmd.add("--conf");
         cmd.add("spark.executorEnv.JAVA_HOME=" + HadooptestConstants.Location.JDK64);
         cmd.add("--conf");
         cmd.add("spark.yarn.appMasterEnv.JAVA_HOME=" + HadooptestConstants.Location.JDK64);
