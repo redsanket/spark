55fd2a326b0c7305a4da6ce12457d83bfbfa01eb
Joshua Martell <jmartell@yahoo-inc.com>
2020-05-19 12:11:23 -0500

    Cleanup docker bits

--- deploySupport/setup_nm_dockerd_check_script.sh
+++ deploySupport/setup_nm_dockerd_check_script.sh
@@ -9,46 +9,15 @@
 HOSTNAME=`hostname`
 OS_VER=`cat /etc/redhat-release | cut -d' ' -f7`
 CLUSTER=`hostname | cut -d- -f1`
-DOCKER_YINST_SET=`/usr/local/bin/yinst set -root /home/gs/gridre/yroot."$CLUSTER" | grep TODO_YARN_NODEMANAGER_RUNTIME_LINUX_ALLOWED_RUNTIMES | cut -d: -f2`
+ALLOWED_RUNTIMES=`/usr/local/bin/yinst set -root /home/gs/gridre/yroot."$CLUSTER" | grep TODO_YARN_NODEMANAGER_RUNTIME_LINUX_ALLOWED_RUNTIMES | cut -d: -f2`
 
-echo "Checking if we need to startup Docker daemon on node $HOSTNAME"
-
-
-function check_dockerd {
-  #echo "INFO: Checking if dockerd is running..."
-
-  # verify the storage driver in use, should be overlayFS
-  CHK_DRIVER=`sudo docker info | grep Storage`
-
-
-  if [[ "$CHK_DRIVER" =~ "overlay" ]]; then
-    echo "Docker storage driver in use is overlay"
-  else
-    echo "WARN: storage driver is not correct or not found!"
-
-    echo "Setting storage driver to overlay"
-    echo "{ \"storage-driver\": \"overlay\" }" > /etc/docker/daemon.json
-
-    echo "Restarting docker service"
-    set -x
-    systemctl restart docker
-    set +x
-  fi
-
-  PS_CHECK=`ps -ef | egrep dockerd | egrep -v grep`
-  if [[ "$PS_CHECK" =~ "docker-runc-current" ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
+echo "Checking if we need to startup image container daemon on node $HOSTNAME"
 
 #
 # based on OS version and docker enable yinst setting, decide it we need
 # to start dockerd
 #
-if [[ "$OS_VER" =~ ^7. ]] && [[ "$DOCKER_YINST_SET" =~ "fsimage" ]]; then
+if [[ "$OS_VER" =~ ^7. ]] && [[ "$ALLOWED_RUNTIMES" =~ "fsimage" ]]; then
   echo "INFO: OS is $OS_VER and runc support is up to date and enabled, ensuring packages are installed..."
 
   # Install missing tools if necessary (from setup_docker_hdfs.sh)
@@ -62,14 +31,12 @@ if [[ "$OS_VER" =~ ^7. ]] && [[ "$DOCKER_YINST_SET" =~ "fsimage" ]]; then
     sudo yum -y remove 'docker*' containers-common
     sudo yum -y --enablerepo=epel --enablerepo=non-core install $NEED_PKGS
   fi
+elif [[ "$OS_VER" =~ "7." ]]; then
+  echo "OS is $OS_VER, ALLOWED_RUNTIMES is not set for containers, not attempting start"
 elif [[ "$OS_VER" =~ "6." ]]; then
-  echo "OS is $OS_VER, RHEL6 does not support Docker, not attempting dockerd start"
-
-elif [[ ! "$DOCKER_YINST_SET" =~ "docker" ]]; then
-  echo "OS is $OS_VER but Docker support is not enabled in yinst setting, not attempting dockerd start"
-
+  echo "OS is $OS_VER, RHEL6 does not support containers, not attempting start"
 else
-  echo "WARN: Unknown OS $OS_VER and Docker support setting combination!"
+  echo "WARN: Unknown OS $OS_VER and ALLOWED_RUNTIMES $ALLOWED_RUNTIMES setting combination!"
   exit 1
 fi
  
