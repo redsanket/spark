9d4204b2c9c4bc61712bf54ce0807a70a21c5225
Joshua Martell <jmartell@yahoo-inc.com>
2020-05-19 15:08:29 -0500

    docker -> runC

--- deploySupport/setup_nm_dockerd_check_script.sh
+++ deploySupport/setup_nm_dockerd_check_script.sh
@@ -11,14 +11,12 @@ OS_VER=`cat /etc/redhat-release | cut -d' ' -f7`
 CLUSTER=`hostname | cut -d- -f1`
 ALLOWED_RUNTIMES=`/usr/local/bin/yinst set -root /home/gs/gridre/yroot."$CLUSTER" | grep TODO_YARN_NODEMANAGER_RUNTIME_LINUX_ALLOWED_RUNTIMES | cut -d: -f2`
 
-echo "Checking if we need to startup image container daemon on node $HOSTNAME"
-
 #
 # based on OS version and docker enable yinst setting, decide it we need
 # to start dockerd
 #
 if [[ "$OS_VER" =~ ^7. ]] && [[ "$ALLOWED_RUNTIMES" =~ "fsimage" ]]; then
-  echo "INFO: OS is $OS_VER and runc support is up to date and enabled, ensuring packages are installed..."
+  echo "INFO: OS is $OS_VER and runC support is enabled, ensuring packages are installed..."
 
   # Install missing tools if necessary (from setup_docker_hdfs.sh)
   NEED_PKGS=
@@ -32,9 +30,9 @@ if [[ "$OS_VER" =~ ^7. ]] && [[ "$ALLOWED_RUNTIMES" =~ "fsimage" ]]; then
     sudo yum -y --enablerepo=epel --enablerepo=non-core install $NEED_PKGS
   fi
 elif [[ "$OS_VER" =~ "7." ]]; then
-  echo "OS is $OS_VER, ALLOWED_RUNTIMES is not set for containers, not attempting start"
+  echo "OS is $OS_VER, ALLOWED_RUNTIMES is not set for runC containers"
 elif [[ "$OS_VER" =~ "6." ]]; then
-  echo "OS is $OS_VER, RHEL6 does not support containers, not attempting start"
+  echo "OS is $OS_VER, RHEL6 does not support runC containers"
 else
   echo "WARN: Unknown OS $OS_VER and ALLOWED_RUNTIMES $ALLOWED_RUNTIMES setting combination!"
   exit 1
