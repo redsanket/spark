e09aff5ddcbbf49ec23e8494bfba0b0a0d10ae93
Raj Chopde <rchopde@verizonmedia.com>
2020-06-11 09:58:29 -0500

    [GRIDCI-4715] - Fix yum db corrupt in deploy

--- deploySupport/115-installsteps-installhadooputils.sh
+++ deploySupport/115-installsteps-installhadooputils.sh
@@ -1,7 +1,7 @@
 set +x
 
 echo "Installing ygrid_hadoop_utils ..."
-cmd="/usr/bin/yum -y install ygrid-hadoop-utils"
+cmd="/usr/bin/yum clean dbcache; /usr/bin/yum clean metadata; /usr/bin/yum makecache; /usr/bin/yum -y install ygrid-hadoop-utils"
 set -x
 fanout $cmd
 set +x
