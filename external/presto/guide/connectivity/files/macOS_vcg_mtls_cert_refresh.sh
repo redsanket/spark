d29e56249983e745a2dd7e4ba93c7ffaec5b8ac0
Rohini Palaniswamy <rohinip@verizonmedia.com>
2020-09-28 12:44:00 -0700

    Fix keytool permission error on rerun. Just do one rm statement (#372)

--- external/presto/guide/connectivity/files/macOS_vcg_mtls_cert_refresh.sh
+++ external/presto/guide/connectivity/files/macOS_vcg_mtls_cert_refresh.sh
@@ -6,14 +6,12 @@ yinit
 athenz-user-cert
 
 # Using the user cert fetch the vcg.user.uid.${USER} role certificate which will be valid for 1 day
-rm -f ${HOME}/.athenz/vcg.user.uid.${USER}.cert.pem
+rm -f ${HOME}/.athenz/vcg.user.uid.${USER}.*
 openssl genrsa -out ${HOME}/.athenz/vcg.user.uid.${USER}.key.pem
 zts-rolecert -svc-key-file ${HOME}/.athenz/key -svc-cert-file ${HOME}/.athenz/cert -zts https://zts.athens.yahoo.com:4443/zts/v1 -role-domain vcg.user -role-name uid.${USER} -dns-domain zts.yahoo.cloud -role-cert-file  ${HOME}/.athenz/vcg.user.uid.${USER}.cert.pem -role-key-file ${HOME}/.athenz/vcg.user.uid.${USER}.key.pem
 openssl x509 -in ${HOME}/.athenz/vcg.user.uid.${USER}.cert.pem -text | grep -A2 Validity
 
 # Create keystore file for HiveServer2 JDBC
-rm -f ${HOME}/.athenz/vcg.user.uid.${USER}.jks
 openssl pkcs12 -export -name vcg.user.uid -inkey ${HOME}/.athenz/vcg.user.uid.${USER}.key.pem -in ${HOME}/.athenz/vcg.user.uid.${USER}.cert.pem -out ${HOME}/.athenz/vcg.user.uid.${USER}.pkcs12 -password pass:changeit
 keytool -importkeystore -srckeystore ${HOME}/.athenz/vcg.user.uid.${USER}.pkcs12 -srcstoretype PKCS12 -srcstorepass changeit -destkeystore ${HOME}/.athenz/vcg.user.uid.${USER}.jks -deststorepass changeit -noprompt
-rm -f ${HOME}/.athenz/vcg.user.uid.${USER}.pkcs12
 chmod 400 ${HOME}/.athenz/vcg.user.uid.*
