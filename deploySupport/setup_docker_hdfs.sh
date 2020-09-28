838b79d93015336965cf57eeeafd87aa46eeb6ac
hadoopqe <philips@oath.com>
2020-09-28 16:35:00 -0500

    GRIDCI-4920: Add /usr/sbin to PATH

--- deploySupport/setup_docker_hdfs.sh
+++ deploySupport/setup_docker_hdfs.sh
@@ -14,9 +14,13 @@ DOCKER_IMAGE_URI="${ADDR[0]}"
 DOCKER_IMAGE_TAG="${ADDR[1]}"
 IFS=' '
 
-echo "$DOCKER_IMAGE_URI"
-echo "$DOCKER_IMAGE_TAG"
+echo "DOCKER IMAGE URI: $DOCKER_IMAGE_URI"
+echo "DOCKER IMAGE TAG: $DOCKER_IMAGE_TAG"
 
+# GRIDCI-4920 - default PATH needs to include /usr/sbin so it will find
+# /usr/sbin/mksquashfs. This was the case when running on devadm102 as root,
+# but not from openre400red-n4.ygridvm.corp.bf1.yahoo.com as hadoopqa
+PATH=$PATH:/usr/sbin
 
 if [[ -z "$DOCKER_HDFS_ROOT" || -z "$DOCKER_IMAGE_TAG" ]]; then
   echo "Usage: setup_docker_hdfs.py docker_hdfs_root docker_image_uri,docker_image_tag"
@@ -44,7 +48,8 @@ fi
 TAG_TO_HASH_FILE="image-tag-to-hash"
 DOCKER_IMAGE_MAGIC_FILE="etc/hadoop-dockerfile-version"
 echo "Installing $DOCKER_IMAGE_URI to $DOCKER_HDFS_ROOT as $DOCKER_IMAGE_TAG"
+set -x
 kinit -kt /homes/hdfsqa/hdfsqa.dev.headless.keytab hdfsqa
 python $DOCKER_SQUASH_SCRIPT pull-build-push-update --log=DEBUG --bootstrap --check-magic-file --magic-file="$DOCKER_IMAGE_MAGIC_FILE" --hdfs-root="$DOCKER_HDFS_ROOT" --image-tag-to-hash="$TAG_TO_HASH_FILE" "$DOCKER_IMAGE_URI,$DOCKER_IMAGE_TAG"
-
+set +x
 exit 0
