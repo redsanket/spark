b7a10df7afbbd673c9e43280ce61a2081285ee51
hadoopqe <philips@oath.com>
2020-09-28 15:48:52 -0500

    updte

--- deploySupport/115-installsteps-installhadooputils.sh
+++ deploySupport/115-installsteps-installhadooputils.sh
@@ -3,6 +3,6 @@ set +x
 echo "Installing ygrid_hadoop_utils ..."
 cmd="/usr/bin/yum clean dbcache; /usr/bin/yum clean metadata; /usr/bin/yum makecache; /usr/bin/yum -y install ygrid-hadoop-utils"
 set -x
-fanout $cmd
+fanout "$cmd"
 set +x
 
