d4a7e2cae7a16e7929abfc8cc91332f8d9d9d4e1
hadoopqe <philips@oath.com>
2020-09-29 14:01:39 -0500

    update

--- deploySupport/167-installsteps-removeexistinglogs.sh
+++ deploySupport/167-installsteps-removeexistinglogs.sh
@@ -10,8 +10,9 @@ echo "== Remove old logs."
 set -x
 fanout "rm -rf ${GSHOME}/var/log && if [ ! -d /home/gs/var ] ; then mkdir -p /home/gs/var; fi && \
 if [ ! -d /grid/0/gs/var/log ] ; then mkdir -m 0755 -p /grid/0/gs/var/log; fi; \
-rm -rf /grid/0/gs/var/log/* ; ln -s /grid/0/gs/var/log ${GSHOME}/var/log "
+rm -rf /grid/0/gs/var/log/* ; ln -s /grid/0/gs/var/log ${GSHOME}/var/log"
+
 fanoutGW "rm -rf ${GSHOME}/var/log && if [ ! -d /home/gs/var ] ; then mkdir -p /home/gs/var; fi && \
 if [ ! -d /grid/0/gs/var/log ] ; then mkdir -m 0755 -p /grid/0/gs/var/log; fi; \
-rm -rf /grid/0/gs/var/log/* ; ln -s /grid/0/gs/var/log ${GSHOME}/var/log "
+rm -rf /grid/0/gs/var/log/* ; ln -s /grid/0/gs/var/log ${GSHOME}/var/log"
 set +x
