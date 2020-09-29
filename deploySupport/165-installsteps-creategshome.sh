b6f25ed44aa077b7455290f2aeb3e1bd4bbedaff
hadoopqe <philips@oath.com>
2020-09-29 18:22:08 -0500

    update

--- deploySupport/165-installsteps-creategshome.sh
+++ deploySupport/165-installsteps-creategshome.sh
@@ -8,9 +8,12 @@ echo "== create ${GSHOME} dirs."
 
 set -x
 fanout "if [ ! -d ${GSHOME} ] ; then  if [ $(df -k /home | awk ' /\// { print $2 } ') -le 100000000 ] ; then \
-mkdir -p /grid/0/gshome; ln -s /grid/0/gshome ${GSHOME}; else mkdir -p ${GSHOME}; fi; fi "
+mkdir -p /grid/0/gshome; ln -s /grid/0/gshome ${GSHOME}; else mkdir -p ${GSHOME}; fi; fi"
+
 fanoutGW "if [ ! -d ${GSHOME} ] ; then if [ $(df -k /home | awk ' /\// { print $2 } ') -le 100000000 ] ; then \
-mkdir -p /grid/0/gshome; ln -s /grid/0/gshome ${GSHOME}; else mkdir -p ${GSHOME}; fi; fi "
+mkdir -p /grid/0/gshome; ln -s /grid/0/gshome ${GSHOME}; else mkdir -p ${GSHOME}; fi; fi"
+
 fanout "if [ ! -d /home/gs/var/jobstatus ] ; then \
-mkdir -p /home/gs/var/jobstatus; chown -R $MAPREDUSER:users /home/gs/var/jobstatus; fi "
+mkdir -p /home/gs/var/jobstatus; chown -R $MAPREDUSER:users /home/gs/var/jobstatus; fi"
+
 set +x
