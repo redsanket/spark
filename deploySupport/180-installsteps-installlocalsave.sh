b367385f5e563504aa0288ba8754af1117023fcb
hadoopqe <philips@oath.com>
2020-09-28 21:39:34 -0500

    update

--- deploySupport/180-installsteps-installlocalsave.sh
+++ deploySupport/180-installsteps-installlocalsave.sh
@@ -5,41 +5,36 @@ if [ "$INSTALLLOCALSAVE" != true ]; then
 fi
 
 set -x
-cp slaves.$cluster.txt  /tmp/slaves.$cluster.txt
-[ -d $scripttmp ] || mkdir -p $scripttmp
-cp slaves.$cluster.txt  $scripttmp/slaves.$cluster.txt
-
-fanoutcmd "scp $scripttmp/slaves.$cluster.txt __HOSTNAME__:${GSHOME}/conf/local/slaves" "$HOSTLIST"
-fanoutcmd "scp $scripttmp/slaves.$cluster.txt __HOSTNAME__:${GSHOME}/gridre/yroot.$cluster/conf/hadoop/slaves.localcopy.txt" "$HOSTLIST"
+# /home/gs/conf/local/slaves
+# /home/gs/gridre/yroot.openphil1blue/conf/hadoop/slaves.localcopy.txt
+cp $WORK_DIR/slaves.$cluster.txt $scripttmp/slaves.$cluster.txt
+fanoutscp "$scripttmp/slaves.$cluster.txt" "${GSHOME}/conf/local/slaves" "$HOSTLIST"
+fanoutscp "$scripttmp/slaves.$cluster.txt" "${GSHOME}/gridre/yroot.$cluster/conf/hadoop/slaves.localcopy.txt" "$HOSTLIST"
 set +x
 
 (
 echo "cd /tmp"
+echo "export GSHOME=${GSHOME} "
 echo 'mkdir /tmp/$$ && cd /tmp/$$ '
 echo "/usr/local/bin/yinst fetch  $LOCAL_CONFIG_INSTALL_STRING -br quarantine "
 echo "tar xzf $LOCAL_CONFIG_PKG_NAME-*.tgz"
 echo "cd share/localhadoopconfigs"
-echo  "[ -d ${GSHOME}/conf/local ] || mkdir -p ${GSHOME}/conf/local "
-echo "for f in *.xml"
-echo "do"
-echo  '   if [ '!' -f ${GSHOME}/conf/local/${f} ] '
-echo '    then '
-echo '        cp $f ${GSHOME}/conf/local/${f}'
+echo "[ -d ${GSHOME}/conf/local ] || mkdir -p ${GSHOME}/conf/local "
+echo "for file in *.xml; do"
+echo '    if [ '!' -f ${GSHOME}/conf/local/${file} ]; then '
+echo '        cp $file ${GSHOME}/conf/local/${file}'
 echo '    fi '
 echo "done"
-echo "for f in dfs.*"
-echo "do"
-echo  '   if [ '!' -f ${GSHOME}/conf/local/${f} ] '
-echo '    then '
-echo '        cp $f ${GSHOME}/conf/local/${f}'
+echo "for file in dfs.*; do"
+echo '    if [ '!' -f ${GSHOME}/conf/local/${file} ]; then '
+echo '        cp $file ${GSHOME}/conf/local/${file}'
 echo '    fi '
 echo "done"
 echo 'cd /tmp && rm -rf /tmp/$$'
 ) > $scripttmp/$cluster.cplocalfiles.sh
 
-fanoutcmd "scp $scripttmp/$cluster.cplocalfiles.sh __HOSTNAME__:/tmp/$cluster.cplocalfiles.sh" "$HOSTLIST"
-cmd="GSHOME=${GSHOME} sh -x /tmp/$cluster.cplocalfiles.sh"
-
+fanoutscp "$scripttmp/$cluster.cplocalfiles.sh" "/tmp/$cluster.cplocalfiles.sh" "$HOSTLIST"
+cmd="sh -x /tmp/$cluster.cplocalfiles.sh"
 set -x
 fanout "$cmd"
 fanoutGW "$cmd"
@@ -48,8 +43,12 @@ set +x
 if [ -n "$secondarynamenode" ]; then
     cmd="echo \"$secondarynamenode\" > ${GSHOME}/conf/local/masters"
     set -x
-    for i in $namenode; do ssh $i "$cmd"; done
-    for j in $secondarynamenode; do ssh $j "$cmd"; done
-    ssh $jobtrackernode "$cmd"
+    for i in $namenode; do
+	ssh $i "sudo bash -c \"$cmd\""
+    done
+    for j in $secondarynamenode; do
+	ssh $j "sudo bash -c \"$cmd\""
+    done
+    ssh $jobtrackernode "sudo bash -c \"$cmd\""
     set +x
 fi
