e5cc08065dbe739edb1c5ad9b26038c95604cb51
hadoopqe <philips@oath.com>
2020-10-04 08:11:17 -0500

    update

--- deploySupport/160-installsteps-removeexistingdata.sh
+++ deploySupport/160-installsteps-removeexistingdata.sh
@@ -4,17 +4,17 @@ if [ "$REMOVEEXISTINGDATA" == "true" ]; then
     for nnode in $namenode; do
         short_name=`echo $nnode| sed 's/\..*$//g'`
         SHARED_DIR=$HOMEDIR/$HDFSUSER/ha_namedir/${cluster}_${short_name}
-        echo "Removing $SHARED_DIR."
-        set +e
-        rm -rf $SHARED_DIR
-        set -e
+        if [ -d $SHARED_DIR ]; then
+            echo "Removing $SHARED_DIR"
+            sudo rm -rf $SHARED_DIR
+        fi
     done
     set -x
     fanoutscp "$scriptdir/cleangrid.sh" "/tmp/cleangrid.sh" "$HOSTLIST"
     fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER &&  sh /tmp/cleangrid.sh && rm /tmp/cleangrid.sh"
     set +x
 elif [ "$REMOVE_YARN_DATA" == "true" ]; then
-    echo == removing existing yarn data on grid.
+    echo "== removing existing yarn data on grid."
     # YINST root is where the deployment yinst package is installed on devadm102
     # ROOT_DIR vs scriptdir...
     set -x
