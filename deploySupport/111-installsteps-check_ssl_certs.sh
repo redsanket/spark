6f73fbacd9feae7fac46fc692027f03eb546ebb4
hadoopqe <philips@oath.com>
2020-10-05 12:32:59 -0500

    update

--- deploySupport/111-installsteps-check_ssl_certs.sh
+++ deploySupport/111-installsteps-check_ssl_certs.sh
@@ -21,11 +21,11 @@ CERT_HOME="/etc/ssl/certs/prod/_open_ygrid_yahoo_com"
 
 echo "== verify Core SSL certs are in place"
 
-fanout "if [ ! -d ${CERT_HOME} ] ; then \
-echo \"Going to create ${CERT_HOME}\"; \
-mkdir -p ${CERT_HOME}; \
-chmod 755 ${CERT_HOME}; \
-fi"
+fanout "if [ ! -d ${CERT_HOME} ] ; then
+           echo \"Going to create ${CERT_HOME}\";
+           mkdir -p ${CERT_HOME};
+           chmod 755 ${CERT_HOME};
+        fi"
 
 NODES=`yinst range -ir @grid_re.clusters.$CLUSTER,@grid_re.clusters.$CLUSTER.gateway`
 transport_files_from_admin $ADM_HOST $CERT_REFERENCE_PATH "$NODES" $CERT_HOME "root:root"
