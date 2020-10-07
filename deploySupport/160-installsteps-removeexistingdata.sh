9662e388e96f378e364c8eca8d3badae00173cab
Phil Su <philips@oath.com>
2020-10-07 10:58:04 -0500

    update to fix scp issue (#452)

--- deploySupport/160-installsteps-removeexistingdata.sh
+++ deploySupport/160-installsteps-removeexistingdata.sh
@@ -9,14 +9,14 @@ if [ "$REMOVEEXISTINGDATA" == "true" ]; then
     done
     set -x
     cp ${YINST_ROOT}/conf/hadoop/hadoopAutomation/cleangrid.sh  $scripttmp/cleangrid.sh
-    fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
+    fanoutcmd "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
     fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER &&  sh /tmp/cleangrid.sh && rm /tmp/cleangrid.sh "
     set +x
 elif [ "$REMOVE_YARN_DATA" == "true" ]; then
     echo == removing existing yarn data on grid.
     set -x
     cp ${YINST_ROOT}/conf/hadoop/hadoopAutomation/cleangrid.sh  $scripttmp/cleangrid.sh
-    fanoutcmd "scp /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
+    fanoutcmd "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /grid/0/tmp/scripts.deploy.$cluster/cleangrid.sh __HOSTNAME__:/tmp/cleangrid.sh" "$HOSTLIST"
     fanout "export GSHOME=$GSHOME && export HDFSUSER=$HDFSUSER && export MAPREDUSER=$MAPREDUSER && sh /tmp/cleangrid.sh yarn && rm /tmp/cleangrid.sh "
     set +x
 else
